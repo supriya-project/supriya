2f437faa35155399171ae64b88c6baddf5fbd113
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-05-12 14:52:20 -0400

    Implemented some of RootNode.

--- supriya/library/controllib/RootNode.py
+++ supriya/library/controllib/RootNode.py
@@ -7,3 +7,23 @@ class RootNode(Group):
 
     __slots__ = (
         )
+
+    _instance = None
+
+    ### CONSTRUCTOR ###
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(RootNode, cls).__new__(
+                cls, *args, **kwargs)
+        return cls._instance
+
+    ### INITIALIZER ###
+
+    def __init__(self):
+        from supriya.library import controllib
+        controllib.Node.__init__(
+            self,
+            node_id=0,
+            server=controllib.Server(),
+            )
