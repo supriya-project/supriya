68bbe2c23cf15febf1ea0cf3d0444e559860aeeb
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-05-30 19:09:06 -0400

    Updated library/__init__.py.

--- supriya/library/__init__.py
+++ supriya/library/__init__.py
@@ -1,3 +1,5 @@
 from supriya.library import audiolib
 from supriya.library import controllib
+from supriya.library import corelib
+from supriya.library import osclib
 from supriya.library import systemlib
