0dcdc80f94b897915f30b53bee7b6ca943b38dcd
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-05-30 13:14:19 -0400

    Implemented OscMessage.to_datagram() and .from_datagram().

--- supriya/library/osclib/OscMessage.py
+++ supriya/library/osclib/OscMessage.py
@@ -26,21 +26,116 @@ class OscMessage(object):
     ### PRIVATE METHODS ###
 
     @staticmethod
+    def _decode_array(type_tags, type_tag_offset, payload, payload_offset):
+        assert type_tags[type_tag_offset] == '['
+        array = []
+        type_tag_offset += 1
+        while type_tags[type_tag_offset] != ']':
+            result, type_tag_offset, payload_offset = \
+                OscMessage._decode_value(
+                    type_tags,
+                    type_tag_offset,
+                    payload,
+                    payload_offset,
+                    )
+            array.append(result)
+        assert type_tags[type_tag_offset] == ']'
+        type_tag_offset += 1
+        return result, type_tag_offset, payload_offset
+
+    @staticmethod
+    def _decode_blob(type_tags, type_tag_offset, payload, payload_offset):
+        assert type_tags[type_tag_offset] == 'b'
+        length = payload[payload_offset:payload_offset + 4]
+        length = struct.unpack('>i', length)
+        length = length[0]
+        payload_offset += 4
+        total_length = length + (-length % 4)
+        result = payload[payload_offset:payload_offset + length]
+        type_tag_offset += 1
+        payload_offset += total_length
+        return result, type_tag_offset, payload_offset
+
+    @staticmethod
+    def _decode_boolean(type_tags, type_tag_offset, payload, payload_offset):
+        type_tag = type_tags[type_tag_offset]
+        assert type_tag in ('T', 'F')
+        if type_tag == 'T':
+            result = True
+        elif type_tag == 'F':
+            result = False
+        type_tag_offset += 1
+        return result, type_tag_offset, payload_offset
+
+    @staticmethod
+    def _decode_float(type_tags, type_tag_offset, payload, payload_offset):
+        assert type_tags[type_tag_offset] == 'f'
+        result = payload[payload_offset:payload_offset + 4]
+        result = struct.unpack('>f', result)
+        result = result[0]
+        type_tag_offset += 1
+        payload_offset += 4
+        return result, type_tag_offset, payload_offset
+
+    @staticmethod
+    def _decode_int(type_tags, type_tag_offset, payload, payload_offset):
+        assert type_tags[type_tag_offset] == 'i'
+        result = payload[payload_offset:payload_offset + 4]
+        result = struct.unpack('>i', result)
+        result = result[0]
+        type_tag_offset += 1
+        payload_offset += 4
+        return result, type_tag_offset, payload_offset
+
+    @staticmethod
+    def _decode_none(type_tags, type_tag_offset, payload, payload_offset):
+        assert type_tags[type_tag_offset] == 'N'
+        result = None
+        type_tag_offset += 1
+        return result, type_tag_offset, payload_offset
+
+    @staticmethod
+    def _decode_string(type_tags, type_tag_offset, payload, payload_offset):
+        assert type_tags[type_tag_offset] == 's'
+        type_tag_offset += 1
+        result, payload_offset = OscMessage._read_string(payload, payload_offset)
+        return result, type_tag_offset, payload_offset
+
+    @staticmethod
+    def _decode_value(type_tags, type_tag_offset, payload, payload_offset):
+        type_tag = type_tags[type_tag_offset]
+        type_tag_mapping = {
+            'N': OscMessage._decode_none,
+            'T': OscMessage._decode_boolean,
+            'F': OscMessage._decode_boolean,
+            'i': OscMessage._decode_int,
+            'f': OscMessage._decode_float,
+            's': OscMessage._decode_string,
+            'b': OscMessage._decode_blob,
+            '[': OscMessage._decode_array,
+            }
+        print repr(type_tags), type_tag_offset, repr(payload), payload_offset
+        procedure = type_tag_mapping[type_tag]
+        result, type_tag_offset, payload_offset = \
+            procedure(type_tags, type_tag_offset, payload, payload_offset)
+        return result, type_tag_offset, payload_offset
+
+    @staticmethod
     def _encode_array(array):
         assert isinstance(array, collections.Sequence)
-        type_tags = b'['
+        type_tags = '['
         encoded_value = b''
         for value in array:
             sub_type_tags, sub_encoded_value = OscMessage._encode_value(value)
             type_tags += sub_type_tags
             encoded_value += sub_encoded_value
-        type_tags += b']'
+        type_tags += ']'
         return type_tags, encoded_value
 
     @staticmethod
     def _encode_blob(value):
         assert isinstance(value, (bytearray, bytes))
-        type_tags = b'b'
+        type_tags = 'b'
         encoded_value = OscMessage._encode_int(len(value))
         encoded_value += value
         difference = 4 - (len(encoded_value) % 4)
@@ -52,39 +147,39 @@ class OscMessage(object):
     def _encode_boolean(value):
         assert value in (True, False)
         if value:
-            type_tags = b'T'
+            type_tags = 'T'
         else:
-            type_tags = b'F'
+            type_tags = 'F'
         encoded_value = None
         return type_tags, encoded_value
 
     @staticmethod
     def _encode_float(value):
         assert isinstance(value, float)
-        type_tags = b'f'
+        type_tags = 'f'
         encoded_value = struct.pack('>f', value)
         return type_tags, encoded_value
 
     @staticmethod
     def _encode_int(value):
         assert isinstance(value, int)
-        type_tags = b'i'
+        type_tags = 'i'
         encoded_value = struct.pack('>i', value)
         return type_tags, encoded_value
 
     @staticmethod
     def _encode_none():
-        type_tags = b'N'
+        type_tags = 'N'
         encoded_value = None
         return type_tags, encoded_value
 
     @staticmethod
     def _encode_string(value):
         assert isinstance(value, str)
-        type_tags = b's'
+        type_tags = 's'
         encoded_value = value.encode('utf-8')
-        difference = 4 - (len(encoded_value) % 4)
-        padding = b'\x00' * difference
+        padding_length = 4 - (len(encoded_value) % 4)
+        padding = b'\x00' * padding_length
         encoded_value += padding
         return type_tags, encoded_value
 
@@ -108,31 +203,62 @@ class OscMessage(object):
             raise TypeError
         return type_tags, encoded_value
 
+    @staticmethod
+    def _read_string(payload, payload_offset):
+        offset = 0
+        while ord(payload[payload_offset + offset]) != 0:
+            offset += 1
+        if (offset % 4) == 0:
+            offset += 4
+        else:
+            offset += -offset % 4
+        result = payload[payload_offset:payload_offset + offset]
+        result = result.replace(b'\x00', '')
+        result = result.decode('utf-8')
+        payload_offset += offset
+        return result, payload_offset
+
     ### PUBLIC METHODS ###
 
-    def to_datagram(
-        self,
-        ):
+    def to_datagram(self):
         datagram = OscMessage._encode_string(self.address)[1]
         if self.arguments is None:
             return datagram
-        encoded_type_tags = b''
+        encoded_type_tags = ','
         encoded_arguments = b''
         for argument in self.arguments:
             type_tags, encoded_value = OscMessage._encode_value(argument)
             encoded_type_tags += type_tags
             if encoded_value is not None:
                 encoded_arguments += encoded_value
-        print datagram, encoded_type_tags, encoded_arguments
+        encoded_type_tags = OscMessage._encode_string(encoded_type_tags)[1]
         datagram += encoded_type_tags
         datagram += encoded_arguments
+        datagram = bytes(datagram)
         return datagram
 
     @staticmethod
-    def from_datagram(
-        datagram,
-        ):
-        pass
+    def from_datagram(datagram):
+        arguments = []
+        offset = 0
+        address, offset = OscMessage._read_string(datagram, offset)
+        type_tags, offset = OscMessage._read_string(datagram, offset)
+        print address, type_tags, offset
+        assert type_tags[0] == ','
+        payload = bytes(datagram[offset:])
+        payload_offset = 0
+        type_tag_offset = 1
+        while type_tag_offset < len(type_tags):
+            result, type_tag_offset, payload_offset = \
+                OscMessage._decode_value(
+                    type_tags,
+                    type_tag_offset,
+                    payload,
+                    payload_offset
+                    )
+            arguments.append(result)
+        osc_message = OscMessage(address, *arguments)
+        return osc_message
 
     ### PUBLIC PROPERTIES ###
 
