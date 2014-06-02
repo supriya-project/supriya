b64a270afa30fd6925f45baa3054b094fe790ac3
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-02 16:42:58 -0400

    Updated tox.ini and setup.py.

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ install_requires = [
 version = '.'.join(str(x) for x in sys.version_info[:3])
 if StrictVersion(version) < StrictVersion('3.4.0'):
     install_requires.append('enum34')
+if StrictVersion(version) < StrictVersion('3.3.0'):
+    install_requires.append('funcsigs')
 
 setup(
     author='Josiah Wolf Oberholtzer',
