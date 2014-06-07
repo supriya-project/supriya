8754c3ce9ea8fa04dd87a69939b06a3080d885e3
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-06 23:26:18 -0400

    Renamed Enumeration.new() to Enumeration.from_expr().

--- supriya/library/systemlib/Enumeration.py
+++ supriya/library/systemlib/Enumeration.py
@@ -7,11 +7,13 @@ class Enumeration(enum.IntEnum):
     ### PUBLIC METHODS ###
 
     @classmethod
-    def new(cls, expr):
+    def from_expr(cls, expr):
         if isinstance(expr, int):
             return cls(expr)
         elif isinstance(expr, str):
             expr = expr.upper()
+            expr = expr.strip()
+            expr = expr.replace(' ', '_')
             return cls[expr]
         message = 'Cannot instantiate {} from {}.'.format(cls.__name__, expr)
         raise ValueError(message)
--- supriya/library/systemlib/__init__.py
+++ supriya/library/systemlib/__init__.py
@@ -1,1 +1,2 @@
+from supriya.library.systemlib.Enumeration import Enumeration
 from supriya.library.systemlib.ImportManager import ImportManager
