ccdf287d97099c50a98bf4aeeaf7c917d1d02b3e
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-07 23:11:33 -0400

    Added controllib.ServerResponse base class.

---
+++ supriya/library/controllib/ServerResponse.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+from supriya.library.systemlib.SupriyaObject import SupriyaObject
+
+
+class ServerResponse(SupriyaObject):
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        )
+
+    ### SPECIAL METHODS ###
+
+    def __eq__(self, expr):
+        from abjad.tools import systemtools
+        return systemtools.StorageFormatManager.compare(self, expr)
+
+    def __hash__(self, expr):
+        from abjad.tools import systemtools
+        hash_values = systemtools.StorageFormatManager.get_hash_values(self)
+        return hash(hash_values)
