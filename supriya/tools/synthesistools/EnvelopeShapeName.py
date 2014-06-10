f94725c8f5f766eb10bc010d7289a6b790aef015
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-09 23:55:56 -0400

    Added EnvelopeShapeName enumeration to synthesistools.

---
+++ supriya/tools/synthesistools/EnvelopeShapeName.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.systemtools.Enumeration import Enumeration
+
+
+class EnvelopeShape(Enumeration):
+
+    ### CLASS VARIABLES ###
+
+    CUBED = 7
+    CUSTOM = 5
+    EXPONENTIAL = 2
+    LINEAR = 1
+    SINE = 3
+    SQUARED = 6
+    STEP = 0
+    WELCH = 4
