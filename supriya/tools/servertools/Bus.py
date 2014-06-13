b3d80b4fb694f664a77d1ea18b3078796539aef7
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-13 01:26:12 -0400

    Implementing BusProxy.

--- supriya/tools/servertools/Bus.py
+++ supriya/tools/servertools/Bus.py
@@ -1,8 +1,9 @@
 # -*- encoding: utf-8 -*-
+import collections
 from supriya.tools.servertools.ServerObjectProxy import ServerObjectProxy
 
 
-class Bus(ServerObjectProxy):
+class Bus(ServerObjectProxy, collections.Sequence):
     r'''A bus.
 
     ::
@@ -13,12 +14,37 @@ class Bus(ServerObjectProxy):
         ...    calculation_rate=synthdeftools.CalculationRate.AUDIO,
         ...    )
 
+    ::
+
+        >>> server = servertools.Server().boot()
+        >>> bus.allocate()
+        >>> bus[0]
+        BusProxy(
+            bus=Bus(
+                calculation_rate=<CalculationRate.AUDIO: 2>,
+                channel_count=1
+                ),
+            index=0
+            )
+
+    ::
+
+        >>> bus[0].map_symbol
+
+    ::
+
+        >>> bus.free()
+        >>> server = server.quit()
+        RECV: OscMessage('/done', '/quit')
+
     '''
     ### CLASS VARIABLES ###
 
     __slots__ = (
         '_bus_id',
+        '_bus_proxies',
         '_calculation_rate',
+        '_channel_count',
         )
 
     ### INITIALIZER ###
@@ -26,18 +52,53 @@ class Bus(ServerObjectProxy):
     def __init__(
         self,
         calculation_rate=None,
+        channel_count=1,
         ):
+        from supriya.tools import servertools
         from supriya.tools import synthdeftools
         ServerObjectProxy.__init__(self)
         if calculation_rate is None:
             calculation_rate = synthdeftools.CalculationRate.AUDIO
-        calculation_rate = synthdeftools.CalculationRate.from_expr(calculation_rate)
+        calculation_rate = synthdeftools.CalculationRate.from_expr(
+            calculation_rate)
         assert calculation_rate in (
             synthdeftools.CalculationRate.AUDIO,
             synthdeftools.CalculationRate.CONTROL,
             )
+        channel_count = int(channel_count)
+        assert 0 < channel_count
         self._calculation_rate = calculation_rate
+        self._channel_count = channel_count
         self._bus_id = None
+        self._bus_proxies = tuple(
+            servertools.BusProxy(bus=self, index=i)
+            for i in range(self.channel_count)
+            )
+
+    ### SPECIAL METHODS ###
+
+    def __getitem__(self, item):
+        return self.bus_proxies.__getitem__(item)
+
+    def __len__(self):
+        return len(self.bus_proxies)
+
+    ### PRIVATE METHODS ###
+
+    @staticmethod
+    def _as_map(
+        bus_id=None,
+        calculation_rate=None,
+        ):
+        from supriya.tools import synthdeftools
+        if calculation_rate == synthdeftools.CalculationRate.AUDIO:
+            string = 'a{}'
+        elif calculation_rate == synthdeftools.CalculationRate.CONTROL:
+            string = 'c{}'
+        else:
+            raise ValueError
+        string = string.format(bus_id)
+        return string
 
     ### PUBLIC METHODS ###
 
@@ -45,17 +106,17 @@ class Bus(ServerObjectProxy):
         from supriya.tools import synthdeftools
         ServerObjectProxy.allocate(self, server=server)
         if self.calculation_rate == synthdeftools.CalculationRate.AUDIO:
-            bus_id = server.audio_bus_allocator.allocate()
+            bus_id = self.server.audio_bus_allocator.allocate()
         else:
-            bus_id = server.control_bus_allocator.allocate()
+            bus_id = self.server.control_bus_allocator.allocate()
         if bus_id is None:
             raise Exception
         if self.calculation_rate == synthdeftools.CalculationRate.AUDIO:
-            assert bus_id not in self._server._audio_busses
-            self._server._audio_busses[self._bus_id] = self
+            assert bus_id not in self.server._audio_busses
+            self.server._audio_busses[bus_id] = self
         else:
-            assert bus_id not in self._server._control_busses
-            self._server._control_busses[self._bus_id] = self
+            assert bus_id not in self.server._control_busses
+            self.server._control_busses[bus_id] = self
         self._bus_id = bus_id
 
     def ar(self):
@@ -97,8 +158,16 @@ class Bus(ServerObjectProxy):
         return self._bus_id
 
     @property
+    def bus_proxies(self):
+        return self._bus_proxies
+
+    @property
     def calculation_rate(self):
         return self._calculation_rate
+
+    @property
+    def channel_count(self):
+        return self._channel_count
 
     @property
     def is_settable(self):
@@ -107,11 +176,8 @@ class Bus(ServerObjectProxy):
 
     @property
     def map_symbol(self):
-        from supriya.tools import synthdeftools
         assert self.bus_id is not None
-        if self.calculation_rate == synthdeftools.CalculationRate.AUDIO:
-            string = 'a{}'
-        else:
-            string = 'c{}'
-        string = string.format(self.bus_id)
-        return string
+        return Bus._as_map(
+            bus_id=self.bus_id,
+            calculation_rate=self.calculation_rate,
+            )
---
+++ supriya/tools/servertools/BusProxy.py
@@ -0,0 +1,65 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.systemtools.SupriyaObject import SupriyaObject
+
+
+class BusProxy(SupriyaObject):
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        '_bus',
+        '_index',
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(self,
+        bus=None,
+        index=None,
+        ):
+        from supriya.tools import servertools
+        assert isinstance(bus, servertools.Bus)
+        index = int(index)
+        assert 0 <= index <= bus.channel_count
+        self._bus = bus
+        self._index = index
+
+    ### SPECIAL METHODS ###
+
+    def __eq__(self, expr):
+        from abjad.tools import systemtools
+        return systemtools.StorageFormatManager.compare(self, expr)
+
+    def __hash__(self):
+        from abjad.tools import systemtools
+        hash_values = systemtools.StorageFormatManager.get_hash_values(self)
+        return hash(hash_values)
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def bus(self):
+        return self._bus
+
+    @property
+    def bus_id(self):
+        if self.bus.bus_id is None:
+            return None
+        return self.bus.bus_id + self.index
+
+    @property
+    def calculation_rate(self):
+        return self.bus.calculation_rate
+
+    @property
+    def index(self):
+        return self._index
+
+    @property
+    def map_symbol(self):
+        from supriya.tools import servertools
+        assert self.bus_id is not None
+        return servertools.Bus._as_map(
+            bus_id=self.bus_id,
+            calculation_rate=self.calculation_rate,
+            )
