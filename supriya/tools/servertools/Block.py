36c4f03411f88b0d3f5f6f062d27a8505ab5458d
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-13 14:17:20 -0400

    Implementing BlockAllocator and Block.

---
+++ supriya/tools/servertools/Block.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools.timespantools.Timespan import Timespan
+
+
+class Block(Timespan):
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        '_used',
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        start_offset=NegativeInfinity,
+        stop_offset=Infinity,
+        used=False,
+        ):
+        if start_offset is None:
+            start_offset = NegativeInfinity
+        if stop_offset is None:
+            stop_offset = Infinity
+        Timespan.__init__(
+            self,
+            start_offset=start_offset,
+            stop_offset=stop_offset,
+            )
+        self._used = bool(used)
+
+    ### PRIVATE METHODS ###
+
+    def _initialize_offset(self, offset):
+        if offset in (NegativeInfinity, Infinity):
+            return offset
+        else:
+            return int(offset)
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def used(self):
+        return self._used
--- supriya/tools/servertools/BlockAllocator.py
+++ supriya/tools/servertools/BlockAllocator.py
@@ -1,103 +1,75 @@
-class BlockAllocator(object):
+# -*- encoding: utf-8 -*-
+import threading
+from supriya.tools.systemtools.SupriyaObject import SupriyaObject
+
+
+class BlockAllocator(SupriyaObject):
+    r'''A block allocator.
+    '''
 
     ### CLASS VARIABLES ###
 
-    class ContiguousBlock(object):
-
-        ### CLASS VARIABLES ###
-
-        __slots__ = (
-            '_start',
-            '_size',
-            '_used',
-            )
-
-        ### INITIALIZER ###
-
-        def __init__(self, start, size, used=False):
-            self._start = int(start)
-            self._size = int(size)
-            self._used = bool(used)
-
-        ### PUBLIC METHODS ###
-
-        def adjoins(self, block):
-            if self.start < block.start and block.start <= self.stop:
-                return True
-            elif block.start < self.start and self.start <= block.stop:
-                return True
-            return False
-
-        def join(self, block):
-            if not self.adjoins(block):
-                return None
-            new_start = min(self.start, block.start)
-            new_size = max(self.stop, block.stop) - new_start
-            return type(self)(new_start, new_size, used=self.used)
-
-        def split(self, size):
-            if size < self.size:
-                result = [
-                    type(self)(
-                        self.start,
-                        size,
-                        used=self.used,
-                        ),
-                    type(self)(
-                        self.start + size,
-                        self.size - size,
-                        used=self.used,
-                        )
-                    ]
-            elif size == self.size:
-                result = [self, None]
-            else:
-                result = []
-            return result
-
-        ### PUBLIC PROPERTIES ###
-
-        @property
-        def start(self):
-            return self._start
-
-        @property
-        def size(self):
-            return self._size
-
-        @property
-        def stop(self):
-            return self.start + self.size
-
-        @property
-        def used(self):
-            return self._used
-
-        @used.setter
-        def used(self, expr):
-            self._used = bool(expr)
-
     __slots__ = (
-        '_size',
-        '_blocks',
-        '_freed_blocks',
-        '_initial_position',
-        '_maximum_position',
+        '_free_heap',
+        '_heap_maximum',
+        '_heap_minimum',
+        '_id_class',
+        '_lock',
+        '_server',
+        '_used_heap',
         )
 
     ### INITIALIZER ###
 
-    def __init__(self, size=None, initial_position=0):
-        self._blocks = {}
-        self._freed_blocks = {}
-        self._initial_position = initial_position
-        self._maximum_position = initial_position
-        self._size = size
+    def __init__(
+        self,
+        heap_maximum=None,
+        heap_minimum=0,
+        id_class=None,
+        server=None,
+        ):
+        from supriya.tools import servertools
+        from supriya.tools import timetools
+        self._free_heap = timetools.TimespanCollection()
+        self._heap_maximum = heap_maximum
+        self._heap_minimum = heap_minimum
+        self._id_class = id_class
+        self._lock = threading.Lock()
+        self._server = server
+        self._used_heap = timetools.TimespanCollection()
+        free_block = servertools.Block(
+            start_offset=heap_minimum,
+            stop_offset=heap_maximum,
+            used=False,
+            )
+        self._free_heap.insert(free_block)
 
     ### PUBLIC METHODS ###
 
-    def allocate(self, size=1):
-        pass
+    def allocate(self, block_size=1):
+        block_size = int(block_size)
+        assert 0 < block_size
+        with self._lock:
+            pass
 
-    def free(self, position):
-        pass
+    def free(self, block_id):
+        with self._lock:
+            pass
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def heap_maximum(self):
+        return self._heap_maximum
+
+    @property
+    def heap_minimum(self):
+        return self._heap_minimum
+
+    @property
+    def id_class(self):
+        return self._id_class
+
+    @property
+    def server(self):
+        return self._server
