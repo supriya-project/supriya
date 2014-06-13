de8b225bd5aae12fe2bf2da609616a815f50a642
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-13 12:31:51 -0400

    Added HeaderFormat and SampleFormat enumerations.

---
+++ supriya/tools/servertools/HeaderFormat.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.systemtools.Enumeration import Enumeration
+
+
+class HeaderFormat(Enumeration):
+
+    ### CLASS VARIABLES ###
+
+    AIFF = 0
+    IRCAM = 1
+    NEXT = 2
+    RAW = 3
+    WAV = 4
---
+++ supriya/tools/servertools/SampleFormat.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.systemtools.Enumeration import Enumeration
+
+
+class SampleFormat(Enumeration):
+
+    ### CLASS VARIABLES ###
+
+    ALAW = 0
+    DOUBLE = 1
+    FLOAT = 2
+    INT8 = 3
+    INT16 = 4
+    INT24 = 5
+    INT32 = 6
+    MULAW = 7
