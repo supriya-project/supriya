db1454d2ff0cd46410f983e8fcd3b1662ef6a532
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-16 16:47:57 -0400

    Added NonBlockingStreamReader to systemtools.

--- supriya/tools/systemtools/__init__.py
+++ supriya/tools/systemtools/__init__.py
@@ -1,3 +1,4 @@
 # -*- encoding: utf-8 -*-
 from supriya.tools.systemtools.Enumeration import Enumeration
+from supriya.tools.systemtools.NonBlockingStreamReader import NonBlockingStreamReader
 from supriya.tools.systemtools.SupriyaObject import SupriyaObject
