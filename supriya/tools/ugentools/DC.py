b3c556b1ebd0a2bef634c71f3cfe0dbfb4e3472c
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-18 14:34:29 -0400

    Implemented DC, A2K and K2A.

---
+++ supriya/tools/ugentools/DC.py
@@ -0,0 +1,116 @@
+# -*- encoding: utf-8 -*-
+import collections
+from supriya.tools.synthdeftools.Argument import Argument
+from supriya.tools.ugentools.PureMultiOutUGen import PureMultiOutUGen
+
+
+class DC(PureMultiOutUGen):
+    r'''DC unit generator.
+
+    ::
+
+        >>> from supriya.tools import ugentools
+        >>> ugentools.DC.ar(source=(1, 2, 3))
+        UGenArray(
+            (
+                OutputProxy(
+                    source=DC(
+                        calculation_rate=<CalculationRate.AUDIO: 2>,
+                        channel_count=3,
+                        source=1.0
+                        ),
+                    output_index=0
+                    ),
+                OutputProxy(
+                    source=DC(
+                        calculation_rate=<CalculationRate.AUDIO: 2>,
+                        channel_count=3,
+                        source=1.0
+                        ),
+                    output_index=1
+                    ),
+                OutputProxy(
+                    source=DC(
+                        calculation_rate=<CalculationRate.AUDIO: 2>,
+                        channel_count=3,
+                        source=1.0
+                        ),
+                    output_index=2
+                    ),
+                )
+            )
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
+
+    _argument_specifications = (
+        Argument('source'),
+        )
+
+    _unexpanded_argument_names = (
+        'source',
+        'channel_count',
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        calculation_rate=None,
+        channel_count=None,
+        source=None,
+        ):
+        PureMultiOutUGen.__init__(
+            self,
+            calculation_rate=calculation_rate,
+            channel_count=channel_count,
+            source=source,
+            )
+
+    ### PRIVATE METHODS ###
+
+    @classmethod
+    def _new(
+        cls,
+        calculation_rate=None,
+        source=None,
+        ):
+        if not isinstance(source, collections.Sequence):
+            source = [source]
+        channel_count = len(source)
+        return super(DC, cls)._new(
+            calculation_rate=calculation_rate,
+            channel_count=channel_count,
+            source=source,
+            )
+
+    ### PUBLIC METHODS ###
+
+    @classmethod
+    def ar(
+        cls,
+        source=None,
+        ):
+        from supriya.tools import synthdeftools
+        calculation_rate = synthdeftools.CalculationRate.AUDIO
+        ugen = cls._new(
+            calculation_rate=calculation_rate,
+            source=source,
+            )
+        return ugen
+
+    @classmethod
+    def kr(
+        cls,
+        source=None,
+        ):
+        from supriya.tools import synthdeftools
+        calculation_rate = synthdeftools.CalculationRate.CONTROL
+        ugen = cls._new(
+            calculation_rate=calculation_rate,
+            source=source,
+            )
+        return ugen
---
+++ supriya/tools/ugentools/K2A.py
@@ -0,0 +1,54 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.synthdeftools.Argument import Argument
+from supriya.tools.ugentools.PureUGen import PureUGen
+
+
+class K2A(PureUGen):
+    r'''Control rate to audio rate convert unit generator.
+
+    ::
+
+        >>> from supriya.tools import ugentools
+        >>> source = ugentools.SinOsc.kr()
+        >>> ugentools.K2A.ar(
+        ...     source=source,
+        ...     )
+        K2A.ar()
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
+
+    _argument_specifications = (
+        Argument('source'),
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        source=None,
+        calculation_rate=None,
+        ):
+        PureUGen.__init__(
+            self,
+            source=source,
+            calculation_rate=calculation_rate,
+            )
+
+    ### PUBLIC METHODS ###
+
+    @classmethod
+    def ar(
+        cls,
+        source=None,
+        ):
+        from supriya.tools import synthdeftools
+        calculation_rate = synthdeftools.CalculationRate.AUDIO
+        ugen = cls._new(
+            calculation_rate=calculation_rate,
+            source=source,
+            )
+        return ugen
