e6cf75a9aa018c9df433aa16d9ea084cd01c85d8
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-18 14:58:36 -0400

    Implemented Delay1 and Delay2 UGens.

---
+++ supriya/tools/ugentools/Delay1.py
@@ -0,0 +1,93 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.synthdeftools.Argument import Argument
+from supriya.tools.ugentools.PureUGen import PureUGen
+
+
+class Delay1(PureUGen):
+    r'''One-sample delay line unit generator.
+
+    ::
+
+        >>> from supriya.tools import ugentools
+        >>> source = ugentools.In.ar(bus=0)
+        >>> ugentools.Delay1.ar(source=source)
+        Delay1.ar()
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        )
+
+    _argument_specifications = (
+        Argument('source'),
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        calculation_rate=None,
+        source=None,
+        ):
+        PureUGen.__init__(
+            self,
+            calculation_rate=calculation_rate,
+            source=source,
+            )
+
+    ### PUBLIC METHODS ###
+
+    @classmethod
+    def ar(
+        cls,
+        source=None,
+        ):
+        r'''Create an audio-rate one-sample delay line.
+
+        ::
+
+            >>> from supriya.tools import ugentools
+            >>> in_ = ugentools.In.ar(bus=0)
+            >>> ugentools.Delay1.ar(
+            ...     source=in_,
+            ...     )
+            Delay1.ar()
+
+        Returns unit generator graph.
+        '''
+        from supriya.tools import synthdeftools
+        calculation_rate = synthdeftools.CalculationRate.AUDIO
+        ugen = cls._new(
+            calculation_rate=calculation_rate,
+            source=source,
+            )
+        return ugen
+
+    @classmethod
+    def kr(
+        cls,
+        source=None,
+        ):
+        r'''Create a control-rate one-sample delay line.
+
+        ::
+
+            >>> from supriya.tools import ugentools
+            >>> in_ = ugentools.In.kr(bus=0)
+            >>> ugentools.Delay1.kr(
+            ...     source=in_,
+            ...     )
+            Delay1.ar()
+
+        Returns unit generator graph.
+        '''
+        from supriya.tools import synthdeftools
+        calculation_rate = synthdeftools.CalculationRate.AUDIO
+        ugen = cls._new(
+            calculation_rate=calculation_rate,
+            source=source,
+            )
+        return ugen
+
---
+++ supriya/tools/ugentools/Delay2.py
@@ -0,0 +1,68 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.ugentools.Delay1 import Delay1
+
+
+class Delay2(Delay1):
+    r'''Two-sample delay line unit generator.
+
+    ::
+
+        >>> from supriya.tools import ugentools
+        >>> source = ugentools.In.ar(bus=0)
+        >>> ugentools.Delay2.ar(source=source)
+        Delay2.ar()
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
+
+    ### PUBLIC METHODS ###
+
+    @classmethod
+    def ar(
+        cls,
+        delay_time=0.2,
+        maximum_delay_time=0.2,
+        source=None,
+        ):
+        r'''Create an audio-rate two-sample delay line.
+
+        ::
+
+            >>> from supriya.tools import ugentools
+            >>> in_ = ugentools.In.ar(bus=0)
+            >>> ugentools.Delay2.ar(
+            ...     source=in_,
+            ...     )
+            Delay2.ar()
+
+        Returns unit generator graph.
+        '''
+        return super(Delay2, cls).ar(
+            source=source,
+            )
+
+    @classmethod
+    def kr(
+        cls,
+        source=None,
+        ):
+        r'''Create a control-rate two-sample delay line.
+
+        ::
+
+            >>> from supriya.tools import ugentools
+            >>> in_ = ugentools.In.kr(bus=0)
+            >>> ugentools.Delay2.kr(
+            ...     source=in_,
+            ...     )
+            Delay2.ar()
+
+        Returns unit generator graph.
+        '''
+        return super(Delay2, cls).kr(
+            source=source,
+            )
+
