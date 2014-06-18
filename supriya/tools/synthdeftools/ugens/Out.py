7e319ef93d871162a1392940dde8eaa84b07a500
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-18 12:01:24 -0400

    Implemented ar() and kr() on Out.

--- supriya/tools/synthdeftools/ugens/Out.py
+++ supriya/tools/synthdeftools/ugens/Out.py
@@ -35,3 +35,33 @@ class Out(UGen):
 
     def _get_outputs(self):
         return []
+
+    ### PUBLIC METHODS ###
+
+    @classmethod
+    def ar(
+        cls,
+        bus=0,
+        source=None,
+        ):
+        from supriya.tools import synthdeftools
+        calculation_rate = synthdeftools.CalculationRate.AUDIO
+        return cls._new(
+            bus=bus,
+            calculation_rate=calculation_rate,
+            source=source,
+            )
+
+    @classmethod
+    def kr(
+        cls,
+        bus=0,
+        source=None,
+        ):
+        from supriya.tools import synthdeftools
+        calculation_rate = synthdeftools.CalculationRate.CONTROL
+        return cls._new(
+            bus=bus,
+            calculation_rate=calculation_rate,
+            source=source,
+            )
