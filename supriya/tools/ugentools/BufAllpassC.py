6af40a1d00d3601c246c6a3d13d14314d8f3da62
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-18 16:52:58 -0400

    Implemented BufAllpassN, BufAllpassL and BufAllpassC UGens.

---
+++ supriya/tools/ugentools/BufAllpassC.py
@@ -0,0 +1,97 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.ugentools.BufAllpassN import BufAllpassN
+
+
+class BufAllpassC(BufAllpassN):
+    r'''Buffer-based cubic-interpolating allpass delay line unit generator.
+
+    ::
+
+        >>> from supriya.tools import ugentools
+        >>> buffer_id = 0
+        >>> source = ugentools.In.ar(bus=0)
+        >>> ugentools.BufAllpassC.ar(
+        ...     buffer_id=buffer_id,
+        ...     source=source,
+        ...     )
+        BufAllpassC.ar()
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
+
+    ### PUBLIC METHODS ###
+
+    @classmethod
+    def ar(
+        cls,
+        buffer_id=None,
+        decay_time=1.0,
+        delay_time=0.2,
+        maximum_delay_time=0.2,
+        source=None,
+        ):
+        r'''Create an audio-rate buffer-based cubic-interpolating allpass
+        delay line.
+
+        ::
+
+            >>> from supriya.tools import ugentools
+            >>> buffer_id = 0
+            >>> source = ugentools.In.ar(bus=0)
+            >>> ugentools.BufAllpassC.ar(
+            ...     buffer_id=buffer_id,
+            ...     decay_time=1.0,
+            ...     delay_time=0.2,
+            ...     maximum_delay_time=0.2,
+            ...     source=source,
+            ...     )
+            BufAllpassC.ar()
+
+        Returns unit generator graph.
+        '''
+        return super(BufAllpassC, cls).ar(
+            buffer_id=buffer_id,
+            decay_time=decay_time,
+            delay_time=delay_time,
+            maximum_delay_time=maximum_delay_time,
+            source=source,
+            )
+
+    @classmethod
+    def kr(
+        cls,
+        buffer_id=None,
+        decay_time=1.0,
+        delay_time=0.2,
+        maximum_delay_time=0.2,
+        source=None,
+        ):
+        r'''Create a control-rate buffer-based cubic-interpolating allpass
+        delay line.
+
+        ::
+
+            >>> from supriya.tools import ugentools
+            >>> buffer_id = 0
+            >>> source = ugentools.In.kr(bus=0)
+            >>> ugentools.BufAllpassC.kr(
+            ...     buffer_id=buffer_id,
+            ...     decay_time=1.0,
+            ...     delay_time=0.2,
+            ...     maximum_delay_time=0.2,
+            ...     source=source,
+            ...     )
+            BufAllpassC.ar()
+
+        Returns unit generator graph.
+        '''
+        return super(BufAllpassC, cls).kr(
+            buffer_id=buffer_id,
+            decay_time=decay_time,
+            delay_time=delay_time,
+            maximum_delay_time=maximum_delay_time,
+            source=source,
+            )
