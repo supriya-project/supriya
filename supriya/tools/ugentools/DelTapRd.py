c7fbe549180f1171a70c45230a44784f02ead5fe
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-18 17:15:55 -0400

    Implemented DelTapRd and DelTapWr.

---
+++ supriya/tools/ugentools/DelTapRd.py
@@ -0,0 +1,110 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.synthdeftools.Argument import Argument
+from supriya.tools.synthdeftools.UGen import UGen
+
+
+class DelTapRd(UGen):
+    r'''Delay tap reader unit generator.
+
+    ::
+
+        >>> from supriya.tools import ugentools
+        >>> buffer_id = 0
+        >>> source = ugentools.SoundIn.ar(0)
+        >>> tapin = ugentools.DelTapWr.ar(
+        ...     buffer_id=buffer_id,
+        ...     source=source,
+        ...     )
+
+    ::
+
+        >>> tapin
+        DelTapWr.ar()
+
+    ::
+
+        >>> tapout = ugentools.DelTapRd.ar(
+        ...     buffer_id=buffer_id,
+        ...     phase=tapin,
+        ...     delay_time=0.1,
+        ...     interpolation=True,
+        ...     )
+
+    ::
+
+        >>> tapout
+        DelTapRd.ar()
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
+
+    _argument_specifications = (
+        Argument('buffer_id'),
+        Argument('phase'),
+        Argument('delay_time'),
+        Argument('interpolation'),
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        calculation_rate=None,
+        buffer_id=None,
+        phase=None,
+        delay_time=None,
+        interpolation=None,
+        ):
+        buffer_id = int(buffer_id)
+        interpolation = int(bool(interpolation))
+        UGen.__init__(
+            self,
+            calculation_rate=calculation_rate,
+            buffer_id=buffer_id,
+            phase=phase,
+            delay_time=delay_time,
+            interpolation=interpolation,
+            )
+
+    ### PUBLIC METHODS ###
+
+    @classmethod
+    def ar(
+        cls,
+        buffer_id=None,
+        phase=None,
+        delay_time=None,
+        interpolation=True,
+        ):
+        from supriya.tools import synthdeftools
+        calculation_rate = synthdeftools.CalculationRate.AUDIO
+        ugen = cls._new(
+            buffer_id=buffer_id,
+            calculation_rate=calculation_rate,
+            phase=phase,
+            delay_time=delay_time,
+            interpolation=interpolation,
+            )
+        return ugen
+
+    @classmethod
+    def kr(
+        cls,
+        buffer_id=None,
+        phase=None,
+        delay_time=None,
+        interpolation=True,
+        ):
+        from supriya.tools import synthdeftools
+        calculation_rate = synthdeftools.CalculationRate.SCALAR
+        ugen = cls._new(
+            buffer_id=buffer_id,
+            calculation_rate=calculation_rate,
+            phase=phase,
+            delay_time=delay_time,
+            interpolation=interpolation,
+            )
+        return ugen
---
+++ supriya/tools/ugentools/DelTapWr.py
@@ -0,0 +1,97 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.synthdeftools.Argument import Argument
+from supriya.tools.synthdeftools.UGen import UGen
+
+
+class DelTapWr(UGen):
+    r'''Delay tap writer unit generator.
+
+    ::
+
+        >>> from supriya.tools import ugentools
+        >>> buffer_id = 0
+        >>> source = ugentools.SoundIn.ar(0)
+        >>> tapin = ugentools.DelTapWr.ar(
+        ...     buffer_id=buffer_id,
+        ...     source=source,
+        ...     )
+
+    ::
+
+        >>> tapin
+        DelTapWr.ar()
+
+    ::
+
+        >>> tapout = ugentools.DelTapRd.ar(
+        ...     buffer_id=buffer_id,
+        ...     phase=tapin,
+        ...     delay_time=0.1,
+        ...     interpolation=True,
+        ...     )
+
+    ::
+
+        >>> tapout
+        DelTapRd.ar()
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
+
+    _argument_specifications = (
+        Argument('buffer_id'),
+        Argument('source'),
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        buffer_id=None,
+        calculation_rate=None,
+        source=None,
+        ):
+        buffer_id = int(buffer_id)
+        UGen.__init__(
+            self,
+            buffer_id=buffer_id,
+            calculation_rate=calculation_rate,
+            source=source,
+            )
+
+    ### PUBLIC METHODS ###
+
+    @classmethod
+    def ar(
+        cls,
+        buffer_id=None,
+        source=None,
+        ):
+        from supriya.tools import synthdeftools
+        calculation_rate = synthdeftools.CalculationRate.AUDIO
+        source = cls.as_audio_rate_input(source)
+        ugen = cls._new(
+            buffer_id=buffer_id,
+            calculation_rate=calculation_rate,
+            source=source,
+            )
+        return ugen
+
+    @classmethod
+    def kr(
+        cls,
+        buffer_id=None,
+        source=None,
+        ):
+        from supriya.tools import synthdeftools
+        calculation_rate = synthdeftools.CalculationRate.CONTROL
+        source = cls.as_audio_rate_input(source)
+        ugen = cls._new(
+            buffer_id=buffer_id,
+            calculation_rate=calculation_rate,
+            source=source,
+            )
+        return ugen
