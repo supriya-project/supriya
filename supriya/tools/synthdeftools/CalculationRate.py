d2fc86a9f873124b34adbdf602e4aaea515275d2
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-18 23:46:44 -0400

    Implemented CalculationRate.from_collection() and from_input().

--- supriya/tools/synthdeftools/CalculationRate.py
+++ supriya/tools/synthdeftools/CalculationRate.py
@@ -24,3 +24,48 @@ class CalculationRate(Enumeration):
     CONTROL = 1
     DEMAND = 3
     SCALAR = 0
+
+    ### PUBLIC METHODS ###
+
+    @staticmethod
+    def from_collection(collection):
+        r'''Gets calculation rate from a collection.
+
+        ::
+
+            >>> from supriya.tools import synthdeftools
+
+        ::
+
+            >>> collection = []
+            >>> collection.append(synthdeftools.DC.ar(0))
+            >>> collection.append(synthdeftools.DC.kr(1))
+            >>> collection.append(2.0)
+            >>> synthdeftools.CalculationRate.from_collection(collection)
+            <CalculationRate.AUDIO: 2>
+
+        ::
+            >>> collection = []
+            >>> collection.append(synthdeftools.DC.kr(1))
+            >>> collection.append(2.0)
+            >>> synthdeftools.CalculationRate.from_collection(collection)
+            <CalculationRate.CONTROL: 1>
+
+        Return calculation rate.
+        '''
+        rates = [
+            CalculationRate.from_input(item) for item in collection
+            ]
+        maximum_rate = max(rates)
+        return maximum_rate
+
+    @staticmethod
+    def from_input(input_):
+        from supriya.tools import synthdeftools
+        prototype = (synthdeftools.OutputProxy, synthdeftools.UGen)
+        if isinstance(input_, (int, float)):
+            return CalculationRate.SCALAR
+        elif isinstance(input_, prototype):
+            return input_.calculation_rate
+        raise ValueError(input_)
+
