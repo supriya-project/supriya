2428f28211f4493d63f13267233cc6ffc63b0615
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-19 18:04:42 -0400

    BufferResponseCallback now knows about sets.

--- supriya/tools/responsetools/BufferResponseCallback.py
+++ supriya/tools/responsetools/BufferResponseCallback.py
@@ -27,12 +27,15 @@ class BufferResponseCallback(OscCallback):
     ### SPECIAL METHODS ###
 
     def __call__(self, message):
+        #print(message)
         response = self._response_manager(message)
         if not isinstance(response, tuple):
             response = (response,)
         for x in response:
+            #print(x)
             buffer_id = x.buffer_id
-            buffer_ = self._server._buffers.get(buffer_id)
-            if buffer_ is None:
+            buffers = self._server._buffers.get(buffer_id)
+            if not buffers:
                 continue
-            buffer_.handle_response(x)
+            for buffer_ in buffers:
+                buffer_.handle_response(x)
