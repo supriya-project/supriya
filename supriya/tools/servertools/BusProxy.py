43fc0acafa437437d4ad3f44cebaa56aa492fd89
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-20 16:53:43 -0400

    Reimplemented BusProxy, mirroring BufferProxy.

--- supriya/tools/servertools/BusProxy.py
+++ supriya/tools/servertools/BusProxy.py
@@ -7,22 +7,29 @@ class BusProxy(SupriyaObject):
     ### CLASS VARIABLES ###
 
     __slots__ = (
-        '_bus',
-        '_index',
+        '_bus_id'
+        '_calculation_rate',
+        '_server',
         )
 
     ### INITIALIZER ###
 
-    def __init__(self,
-        bus=None,
-        index=None,
+    def __init__(
+        self,
+        bus_id=None,
+        calculation_rate=None,
+        server=None,
         ):
         from supriya.tools import servertools
-        assert isinstance(bus, servertools.Bus)
-        index = int(index)
-        assert 0 <= index <= bus.channel_count
-        self._bus = bus
-        self._index = index
+        from supriya.tools import synthdeftools
+        bus_id = int(bus_id)
+        assert 0 <= bus_id
+        calculation_rate = synthdeftools.CalculationRate.from_expr(
+            calculation_rate)
+        assert isinstance(server, servertools.Server)
+        self._bus_id = int(bus_id)
+        self._calculation_rate = calculation_rate
+        self._server = server
 
     ### SPECIAL METHODS ###
 
@@ -44,31 +51,36 @@ class BusProxy(SupriyaObject):
     def __str__(self):
         return self.map_symbol
 
+    ### PUBLIC METHODS ###
+
+    def handle_response(self, response):
+        from supriya.tools import responsetools
+        assert response.buffer_id == self.buffer_id
+        if isinstance(response, responsetools.ControlBusSetContiguousResponse):
+            pass
+        elif isinstance(response, responsetools.ControlBusSetResponse):
+            pass
+
     ### PUBLIC PROPERTIES ###
 
     @property
-    def bus(self):
-        return self._bus
-
-    @property
     def bus_id(self):
-        if self.bus.bus_id is None:
-            return None
-        return self.bus.bus_id + self.index
+        return self._bus_id
 
     @property
     def calculation_rate(self):
-        return self.bus.calculation_rate
-
-    @property
-    def index(self):
-        return self._index
+        return self._calculation_rate
 
     @property
     def map_symbol(self):
-        from supriya.tools import servertools
-        assert self.bus_id is not None
-        return servertools.Bus._as_map(
-            bus_id=self.bus_id,
-            calculation_rate=self.calculation_rate,
-            )
+        from supriya.tools import synthdeftools
+        if self.calculation_rate == synthdeftools.CalculationRate.AUDIO:
+            map_symbol = 'a'
+        else:
+            map_symbol = 'c'
+        map_symbol += str(self.bus_id)
+        return map_symbol
+
+    @property
+    def server(self):
+        return self._server
