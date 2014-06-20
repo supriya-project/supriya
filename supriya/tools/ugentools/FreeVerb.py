8846334d18a547f535a3337f0c9702fb5ab97353
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-20 11:45:11 -0400

    Modernized FreeVerb UGen.

--- supriya/tools/ugentools/FreeVerb.py
+++ supriya/tools/ugentools/FreeVerb.py
@@ -3,6 +3,18 @@ from supriya.tools.synthdeftools.UGen import UGen
 
 
 class FreeVerb(UGen):
+    r'''FreeVerb reverb unit generator.
+
+    ::
+
+        >>> from supriya.tools import ugentools
+        >>> source = ugentools.SoundIn.ar()
+        >>> ugentools.FreeVerb.ar(
+        ...     source=source,
+        ...     )
+        FreeVerb.ar()
+
+    '''
 
     ### CLASS VARIABLES ###
 
@@ -33,3 +45,39 @@ class FreeVerb(UGen):
             room_size=room_size,
             source=source,
             )
+
+    ### PUBLIC METHODS ###
+
+    @classmethod
+    def ar(
+        cls,
+        damping=0.5,
+        mix=0.33,
+        room_size=0.5,
+        source=None,
+        ):
+        r'''Creates an audio-rate FreeVerb reverb unit.
+
+        ::
+
+            >>> from supriya.tools import ugentools
+            >>> source = ugentools.SoundIn.ar(bus=0)
+            >>> ugentools.FreeVerb.ar(
+            ...     damping=0.5,
+            ...     mix=0.33,
+            ...     room_size=0.5,
+            ...     source=source,
+            ...     )
+            FreeVerb.ar()
+
+        '''
+        from supriya.tools import synthdeftools
+        calculation_rate = synthdeftools.CalculationRate.AUDIO
+        ugen = cls._new(
+            calculation_rate=calculation_rate,
+            damping=damping,
+            mix=mix,
+            room_size=room_size,
+            source=source,
+            )
+        return ugen
