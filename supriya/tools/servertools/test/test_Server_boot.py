5da46070f417ea9122753387819e48c74e86b416
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-20 15:34:31 -0400

    Simplified Server.boot() test.

--- supriya/tools/servertools/test/test_Server_boot.py
+++ supriya/tools/servertools/test/test_Server_boot.py
@@ -4,7 +4,7 @@ from supriya import servertools
 def test_Server_boot():
 
     server = servertools.Server(port=57757)
-    for i in range(10):
+    for i in range(4):
         assert not server.is_running
         server.boot()
         assert server.is_running
