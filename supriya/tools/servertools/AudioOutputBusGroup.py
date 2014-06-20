da9234824e8469716071dc73e9be714fb020bd6f
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-06-20 17:59:31 -0400

    Added AudioInputBusGroup and AudioOutputBusGroup to servertools.

---
+++ supriya/tools/servertools/AudioOutputBusGroup.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.servertools.BusGroup import BusGroup
+
+
+class AudioOutputBusGroup(BusGroup):
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        server,
+        ):
+        from supriya.tools import servertools
+        from supriya.tools import synthdeftools
+        assert isinstance(server, servertools.Server)
+        assert server.is_running
+        self._server = server
+        bus_id = 0
+        bus_count = server.server_options.input_bus_channel_count
+        calculation_rate = synthdeftools.CalculationRate.AUDIO
+        BusGroup.__init__(
+            self,
+            bus_count=bus_count,
+            calculation_rate=calculation_rate,
+            )
+        self._bus_id = bus_id
+
+    ### PUBLIC METHODS ###
+
+    def allocate(self):
+        pass
+
+    def free(self):
+        pass
