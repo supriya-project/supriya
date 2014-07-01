45e9f250894ae6891393ca1ae6dd42ebfaa7de79
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-01 12:42:01 -0400

    Fleshed out SynthControlGroup.

--- supriya/tools/servertools/SynthControlGroup.py
+++ supriya/tools/servertools/SynthControlGroup.py
@@ -8,46 +8,43 @@ class SynthControlGroup(SupriyaObject, collections.Mapping):
     ### CLASS VARIABLES ###
 
     __slots__ = (
+        '_synthdef',
         '_synth_controls',
+        '_synth_control_map',
         )
 
     ### INITIALIZER ###
 
     def __init__(
         self,
-        synth_controls=None,
+        synthdef,
         ):
         from supriya.tools import servertools
-        synth_controls = synth_controls or ()
-        prototype = servertools.SynthControl
-        assert all(isinstance(x, prototype) for x in synth_controls)
-        synth_controls = dict(
-            (synth_control.name, synth_control)
-            for synth_control in synth_controls
-            )
-        self._synth_controls = synth_controls
+        synth_controls = []
+        synth_control_map = collections.OrderedDict()
+        for parameter in synthdef.parameters:
+            synth_control = servertools.SynthControl.from_parameter(parameter)
+            synth_controls.append(synth_control)
+            synth_control_map[synth_control.name] = synth_control
+        self._synth_controls = tuple(synth_controls)
+        self._synth_control_map = synth_control_map
 
     ### SPECIAL METHODS ###
 
     def __getitem__(self, item):
-        return self._synth_controls[item]
-
-    def __len__(self):
-        return len(self._synth_controls)
-
-    def __setitem__(self, item, value):
-        synth_control = self._synth_controls[item]
-        synth_control.value = value
-
-    ### PUBLIC METHODS ###
-
-    @staticmethod
-    def from_synthdef(synthdef):
-        from supriya.tools import synthdeftools
-        assert isinstance(synthdef, synthdeftools.SynthDef)
+        if isinstance(item, (int, slice)):
+            return self._synth_controls[item]
+        elif isinstance(item, str):
+            return self._synth_control_map[item]
+        elif isinstance(item, tuple):
+            result = []
+            for x in item:
+                result.append(self.__getitem__(x))
+            return tuple(result)
+        return ValueError(item)
 
     ### PUBLIC PROPERTIES ###
 
     @property
-    def synth_controls(self):
-        return self._synth_controls.copy()
+    def synthdef(self):
+        return self._synthdef
