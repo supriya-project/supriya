666b8c33d5eb82fad47488619c19c03a6b44dbe4
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-22 17:19:32 -0400

    Streamlined Clock.

--- supriya/tools/clocktools/Clock.py
+++ supriya/tools/clocktools/Clock.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+from __future__ import print_function
 import time
 import threading
 from supriya.tools.systemtools.SupriyaObject import SupriyaObject
@@ -12,40 +13,39 @@ class Clock(SupriyaObject):
         '_lock',
         '_next_event_time',
         '_queue',
+        '_start_time',
         '_timer',
         )
 
     ### INITIALIZER ###
 
     def __init__(self):
+        self._start_time = time.time()
         self._lock = threading.RLock()
-        self._next_event_time = 0
+        self._next_event_time = -1
         self._queue = []
         self._timer = None
 
     ### PRIVATE METHODS ###
 
     def _execute(self, scheduled_time):
-        print('TRYING TO EXECUTE')
         if scheduled_time != self._next_event_time:
             return
+        pairs = []
         with self._lock:
-            now = time.time()
-            events = []
             while self._queue and self._queue[0][0] <= scheduled_time:
                 pair = self._queue.pop(0)
-                events.append(pair[1])
-            for event in events:
-                print(event)
-                event(self, now)
+                pairs.append(pair)
+            for scheduled_time, event in pairs:
+                reschedule_time = event()
+                if reschedule_time is not None:
+                    self.schedule(event, reschedule_time)
             self._new_timer()
 
     def _new_timer(self):
-        print('NEW TIMER')
-        now = time.time()
         scheduled_time = self._queue[0][0]
+        now = time.time()
         delta = scheduled_time - now
-        print('DELTA', delta)
         self._next_event_time = scheduled_time
         self._timer = threading.Timer(
             delta,
@@ -56,20 +56,23 @@ class Clock(SupriyaObject):
 
     ### PUBLIC METHODS ###
 
+    def reset(self):
+        with self._lock:
+            if self._timer is not None:
+                self._timer.cancel()
+                self._timer = None
+            self._queue[:] = []
+            self._next_event_time = 0
+
     def schedule(self, event, relative_time):
         absolute_time = time.time() + relative_time
         self.schedule_absolutely(event, absolute_time)
 
     def schedule_absolutely(self, event, absolute_time):
-        from supriya.tools import clocktools
-        event = clocktools.Event(
-            scheduled_time=absolute_time,
-            payload=event,
-            )
-        now = time.time()
-        if absolute_time <= now:
-            event(self, now)
-            return
+        if absolute_time <= time.time():
+            reschedule_time = event()
+            if reschedule_time is not None:
+                self.schedule(event, reschedule_time)
         with self._lock:
             if absolute_time < self._next_event_time or not self._queue:
                 if self._timer is not None:
