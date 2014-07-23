52b8070e22972f491ce2daede94093f2e0e8d1fd
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-23 11:20:53 -0400

    Added RHPF and RLPF UGens.

---
+++ supriya/tools/ugentools/RLFP.py
@@ -0,0 +1,108 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.ugentools.Filter import Filter
+
+
+class RLPF(Filter):
+    r'''Resonant lowpass filter unit generator.
+
+    ::
+
+        >>> from supriya.tools import ugentools
+        >>> source = ugentools.In.ar(bus=0)
+        >>> ugentools.RLPF.ar(source=source)
+        RLPF.ar()
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
+
+    _ordered_input_names = (
+        'source',
+        'frequency',
+        'reciprocal_of_q',
+        )
+
+    ### PUBLIC METHODS ###
+
+    def __init__(
+        self,
+        frequency=440,
+        rate=None,
+        reciprocal_of_q=1.0,
+        source=None,
+        ):
+        Filter.__init__(
+            self,
+            frequency=frequency,
+            rate=rate,
+            reciprocal_of_q=reciprocal_of_q,
+            source=source,
+            )
+
+    ### PUBLIC METHODS ###
+
+    @classmethod
+    def ar(
+        cls,
+        frequency=440,
+        reciprocal_of_q=1.0,
+        source=None,
+        ):
+        r'''Creates an audio-rate resonant lowpass filter.
+
+        ::
+
+            >>> from supriya.tools import ugentools
+            >>> source = ugentools.In.ar(bus=0)
+            >>> ugentools.RLPF.ar(
+            ...     frequency=440,
+            ...     reciprocal_of_q=1.0,
+            ...     source=source,
+            ...     )
+            RLPF.ar()
+
+        Returns unit generator graph.
+        '''
+        from supriya.tools import synthdeftools
+        rate = synthdeftools.Rate.AUDIO
+        ugen = cls._new_expanded(
+            frequency=frequency,
+            rate=rate,
+            reciprocal_of_q=reciprocal_of_q,
+            source=source,
+            )
+        return ugen
+
+    @classmethod
+    def kr(
+        cls,
+        frequency=440,
+        reciprocal_of_q=1.0,
+        source=None,
+        ):
+        r'''Creates a control-rate resonant lowpass filter.
+
+        ::
+
+            >>> from supriya.tools import ugentools
+            >>> source = ugentools.In.kr(bus=0)
+            >>> ugentools.RLPF.kr(
+            ...     frequency=440,
+            ...     reciprocal_of_q=1.0,
+            ...     source=source,
+            ...     )
+            RLPF.kr()
+
+        Returns unit generator graph.
+        '''
+        from supriya.tools import synthdeftools
+        rate = synthdeftools.Rate.CONTROL
+        ugen = cls._new_expanded(
+            frequency=frequency,
+            rate=rate,
+            reciprocal_of_q=reciprocal_of_q,
+            source=source,
+            )
+        return ugen
