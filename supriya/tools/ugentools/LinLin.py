ecbf95e31bf4f8e1b37b25b78959a02c45fd2cba
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-23 11:57:33 -0400

    Added LinLin pseudo-UGen.

---
+++ supriya/tools/ugentools/LinLin.py
@@ -0,0 +1,49 @@
+# -*- encoding: utf-8 -*-
+import abc
+from supriya.tools.ugentools.PseudoUGen import PseudoUGen
+
+
+class LinLin(PseudoUGen):
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
+
+    ### INITIALIZER ###
+
+    @abc.abstractmethod
+    def __init__(self):
+        raise NotImplementedError
+
+    ### PUBLIC METHODS ###
+
+    @staticmethod
+    def ar(
+        source=None,
+        in_min=0.0,
+        in_max=1.0,
+        out_min=1.0,
+        out_max=2.0,
+        ):
+        from supriya.tools import ugentools
+        scale = (out_max - out_min) / (in_max - in_min)
+        offset = out_min - (scale * in_min)
+        ugen = ugentools.MulAdd(
+            source=source,
+            multiplier=scale,
+            addend=offset,
+            )
+        return ugen
+
+    @staticmethod
+    def kr(
+        source=None,
+        in_min=0.0,
+        in_max=1.0,
+        out_min=1.0,
+        out_max=2.0,
+        ):
+        scale = (out_max - out_min) / (in_max - in_min)
+        offset = out_min - (scale * in_min)
+        ugen = (source * scale) + offset
+        return ugen
