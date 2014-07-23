84c77670f3f5d1aa9416fe8603c49a1ef0701a2f
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-23 13:12:34 -0400

    Added CompanderD pseudo-UGen.

---
+++ supriya/tools/ugentools/CompanderD.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.ugentools.PseudoUGen import PseudoUGen
+
+
+class CompanderD(PseudoUGen):
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
+
+    ### PUBLIC METHODS ###
+
+    @classmethod
+    def ar(
+        cls,
+        source=0.,
+        thresh=0.5,
+        clamp_time=0.01,
+        relax_time=0.1,
+        slope_above=1.,
+        slope_below=1.,
+        ):
+        from supriya.tools import synthdeftools
+        from supriya.tools import ugentools
+        rate = synthdeftools.Rate.AUDIO
+        control = ugentools.DelayN.ar(
+            source=source,
+            maximum_delay_time=clamp_time,
+            delay_time=clamp_time,
+            )
+        ugen = ugentools.Compander._new_expanded(
+            clamp_time=clamp_time,
+            rate=rate,
+            relax_time=relax_time,
+            slope_above=slope_above,
+            slope_below=slope_below,
+            source=source,
+            control=control,
+            thresh=thresh,
+            )
+        return ugen
