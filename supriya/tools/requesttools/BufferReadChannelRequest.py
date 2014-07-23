89d0ce95bd99c107cbcc6d9429f3432a7acc7189
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-23 17:13:59 -0400

    Streamlined requesttools.

--- supriya/tools/requesttools/BufferReadChannelRequest.py
+++ supriya/tools/requesttools/BufferReadChannelRequest.py
@@ -14,15 +14,12 @@ class BufferReadChannelRequest(Request):
     def __init__(
         self,
         ):
-        pass
+        raise NotImplementedError
 
     ### PUBLIC METHODS ###
 
     def to_osc_message(self):
-        from supriya.tools import requesttools
-        manager = requesttools.RequestManager
-        message = manager.make_buffer_read_channel_message()
-        return message
+        raise NotImplementedError
 
     ### PUBLIC PROPERTIES ###
 
