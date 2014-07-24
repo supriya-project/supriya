94dd186c48ad667edb6d5348b5fb53b1cf525484
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-24 15:41:40 -0400

    Implemented DumpOscRequest.

--- supriya/tools/requesttools/DumpOscRequest.py
+++ supriya/tools/requesttools/DumpOscRequest.py
@@ -1,27 +1,64 @@
 # -*- encoding: utf-8 -*-
+from supriya.tools import osctools
 from supriya.tools.requesttools.Request import Request
 
 
 class DumpOscRequest(Request):
+    r'''A /dumpOSC request.
+
+    ::
+
+        >>> from supriya.tools import requesttools
+        >>> request = requesttools.DumpOscRequest(1)
+        >>> request
+        DumpOscRequest(
+            osc_status=1
+            )
+
+    ::
+
+        >>> message = request.to_osc_message()
+        >>> message
+        OscMessage(39, 1)
+
+    ::
+
+        >>> message.address == requesttools.RequestId.DUMP_OSC
+        True
+
+    '''
 
     ### CLASS VARIABLES ###
 
     __slots__ = (
+        '_osc_status',
         )
 
     ### INITIALIZER ###
 
     def __init__(
         self,
+        osc_status=None,
         ):
-        raise NotImplementedError
+        self._osc_status = int(osc_status)
 
     ### PUBLIC METHODS ###
 
     def to_osc_message(self):
-        raise NotImplementedError
+        request_id = int(self.request_id)
+        osc_status = int(self.osc_status)
+        assert 0 <= osc_status <= 4
+        message = osctools.OscMessage(
+            request_id,
+            osc_status,
+            )
+        return message
 
     ### PUBLIC PROPERTIES ###
+
+    @property
+    def osc_status(self):
+        return self._osc_status
 
     @property
     def response_prototype(self):
