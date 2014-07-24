9941166c73d55bde9a678104cbe24ed3d8f9d5a1
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-24 16:39:57 -0400

    Implemented NodeFreeRequest.

--- supriya/tools/requesttools/NodeFreeRequest.py
+++ supriya/tools/requesttools/NodeFreeRequest.py
@@ -1,27 +1,65 @@
 # -*- encoding: utf-8 -*-
+from supriya.tools import osctools
 from supriya.tools.requesttools.Request import Request
 
 
 class NodeFreeRequest(Request):
+    r'''A /n_free request.
+
+    ::
+
+        >>> from supriya.tools import requesttools
+        >>> request = requesttools.NodeFreeRequest(
+        ...     node_id=1000,
+        ...     )
+        >>> request
+        NodeFreeRequest(
+            node_id=1000
+            )
+
+    ::
+
+        >>> message = request.to_osc_message()
+        >>> message
+        OscMessage(11, 1000)
+
+    ::
+
+        >>> message.address == requesttools.RequestId.NODE_FREE
+        True
+
+    '''
 
     ### CLASS VARIABLES ###
 
     __slots__ = (
+        '_node_id',
         )
 
     ### INITIALIZER ###
 
     def __init__(
         self,
+        node_id=None
         ):
-        raise NotImplementedError
+        self._node_id = node_id
 
     ### PUBLIC METHODS ###
 
     def to_osc_message(self):
-        raise NotImplementedError
+        request_id = int(self.request_id)
+        node_id = int(self.node_id)
+        message = osctools.OscMessage(
+            request_id,
+            node_id,
+            )
+        return message
 
     ### PUBLIC PROPERTIES ###
+
+    @property
+    def node_id(self):
+        return self._node_id
 
     @property
     def response_prototype(self):
