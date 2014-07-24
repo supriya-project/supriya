96c3876d9dd0a14c36a41574bd400bc6f8f0a360
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-24 15:00:11 -0400

    Implemented ControlBusGetContiguousRequest.

--- supriya/tools/requesttools/ControlBusGetContiguousRequest.py
+++ supriya/tools/requesttools/ControlBusGetContiguousRequest.py
@@ -1,27 +1,86 @@
 # -*- encoding: utf-8 -*-
+from supriya.tools import osctools
 from supriya.tools.requesttools.Request import Request
 
 
 class ControlBusGetContiguousRequest(Request):
+    r'''A /c_getn request.
+
+    ::
+
+        >>> from supriya.tools import requesttools
+        >>> request = requesttools.ControlBusGetContiguousRequest(
+        ...     index_count_pairs=[
+        ...         (0, 2),
+        ...         (4, 2),
+        ...         (8, 2),
+        ...         (12, 2),
+        ...         ],
+        ...     )
+        >>> request
+        ControlBusGetContiguousRequest(
+            index_count_pairs=(
+                (0, 2),
+                (4, 2),
+                (8, 2),
+                (12, 2),
+                )
+            )
+
+    ::
+
+        >>> message = request.to_osc_message()
+        >>> message
+        OscMessage(41, 0, 2, 4, 2, 8, 2, 12, 2)
+
+    ::
+
+        >>> message.address == \
+        ...     requesttools.RequestId.CONTROL_BUS_GET_CONTIGUOUS
+        True
+
+    '''
 
     ### CLASS VARIABLES ###
 
     __slots__ = (
+        '_index_count_pairs',
         )
 
     ### INITIALIZER ###
 
     def __init__(
         self,
+        index_count_pairs=None,
         ):
-        raise NotImplementedError
+        if index_count_pairs:
+            pairs = []
+            for index, count in index_count_pairs:
+                index = int(index)
+                count = int(count)
+                assert 0 <= index
+                assert 0 < count
+                pair = (index, count)
+                pairs.append(pair)
+            index_count_pairs = tuple(pairs)
+        self._index_count_pairs = index_count_pairs
 
     ### PUBLIC METHODS ###
 
     def to_osc_message(self):
-        raise NotImplementedError
+        request_id = int(self.request_id)
+        contents = [request_id]
+        if self.index_count_pairs:
+            for pair in self.index_count_pairs:
+                contents.extend(pair)
+        message = osctools.OscMessage(*contents)
+        return message
 
     ### PUBLIC PROPERTIES ###
+
+    @property
+    def index_count_pairs(self):
+        return self._index_count_pairs
 
     @property
     def response_prototype(self):
