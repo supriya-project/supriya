471069698dbbb1bb4b44076336ce26db38525beb
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-24 14:16:38 -0400

    Added Saw UGen.

---
+++ supriya/tools/ugentools/Saw.py
@@ -0,0 +1,94 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.ugentools.PureUGen import PureUGen
+
+
+class Saw(PureUGen):
+    r'''A band-limited sawtooth oscillator unit generator.
+
+    ::
+
+        >>> from supriya.tools import ugentools
+        >>> ugentools.Saw.ar()
+        Saw.ar()
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
+
+    _ordered_input_names = (
+        'frequency',
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        rate=None,
+        frequency=440.,
+        ):
+        PureUGen.__init__(
+            self,
+            rate=rate,
+            frequency=frequency,
+            )
+
+    ### PUBLIC METHODS ###
+
+    @classmethod
+    def ar(
+        cls,
+        frequency=440,
+        ):
+        r'''Creates an audio-rate band-limited sawtooth oscillator.
+
+        ::
+
+            >>> from supriya.tools import ugentools
+            >>> ugentools.Saw.ar(
+            ...     frequency=443,
+            ...     )
+            Saw.ar()
+
+        Returns unit generator graph.
+        '''
+        from supriya.tools import synthdeftools
+        rate = synthdeftools.Rate.AUDIO
+        ugen = cls._new_expanded(
+            rate=rate,
+            frequency=frequency,
+            )
+        return ugen
+
+    @classmethod
+    def kr(
+        cls,
+        frequency=440,
+        ):
+        r'''Creates a control-rate band-limited sawtooth oscillator.
+
+        ::
+
+            >>> from supriya.tools import ugentools
+            >>> ugentools.Saw.kr(
+            ...     frequency=443,
+            ...     )
+            Saw.kr()
+
+        Returns unit generator graph.
+        '''
+        from supriya.tools import synthdeftools
+        rate = synthdeftools.Rate.CONTROL
+        ugen = cls._new_expanded(
+            rate=rate,
+            frequency=frequency,
+            )
+        return ugen
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def frequency(self):
+        index = self._ordered_input_names.index('frequency')
+        return self._inputs[index]
