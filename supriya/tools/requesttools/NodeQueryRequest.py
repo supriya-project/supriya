ec2c011086e32af42047549e0e3d3818633f5efa
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-25 13:54:15 -0400

    Implemented NodeQueryRequest.

--- supriya/tools/requesttools/NodeQueryRequest.py
+++ supriya/tools/requesttools/NodeQueryRequest.py
@@ -1,27 +1,66 @@
 # -*- encoding: utf-8 -*-
+from supriya.tools import osctools
 from supriya.tools.requesttools.Request import Request
 
 
 class NodeQueryRequest(Request):
+    r'''A /n_query request.
+
+    ::
+
+        >>> from supriya.tools import requesttools
+        >>> request = requesttools.NodeQueryRequest(
+        ...     node_id=1000,
+        ...     )
+        >>> request
+        NodeQueryRequest(
+            node_id=1000
+            )
+
+    ::
+
+        >>> message = request.to_osc_message()
+        >>> message
+        OscMessage(46, 1000)
+
+    ::
+
+        >>> message.address == requesttools.RequestId.NODE_QUERY
+        True
+
+    '''
+
 
     ### CLASS VARIABLES ###
 
     __slots__ = (
+        '_node_id',
         )
 
     ### INITIALIZER ###
 
     def __init__(
         self,
+        node_id=None
         ):
-        raise NotImplementedError
+        self._node_id = node_id
 
     ### PUBLIC METHODS ###
 
     def to_osc_message(self):
-        raise NotImplementedError
+        request_id = int(self.request_id)
+        node_id = int(self.node_id)
+        message = osctools.OscMessage(
+            request_id,
+            node_id,
+            )
+        return message
 
     ### PUBLIC PROPERTIES ###
+
+    @property
+    def node_id(self):
+        return self._node_id
 
     @property
     def response_specification(self):
