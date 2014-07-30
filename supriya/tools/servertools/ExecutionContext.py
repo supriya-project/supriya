0c4babc393613432d8458264de48d45126858e69
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-30 13:55:11 -0400

    ExecutionContext now handles syncing more elegantly.

--- supriya/tools/servertools/ExecutionContext.py
+++ supriya/tools/servertools/ExecutionContext.py
@@ -3,6 +3,7 @@ class ExecutionContext(object):
     __slots__ = (
         '_messages',
         '_server',
+        '_sync',
         '_timestamp',
         )
 
@@ -17,34 +18,50 @@ class ExecutionContext(object):
         from supriya.tools import servertools
         server = server or servertools.Server()
         assert isinstance(server, servertools.Server)
+        self._messages = []
         self._server = server
+        self._sync = bool(sync)
+        if timestamp is not None:
+            timestamp = float(timestamp)
         self._timestamp = timestamp
 
     ### SPECIAL METHODS ###
 
     def __enter__(self):
-        self._message = []
+        self._messages = []
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         from supriya.tools import osctools
+        from supriya.tools import requesttools
         messages = []
         prototype = (osctools.OscMessage, osctools.OscBundle)
-        for message in self.messages:
+        for message in self._messages:
             if not isinstance(message, prototype):
-                message = message.as_osc_message()
+                message = message.to_osc_message()
+            messages.append(message)
+        if self._sync:
+            sync_id = self._server.next_sync_id
+            sync_request = requesttools.SyncRequest(
+                sync_id=sync_id,
+                )
+            message = sync_request.to_osc_message()
             messages.append(message)
         osc_bundle = osctools.OscBundle(
-            timestamp=self.timestamp,
+            timestamp=self._timestamp,
             contents=messages,
             )
-        self.server.send_message(osc_bundle)
-        if self.sync:
-            self.server.sync()
+        if self._sync:
+            sync_request.communicate(
+                message=osc_bundle,
+                server=self._server,
+                )
+        else:
+            self._server.send_message(osc_bundle)
 
     ### PUBLIC METHODS ###
 
-    def send_message(self, request):
+    def send_message(self, message):
         from supriya.tools import osctools
         from supriya.tools import requesttools
         prototype = (
@@ -52,8 +69,8 @@ class ExecutionContext(object):
             osctools.OscBundle,
             requesttools.Request,
             )
-        assert isinstance(request, prototype)
-        self._requests.append(request)
+        assert isinstance(message, prototype)
+        self._messages.append(message)
 
-    def sync(self):
-        self._sync = True
+    def sync(self, sync=True):
+        self._sync = bool(sync)
