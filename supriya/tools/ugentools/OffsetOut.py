ca2acbda86e306be01bb7341b2ec256115b88b15
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-02 21:02:15 +0200

    Added OffsetOut UGen.

---
+++ supriya/tools/ugentools/OffsetOut.py
@@ -0,0 +1,73 @@
+# -*- encoding: utf-8 -*-
+import collections
+from supriya.tools.synthdeftools.UGen import UGen
+
+
+class OffsetOut(UGen):
+    r'''A bus output unit generator with sample-accurate timing.
+
+    ::
+
+        >>> from supriya.tools import ugentools
+        >>> source = ugentools.SinOsc.ar()
+        >>> ugentools.OffsetOut.ar(
+        ...     bus=0,
+        ...     source=source,
+        ...     )
+        OffsetOut.ar()
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
+
+    _ordered_input_names = (
+        'bus',
+        )
+
+    _unexpanded_input_names = (
+        'source',
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        rate=None,
+        bus=0,
+        source=None,
+        ):
+        UGen.__init__(
+            self,
+            bus=bus,
+            rate=rate,
+            )
+        if not isinstance(source, collections.Sequence):
+            source = [source]
+        for single_source in source:
+            self._configure_input('source', single_source)
+
+    ### PRIVATE METHODS ###
+
+    def _get_outputs(self):
+        return []
+
+    ### PUBLIC METHODS ###
+
+    @classmethod
+    def ar(
+        cls,
+        bus=0,
+        source=None,
+        ):
+        from supriya.tools import servertools
+        from supriya.tools import synthdeftools
+        rate = synthdeftools.Rate.AUDIO
+        if isinstance(bus, servertools.BusMixin):
+            bus = int(bus)
+        return cls._new_expanded(
+            bus=bus,
+            rate=rate,
+            source=source,
+            )
