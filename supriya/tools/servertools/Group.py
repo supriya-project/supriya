f0f4d65ceaffba9c5e7f56f67a87a0d77a5e7894
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-01 23:53:18 -0400

    Made first-draft implementation of Group.__setitem__().

--- supriya/tools/servertools/Group.py
+++ supriya/tools/servertools/Group.py
@@ -66,7 +66,79 @@ class Group(Node):
         return len(self._children)
 
     def __setitem__(self, i, expr):
+        from supriya.tools import requesttools
+        from supriya.tools import servertools
+
+        assert self.is_allocated
         raise NotImplementedError
+
+        if isinstance(i, int):
+            if i < 0:
+                i = len(self) + i
+            i = slice(i, i + 1)
+            expr = [expr]
+        assert all(isinstance(_, servertools.Node) for _ in expr)
+        start, stop, _ = i.indices(len(self))
+
+        del(self[start:stop])
+        self._children.__setitem__(slice(start, start), expr)
+        for node in expr:
+            node._set_parent(self)
+
+        message_bundler = servertools.MessageBundler(
+            server=self.server,
+            sync=True,
+            )
+
+        with message_bundler:
+
+            if not start or not self:
+                target_node = self
+            else:
+                target_node = self[start - 1]
+
+            for node in expr:
+                node._set_parent(self)
+                if node.is_allocated:
+                    if target_node is self:
+                        request = requesttools.GroupHeadRequest(
+                            group_id=self.node_id,
+                            node_id=node.node_id,
+                            )
+                    else:
+                        request = requesttools.NodeAfterRequest(
+                            node_id=node.node_id,
+                            target_node_id=target_node.node_id,
+                            )
+                    message_bundler.add_message(request)
+                else:
+                    if target_node is self:
+                        add_action = servertools.AddAction.ADD_TO_HEAD
+                    else:
+                        add_action = servertools.AddAction.ADD_AFTER
+                    add_action, node_id, target_node_id = Node.allocate(
+                        node,
+                        add_action=add_action,
+                        target_node=target_node,
+                        )
+                    if isinstance(node, servertools.Group):
+                        request = requesttools.GroupNewRequest(
+                            add_action=add_action,
+                            node_id=node,
+                            target_node_id=target_node,
+                            )
+                    else:
+                        request = requesttools.SynthNewRequest(
+                            add_action=add_action,
+                            node_id=node,
+                            synthdef=node.synthdef,
+                            target_node_id=target_node,
+                            )
+                        # handle synth control settings too?
+                        # handle synth controls which are buses?
+                    message_bundler.add_message(request)
+                            
+                target_node = node
 
     ### PRIVATE METHODS ###
 
