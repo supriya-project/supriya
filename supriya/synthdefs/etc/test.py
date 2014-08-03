6d612f9c090eb1c639b8e2c52a61954819231b41
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-03 16:50:52 +0200

    Improved test synthdef.

--- supriya/synthdefs/etc/test.py
+++ supriya/synthdefs/etc/test.py
@@ -4,13 +4,13 @@ from supriya.tools import ugentools
 
 
 def _build_test_synthdef():
-
     builder = synthdeftools.SynthDefBuilder()
-    builder.add_ugen(
-        ugentools.Out.ar(
-            source=ugentools.SinOsc.ar(),
-            ),
-        )
+    builder.add_parameter('frequency', 440)
+    builder.add_parameter('amplitude', 1.0, 'audio')
+    sin_osc = ugentools.SinOsc.ar(frequency=builder['frequency'])
+    enveloped_sin = sin_osc * builder['amplitude']
+    out = ugentools.Out.ar(bus=0, source=enveloped_sin)
+    builder.add_ugen(out)
     synthdef = builder.build(name='test')
     return synthdef
 
