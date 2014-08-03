4457d961be5f98f4a5e433f13205a520396ff49b
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-03 16:52:03 +0200

    Added optional synchronizing request to MessageBundler.

--- supriya/tools/servertools/MessageBundler.py
+++ supriya/tools/servertools/MessageBundler.py
@@ -8,8 +8,11 @@ class MessageBundler(SupriyaObject):
 
     __slots__ = (
         '_messages',
+        '_result',
+        '_send_to_server',
         '_server',
         '_sync',
+        '_synchronizing_request',
         '_timestamp',
         )
 
@@ -17,15 +20,19 @@ class MessageBundler(SupriyaObject):
 
     def __init__(
         self,
+        send_to_server=True,
         server=None,
         sync=False,
         timestamp=None,
         ):
         from supriya.tools import servertools
+        self._result = None
+        self._send_to_server = bool(send_to_server)
         server = server or servertools.Server()
         self._server = server
         self._messages = []
         self._sync = bool(sync)
+        self._synchronizing_request = None
         if timestamp is not None:
             timestamp = float(timestamp)
         self._timestamp = timestamp
@@ -45,24 +52,30 @@ class MessageBundler(SupriyaObject):
             if not isinstance(message, prototype):
                 message = message.to_osc_message()
             messages.append(message)
-        if self._sync:
+        if self._sync and not self._synchronizing_request:
             sync_id = self._server.next_sync_id
             sync_request = requesttools.SyncRequest(
                 sync_id=sync_id,
                 )
             message = sync_request.to_osc_message()
             messages.append(message)
-        osc_bundle = osctools.OscBundle(
-            timestamp=self._timestamp,
-            contents=messages,
-            )
+        if 1 < len(messages) or self._timestamp is not None:
+            self._result = osctools.OscBundle(
+                timestamp=self._timestamp,
+                contents=messages,
+                )
+        else:
+            self._result = messages[0]
+        if not self._send_to_server:
+            return
         if self._sync:
+            sync_request = self._synchronizing_request or sync_request
             sync_request.communicate(
-                message=osc_bundle,
+                message=self.result,
                 server=self._server,
                 )
         else:
-            self._server.send_message(osc_bundle)
+            self._server.send_message(self._result)
 
     ### PUBLIC METHODS ###
 
@@ -76,3 +89,14 @@ class MessageBundler(SupriyaObject):
             )
         assert isinstance(message, prototype)
         self._messages.append(message)
+
+    def add_synchronizing_request(self, request):
+        from supriya.tools import requesttools
+        assert isinstance(request, (type(None), requesttools.Request))
+        self._synchronizing_request = request
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def result(self):
+        return self._result
