45f2c012df80f68c1ff8ce3be5e283b23faeafc6
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-04 21:12:06 +0200

    Wrote Group.remove() test.

---
+++ supriya/tools/servertools/test/test_Group_remove.py
@@ -0,0 +1,129 @@
+# -*- encoding: utf-8 -*- import pytest
+import pytest
+from abjad.tools import systemtools
+from supriya import synthdefs
+from supriya.tools import servertools
+
+
+@pytest.fixture(scope='function')
+def server(request):
+    def server_teardown():
+        server.quit()
+    server = servertools.Server().boot()
+    request.addfinalizer(server_teardown)
+    return server
+
+
+def test_Group_remove_01(server):
+
+    group_a = servertools.Group()
+    group_a.allocate()
+    synth_a = servertools.Synth(synthdefs.test)
+    group_a.append(synth_a)
+    group_b = servertools.Group()
+    group_a.append(group_b)
+    synth_b = servertools.Synth(synthdefs.test)
+    group_b.append(synth_b)
+    synth_c = servertools.Synth(synthdefs.test)
+    group_b.append(synth_c)
+    group_c = servertools.Group()
+    group_b.append(group_c)
+    synth_d = servertools.Synth(synthdefs.test)
+    group_a.append(synth_d)
+
+    server_state = str(server.query_remote_nodes())
+    assert systemtools.TestManager.compare(
+        server_state,
+        '''
+        NODE TREE 0 group
+            1 group
+                1000 group
+                    1001 test
+                    1002 group
+                        1003 test
+                        1004 test
+                        1005 group
+                    1006 test
+        ''',
+        ), server_state
+
+    group_a.remove(synth_d)
+
+    server_state = str(server.query_remote_nodes())
+    assert systemtools.TestManager.compare(
+        server_state,
+        '''
+        NODE TREE 0 group
+            1 group
+                1000 group
+                    1001 test
+                    1002 group
+                        1003 test
+                        1004 test
+                        1005 group
+        ''',
+        ), server_state
+
+    group_b.remove(synth_c)
+
+    server_state = str(server.query_remote_nodes())
+    assert systemtools.TestManager.compare(
+        server_state,
+        '''
+        NODE TREE 0 group
+            1 group
+                1000 group
+                    1001 test
+                    1002 group
+                        1003 test
+                        1005 group
+        ''',
+        ), server_state
+
+    group_a.remove(synth_a)
+
+    server_state = str(server.query_remote_nodes())
+    assert systemtools.TestManager.compare(
+        server_state,
+        '''
+        NODE TREE 0 group
+            1 group
+                1000 group
+                    1002 group
+                        1003 test
+                        1005 group
+        ''',
+        ), server_state
+
+    group_b.remove(group_c)
+
+    server_state = str(server.query_remote_nodes())
+    assert systemtools.TestManager.compare(
+        server_state,
+        '''
+        NODE TREE 0 group
+            1 group
+                1000 group
+                    1002 group
+                        1003 test
+        ''',
+        ), server_state
+
+    group_a.remove(group_b)
+
+    server_state = str(server.query_remote_nodes())
+    assert systemtools.TestManager.compare(
+        server_state,
+        '''
+        NODE TREE 0 group
+            1 group
+                1000 group
+        ''',
+        ), server_state
+
+    assert not group_b.is_allocated
+    assert not group_c.is_allocated
+    assert not synth_a.is_allocated
+    assert not synth_b.is_allocated
+    assert not synth_c.is_allocated
+    assert not synth_d.is_allocated
