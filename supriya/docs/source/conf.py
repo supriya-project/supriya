22bca7da3d16e9c2018c20ea5364c5e4d88ff739
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-10-28 15:56:14 -0700

    Modernized docs.

--- supriya/docs/source/conf.py
+++ supriya/docs/source/conf.py
@@ -12,8 +12,6 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sphinx_bootstrap_theme
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -109,8 +107,6 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 
 html_theme = 'default'
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
