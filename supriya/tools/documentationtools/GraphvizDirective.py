aa0dd0cedc470c94522de9939577a2096b1dfa52
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-10-28 12:10:53 -0700

    Added documentationtools.GraphvizDirective.

---
+++ supriya/tools/documentationtools/GraphvizDirective.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools import documentationtools
+
+
+class GraphvizDirective(documentationtools.ReSTDirective):
+
+    ### INITIALIZER ###
+
+    def __init__(self, graph, name=None):
+        assert isinstance(graph, documentationtools.GraphvizGraph)
+        self._graph = graph
+        documentationtools.ReSTDirective.__init__(
+            self,
+            name=name,
+            )
+
+    ### PRIVATE PROPERTIES ###
+
+    @property
+    def _children_rest_format_contributions(self):
+        result = []
+        graphviz_format = str(self.graph)
+        for line in graphviz_format.splitlines():
+            line = '   {}'.format(line)
+            result.append(line)
+        return result
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def directive(self):
+        return 'graphviz'
+
+    @property
+    def graph(self):
+        return self._graph
