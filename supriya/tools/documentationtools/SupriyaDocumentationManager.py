b35b616285a63bd14fb84aff8ee53a25736428fb
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-10-30 11:32:55 -0700

    Beautified documentation.

--- supriya/tools/documentationtools/SupriyaDocumentationManager.py
+++ supriya/tools/documentationtools/SupriyaDocumentationManager.py
@@ -4,6 +4,7 @@ import enum
 import inspect
 import importlib
 import os
+import re
 import shutil
 import types
 
@@ -174,91 +175,106 @@ class SupriyaDocumentationManager(object):
         return result
 
     @staticmethod
-    def get_lineage_graph(cls):
-        def get_node_name(original_name):
-            parts = original_name.split('.')
-            name = [parts[0]]
-            for part in parts[1:]:
-                if part != name[-1]:
-                    name.append(part)
-            if name[0] in ('abjad', 'experimental'):
-                return str('.'.join(name[2:]))
-            return str('.'.join(name))
-        from abjad.tools import documentationtools
-        addresses = ('abjad', 'supriya')
-        module_name, _, class_name = cls.__module__.rpartition('.')
-        importlib.import_module(module_name)
-        lineage = documentationtools.InheritanceGraph(
-            addresses=addresses,
-            lineage_addresses=((module_name, class_name),)
+    def ensure_directory(path):
+        path = os.path.dirname(path)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    @staticmethod
+    def execute():
+        print('Rebuilding Supriya documentation source.')
+        manager = SupriyaDocumentationManager
+        rewritten_files = set()
+        tools_packages = manager.get_tools_packages()
+        api_index_rst = manager.get_api_index_rst(tools_packages)
+        api_index_file_path = manager.get_api_index_file_path()
+        manager.ensure_directory(api_index_file_path)
+        manager.write(
+            api_index_file_path,
+            api_index_rst.rest_format,
+            rewritten_files,
             )
-        graph = lineage.__graph__()
-        maximum_node_count = 30
-        if maximum_node_count < len(graph.leaves):
-            lineage = documentationtools.InheritanceGraph(
-                addresses=addresses,
-                lineage_addresses=((module_name, class_name),),
-                lineage_prune_distance=2,
+        for package in tools_packages:
+            tools_package_rst = manager.get_tools_package_rst(package)
+            tools_package_file_path = manager.package_path_to_file_path(
+                package.__package__)
+            manager.ensure_directory(tools_package_file_path)
+            manager.write(
+                tools_package_file_path,
+                tools_package_rst.rest_format,
+                rewritten_files,
                 )
-            graph = lineage.__graph__()
-        if maximum_node_count < len(graph.leaves):
-            lineage = documentationtools.InheritanceGraph(
-                addresses=addresses,
-                lineage_addresses=((module_name, class_name),),
-                lineage_prune_distance=1,
-                )
-            graph = lineage.__graph__()
-        if maximum_node_count < len(graph.leaves):
-            lineage = documentationtools.InheritanceGraph(
-                addresses=((module_name, class_name),),
-                )
-            node_name = get_node_name(module_name + '.' + class_name)
-            graph = lineage.__graph__()
-            graph_node = graph[node_name]
-            graph_node.attributes['color'] = 'black'
-            graph_node.attributes['fontcolor'] = 'white'
-            graph_node.attributes['style'] = ('filled', 'rounded')
-        return graph
+            classes, functions = \
+                manager.get_tools_package_contents(package)
+            for cls in classes:
+                file_path = manager.module_path_to_file_path(
+                    cls.__module__,
+                    )
+                rst = manager.get_class_rst(cls)
+                manager.write(file_path, rst.rest_format, rewritten_files)
+            for function in functions:
+                file_path = manager.module_path_to_file_path(
+                    function.__module__,
+                    )
+                rst = manager.get_function_rst(function)
+                manager.write(file_path, rst.rest_format, rewritten_files)
+        for root, directory_names, file_names in os.walk(
+            manager.get_api_directory_path(),
+            topdown=False,
+            ):
+            for file_name in file_names[:]:
+                file_path = os.path.join(root, file_name)
+                if file_path not in rewritten_files:
+                    file_names.remove(file_name)
+                    os.remove(file_path)
+                    print('PRUNED: {}'.format(file_path))
+            if not file_names and not directory_names:
+                shutil.rmtree(root)
+                print('PRUNED: {}'.format(root))
 
     @staticmethod
-    def get_tools_packages():
-        import supriya.tools
-        tools_packages = []
-        for name in dir(supriya.tools):
-            if name.startswith('_'):
-                continue
-            module = getattr(supriya.tools, name)
-            if not isinstance(module, types.ModuleType):
-                continue
-            if not module.__package__.startswith(supriya.__package__):
-                continue
-            tools_packages.append(module)
-        tools_packages.sort(key=lambda x: x.__name__)
-        tools_packages = tuple(tools_packages)
-        return tools_packages
+    def get_api_directory_path():
+        import supriya
+        path = supriya.__path__[0]
+        path = os.path.join(
+            path,
+            'docs',
+            'source',
+            'tools',
+            )
+        return path
 
     @staticmethod
-    def get_tools_package_contents(tools_package):
-        classes = []
-        functions = []
-        for name in dir(tools_package):
-            if name.startswith('_'):
-                continue
-            obj = getattr(tools_package, name)
-            if not hasattr(obj, '__module__'):
-                print('Warning: no nominative object in {}'.format(obj))
-                continue
-            if not obj.__module__.startswith(tools_package.__package__):
-                continue
-            if isinstance(obj, type):
-                classes.append(obj)
-            elif isinstance(obj, types.FunctionType):
-                functions.append(obj)
-        classes.sort(key=lambda x: x.__name__)
-        classes = tuple(classes)
-        functions.sort(key=lambda x: x.__name__)
-        functions = tuple(functions)
-        return classes, functions
+    def get_api_index_file_path():
+        manager = SupriyaDocumentationManager
+        path = os.path.join(
+            manager.get_api_directory_path(),
+            'index.rst',
+            )
+        return path
+
+    @staticmethod
+    def get_api_index_rst(tools_packages):
+        from abjad.tools import documentationtools
+        document = documentationtools.ReSTDocument()
+        heading = documentationtools.ReSTHeading(
+            level=2,
+            text='Supriya API',
+            )
+        document.append(heading)
+        toc = documentationtools.ReSTTOCDirective(
+            options={
+                'maxdepth': 2,
+                },
+            )
+        for tools_package in tools_packages:
+            tools_package_name = tools_package.__package__.split('.')[-1]
+            toc_item = documentationtools.ReSTTOCItem(
+                text='{}/index'.format(tools_package_name),
+                )
+            toc.append(toc_item)
+        document.append(toc)
+        return document
 
     @staticmethod
     def get_class_rst(cls):
@@ -352,6 +368,21 @@ class SupriyaDocumentationManager(object):
         return document
 
     @staticmethod
+    def get_class_summary(cls):
+        doc = cls.__doc__
+        if doc is None:
+            doc = ''
+        doc = doc.splitlines()
+        m = re.search(r"^([A-Z].*?\.)(?:\s|$)", " ".join(doc).strip())
+        if m:
+            summary = m.group(1).strip()
+        elif doc:
+            summary = doc[0].strip()
+        else:
+            summary = ''
+        return summary
+
+    @staticmethod
     def get_function_rst(function):
         import abjad
         import supriya
@@ -371,12 +402,98 @@ class SupriyaDocumentationManager(object):
             )
         document.append(heading)
         autodoc_directive = abjad.documentationtools.ReSTAutodocDirective(
-            #argument=function.__module__,
             argument=function.__name__,
             directive='autofunction',
             )
         document.append(autodoc_directive)
         return document
+
+    @staticmethod
+    def get_lineage_graph(cls):
+        def get_node_name(original_name):
+            parts = original_name.split('.')
+            name = [parts[0]]
+            for part in parts[1:]:
+                if part != name[-1]:
+                    name.append(part)
+            if name[0] in ('abjad', 'experimental'):
+                return str('.'.join(name[2:]))
+            return str('.'.join(name))
+        from abjad.tools import documentationtools
+        addresses = ('abjad', 'supriya')
+        module_name, _, class_name = cls.__module__.rpartition('.')
+        importlib.import_module(module_name)
+        lineage = documentationtools.InheritanceGraph(
+            addresses=addresses,
+            lineage_addresses=((module_name, class_name),)
+            )
+        graph = lineage.__graph__()
+        maximum_node_count = 30
+        if maximum_node_count < len(graph.leaves):
+            lineage = documentationtools.InheritanceGraph(
+                addresses=addresses,
+                lineage_addresses=((module_name, class_name),),
+                lineage_prune_distance=2,
+                )
+            graph = lineage.__graph__()
+        if maximum_node_count < len(graph.leaves):
+            lineage = documentationtools.InheritanceGraph(
+                addresses=addresses,
+                lineage_addresses=((module_name, class_name),),
+                lineage_prune_distance=1,
+                )
+            graph = lineage.__graph__()
+        if maximum_node_count < len(graph.leaves):
+            lineage = documentationtools.InheritanceGraph(
+                addresses=((module_name, class_name),),
+                )
+            node_name = get_node_name(module_name + '.' + class_name)
+            graph = lineage.__graph__()
+            graph_node = graph[node_name]
+            graph_node.attributes['color'] = 'black'
+            graph_node.attributes['fontcolor'] = 'white'
+            graph_node.attributes['style'] = ('filled', 'rounded')
+        return graph
+
+    @staticmethod
+    def get_tools_packages():
+        import supriya.tools
+        tools_packages = []
+        for name in dir(supriya.tools):
+            if name.startswith('_'):
+                continue
+            module = getattr(supriya.tools, name)
+            if not isinstance(module, types.ModuleType):
+                continue
+            if not module.__package__.startswith(supriya.__package__):
+                continue
+            tools_packages.append(module)
+        tools_packages.sort(key=lambda x: x.__name__)
+        tools_packages = tuple(tools_packages)
+        return tools_packages
+
+    @staticmethod
+    def get_tools_package_contents(tools_package):
+        classes = []
+        functions = []
+        for name in dir(tools_package):
+            if name.startswith('_'):
+                continue
+            obj = getattr(tools_package, name)
+            if not hasattr(obj, '__module__'):
+                print('Warning: no nominative object in {}'.format(obj))
+                continue
+            if not obj.__module__.startswith(tools_package.__package__):
+                continue
+            if isinstance(obj, type):
+                classes.append(obj)
+            elif isinstance(obj, types.FunctionType):
+                functions.append(obj)
+        classes.sort(key=lambda x: x.__name__)
+        classes = tuple(classes)
+        functions.sort(key=lambda x: x.__name__)
+        functions = tuple(functions)
+        return classes, functions
 
     @staticmethod
     def get_tools_package_rst(tools_package):
@@ -433,7 +550,7 @@ class SupriyaDocumentationManager(object):
                 document.append(heading)
                 toc = documentationtools.ReSTTOCDirective(
                     options={
-                        'maxdepth': 1,
+                        'hidden': True,
                         },
                     )
                 for cls in sections[section_name]:
@@ -442,6 +559,17 @@ class SupriyaDocumentationManager(object):
                         )
                     toc.append(toc_item)
                 document.append(toc)
+                autosummary = documentationtools.ReSTAutosummaryDirective(
+                    options={
+                        'nosignatures': True,
+                        },
+                    )
+                for cls in sections[section_name]:
+                    item = documentationtools.ReSTAutosummaryItem(
+                        text=cls.__name__,
+                        )
+                    autosummary.append(item)
+                document.append(autosummary)
         if functions:
             heading = documentationtools.ReSTHeading(
                 level=3,
@@ -450,59 +578,26 @@ class SupriyaDocumentationManager(object):
             document.append(heading)
             toc = documentationtools.ReSTTOCDirective(
                 options={
-                    'maxdepth': 1,
+                    'hidden': True,
                     },
                 )
             for function in functions:
                 toc_item = documentationtools.ReSTTOCItem(
-                    text=function.__name__,
+                    text = function.__name__,
                     )
                 toc.append(toc_item)
             document.append(toc)
-        return document
-
-    @staticmethod
-    def get_api_directory_path():
-        import supriya
-        path = supriya.__path__[0]
-        path = os.path.join(
-            path,
-            'docs',
-            'source',
-            'tools',
-            )
-        return path
-
-    @staticmethod
-    def get_api_index_file_path():
-        manager = SupriyaDocumentationManager
-        path = os.path.join(
-            manager.get_api_directory_path(),
-            'index.rst',
-            )
-        return path
-
-    @staticmethod
-    def get_api_index_rst(tools_packages):
-        from abjad.tools import documentationtools
-        document = documentationtools.ReSTDocument()
-        heading = documentationtools.ReSTHeading(
-            level=2,
-            text='Supriya API',
-            )
-        document.append(heading)
-        toc = documentationtools.ReSTTOCDirective(
-            options={
-                'maxdepth': 2,
-                },
-            )
-        for tools_package in tools_packages:
-            tools_package_name = tools_package.__package__.split('.')[-1]
-            toc_item = documentationtools.ReSTTOCItem(
-                text='{}/index'.format(tools_package_name),
+            autosummary = documentationtools.ReSTAutosummaryDirective(
+                options={
+                    'nosignatures': True,
+                    },
                 )
-            toc.append(toc_item)
-        document.append(toc)
+            for function in functions:
+                item = documentationtools.ReSTAutosummaryItem(
+                    text=function.__name__,
+                    )
+                autosummary.append(item)
+            document.append(autosummary)
         return document
 
     @staticmethod
@@ -533,12 +628,6 @@ class SupriyaDocumentationManager(object):
             shutil.rmtree(path)
 
     @staticmethod
-    def ensure_directory(path):
-        path = os.path.dirname(path)
-        if not os.path.exists(path):
-            os.makedirs(path)
-
-    @staticmethod
     def write(file_path, string, rewritten_files):
         should_write = True
         if os.path.exists(file_path):
@@ -553,56 +642,3 @@ class SupriyaDocumentationManager(object):
         else:
             print('PRESERVED: {}'.format(file_path))
         rewritten_files.add(file_path)
-
-    @staticmethod
-    def execute():
-        print('Rebuilding Supriya documentation source.')
-        manager = SupriyaDocumentationManager
-        rewritten_files = set()
-        #manager.remove_api_directory()
-        tools_packages = manager.get_tools_packages()
-        api_index_rst = manager.get_api_index_rst(tools_packages)
-        api_index_file_path = manager.get_api_index_file_path()
-        manager.ensure_directory(api_index_file_path)
-        manager.write(
-            api_index_file_path,
-            api_index_rst.rest_format,
-            rewritten_files,
-            )
-        for package in tools_packages:
-            tools_package_rst = manager.get_tools_package_rst(package)
-            tools_package_file_path = manager.package_path_to_file_path(
-                package.__package__)
-            manager.ensure_directory(tools_package_file_path)
-            manager.write(
-                tools_package_file_path,
-                tools_package_rst.rest_format,
-                rewritten_files,
-                )
-            classes, functions = \
-                manager.get_tools_package_contents(package)
-            for cls in classes:
-                file_path = manager.module_path_to_file_path(
-                    cls.__module__,
-                    )
-                rst = manager.get_class_rst(cls)
-                manager.write(file_path, rst.rest_format, rewritten_files)
-            for function in functions:
-                file_path = manager.module_path_to_file_path(
-                    function.__module__,
-                    )
-                rst = manager.get_function_rst(function)
-                manager.write(file_path, rst.rest_format, rewritten_files)
-        for root, directory_names, file_names in os.walk(
-            manager.get_api_directory_path(),
-            topdown=False,
-            ):
-            for file_name in file_names[:]:
-                file_path = os.path.join(root, file_name)
-                if file_path not in rewritten_files:
-                    file_names.remove(file_name)
-                    os.remove(file_path)
-                    print('PRUNED: {}'.format(file_path))
-            if not file_names and not directory_names:
-                shutil.rmtree(root)
-                print('PRUNED: {}'.format(root))
