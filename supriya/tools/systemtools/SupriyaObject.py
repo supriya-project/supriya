b93f7ca856685f3a14452fd388a2f221b0f69d3d
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-11-01 15:17:18 -0700

    Documented SupriyaObject.

--- supriya/tools/systemtools/SupriyaObject.py
+++ supriya/tools/systemtools/SupriyaObject.py
@@ -19,21 +19,36 @@ class SupriyaObject(AbstractBase):
 
     ### CLASS VARIABLES ###
 
-    __slots__ = (
-        )
+    __slots__ = ()
 
     ### SPECIAL METHODS ###
 
     def __eq__(self, expr):
+        r'''Is true when ID of `expr` equals ID of Supriya object.
+        Otherwise false.
+
+        Returns boolean.
+        '''
         return id(self) == id(expr)
 
     def __format__(self, format_specification=''):
+        r'''Formats Supriya object.
+
+        Set `format_specification` to `''` or `'storage'`.
+        Interprets `''` equal to `'storage'`.
+
+        Returns string.
+        '''
         from abjad.tools import systemtools
         if format_specification in ('', 'storage'):
             return systemtools.StorageFormatManager.get_storage_format(self)
         return str(self)
 
     def __getstate__(self):
+        r'''Gets state of Supriya object.
+
+        Returns dictionary.
+        '''
         if hasattr(self, '__dict__'):
             return vars(self)
         state = {}
@@ -43,13 +58,27 @@ class SupriyaObject(AbstractBase):
         return state
 
     def __hash__(self):
-        return id(self) 
+        r'''Hashes Supriya object.
+
+        Required to be explicitely re-defined on Python 3 if __eq__ changes.
+
+        Returns integer.
+        '''
+        return id(self)
 
     def __repr__(self):
+        r'''Gets interpreter representation of Supriya object.
+
+        Returns string.
+        '''
         from abjad.tools import systemtools
         return systemtools.StorageFormatManager.get_repr_format(self)
 
     def __setstate__(self, state):
+        r'''Sets state of Supriya object.
+
+        Returns none.
+        '''
         for key, value in state.items():
             setattr(self, key, value)
 
