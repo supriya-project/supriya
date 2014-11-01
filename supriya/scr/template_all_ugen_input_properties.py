29777e402e6e4951ac0c50dcb330cd82a2ea2294
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-11-01 10:55:56 -0700

    Improved UGen property templating script.

--- supriya/scr/template_all_ugen_input_properties.py
+++ supriya/scr/template_all_ugen_input_properties.py
@@ -10,20 +10,23 @@ def template_new_property(input_name, ugen_class):
     class_name = ugen_class.__name__
     lower_name = stringtools.to_snake_case(class_name)
     template_lines = [
-        ""
-        "    @property"
-        "    def {input_name}(self):"
-        "        r'''Gets `{input_name}` input of {class_name}."
-        ""
-        "        ::"
-        ""
-        "            >>> {lower_name} = ugentools.{class_name}.ar({input_name}=0.5)"
-        "            >>> {lower_name}.{input_name}"
-        ""
-        "        Returns input."
-        "        '''"
-        "        index = self._ordered_input_names.index('{}')"
-        "        return self._inputs[index]"
+        "",
+        "    @property",
+        "    def {input_name}(self):",
+        "        r'''Gets `{input_name}` input of {class_name}.",
+        "",
+        "        ::",
+        "",
+        "            >>> {input_name} = None",
+        "            >>> {lower_name} = ugentools.{class_name}.ar(",
+        "            ...     {input_name}={input_name},",
+        "            ...     )",
+        "            >>> {lower_name}.{input_name}",
+        "",
+        "        Returns input.",
+        "        '''",
+        "        index = self._ordered_input_names.index('{input_name}')",
+        "        return self._inputs[index]",
         ]
     formatted_lines = [
         line.format(
@@ -46,11 +49,16 @@ def rebuild_ugen_source(ugen_class):
         if input_name not in attrs:
             missing_property_names.append(input_name)
     ugen_source = inspect.findsource(ugen_class)[0]
-    new_lines = []
-    for property_name in missing_property_names:
-        new_property_lines = template_new_property(input_name, ugen_class)
-        new_lines.extend(new_property_lines)
-    ugen_source.extend(new_lines)
+    ugen_source = ''.join(ugen_source).splitlines()
+    if missing_property_names:
+        new_lines = []
+        new_lines.append('')
+        new_lines.append('    ### PUBLIC PROPERTIES ###')
+        for property_name in sorted(missing_property_names):
+            new_property_lines = template_new_property(
+                property_name, ugen_class)
+            new_lines.extend(new_property_lines)
+        ugen_source.extend(new_lines)
     ugen_source = '\n'.join(ugen_source)
     return ugen_source
 
