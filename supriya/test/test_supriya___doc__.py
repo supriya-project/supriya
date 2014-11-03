d14c07332835e21e6b651bab36c7d1515f956270
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-11-03 14:05:31 -0800

    Improved documentation parameterized test.

--- supriya/test/test_supriya___doc__.py
+++ supriya/test/test_supriya___doc__.py
@@ -19,34 +19,46 @@ classes = documentationtools.list_all_supriya_classes(
     bases=synthdeftools.UGen,
     )
 
+class_attr_pairs = []
+for cls in classes:
+    if cls.__name__ in ignored_classes:
+        continue
+    attrs = inspect.classify_class_attrs(cls)
+    for attr in attrs:
+        if attr.name in ignored_names:
+            continue
+        elif attr.defining_class is not cls:
+            continue
+        elif attr.kind == 'data':
+            continue
+        class_attr_pairs.append((cls, attr))
+
 functions = documentationtools.list_all_supriya_functions()
 
 
 @pytest.mark.parametrize('obj', classes)
 def test_supriya___doc___01(obj):
-    r'''All classes have a docstring. All class methods have a docstring.
+    r'''All classes have a docstring.
     '''
     if obj.__doc__ is None:
         message = 'No documentation for: {}'.format(obj.__name__)
         raise Exception(message)
-    if obj.__name__ in ignored_classes:
-        return
-    for attr in inspect.classify_class_attrs(obj):
-        if attr.name in ignored_names:
-            continue
-        elif attr.defining_class is not obj:
-            continue
-        elif attr.kind == 'data':
-            continue
-        if attr.name[0].isalpha() or attr.name.startswith('__'):
-            if getattr(obj, attr.name).__doc__ is None:
-                message = 'No documentation for: {}.{}'
-                message = message.format(obj.__name__, attr.name)
-                raise Exception(message)
+
+
+@pytest.mark.parametrize('pair', class_attr_pairs)
+def test_supriya___doc___02(pair):
+    r'''All methods and properties have a docstring.
+    '''
+    cls, attr = pair
+    if attr.name[0].isalpha() or attr.name.startswith('__'):
+        if getattr(cls, attr.name).__doc__ is None:
+            message = 'No documentation for: {}.{}'
+            message = message.format(cls.__name__, attr.name)
+            raise Exception(message)
 
 
 @pytest.mark.parametrize('obj', functions)
-def test_supriya___doc___02(obj):
+def test_supriya___doc___03(obj):
     r'''All functions have a docstring.
     '''
     if obj.__doc__ is None:
