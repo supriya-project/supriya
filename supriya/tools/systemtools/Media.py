ff34a7c05e8509d6a9655aa0fd9ddc2841ffae08
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-11-09 19:27:13 -0800

    Integrated glob pattern matching in systemtools.Media.

--- supriya/tools/systemtools/Media.py
+++ supriya/tools/systemtools/Media.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+import fnmatch
 import os
 
 
@@ -6,15 +7,19 @@ class Media(object):
 
     ### SPECIAL METHODS ###
 
-    def __getitem__(self, item):
+    def __getitem__(self, pattern):
         import supriya
-        return os.path.abspath(
-            os.path.join(
-                supriya.__path__[0],
-                'media',
-                str(item),
-                )
+        media_path = os.path.join(
+            supriya.__path__[0],
+            'media',
             )
+        result = []
+        result = os.listdir(media_path)
+        result = fnmatch.filter(result, pattern)
+        result = [os.path.join(media_path, _) for _ in result]
+        if len(result) == 1:
+            return result[0]
+        return result
 
 
 Media = Media()
