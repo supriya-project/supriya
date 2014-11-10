95e787fde4a8d7c73ad8fd8b67bd6252bb9b0046
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-11-09 21:04:12 -0800

    Implemented MouseX, MouseY and MouseButton UGens.

---
+++ supriya/tools/ugentools/MouseButton.py
@@ -0,0 +1,82 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.synthdeftools.CalculationRate import CalculationRate
+from supriya.tools.synthdeftools.SignalRange import SignalRange
+from supriya.tools.synthdeftools.UGen import UGen
+
+
+class MouseButton(UGen):
+    r'''Mouse-button tracking unit generator.
+
+    ::
+
+        >>> ugentools.MouseButton.kr()
+        MouseButton.kr()
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __documentation_section__ = 'User Interaction'
+
+    __slots__ = ()
+
+    _ordered_input_names = (
+        'minimum',
+        'maximum',
+        'lag',
+        )
+
+    _signal_range = SignalRange.UNIPOLAR
+
+    _valid_calculation_rates = (
+        CalculationRate.CONTROL,
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        calculation_rate=None,
+        lag=0.2,
+        maximum=1,
+        minimum=0,
+        ):
+        UGen.__init__(
+            self,
+            calculation_rate=calculation_rate,
+            lag=lag,
+            maximum=maximum,
+            minimum=minimum,
+            )
+
+    ### PUBLIC METHODS ###
+
+    @classmethod
+    def kr(
+        cls,
+        lag=0.2,
+        maximum=1,
+        minimum=0,
+        ):
+        r'''Creates a control-rate mouse button tracking unit generator.
+
+        ::
+
+            >>> ugentools.MouseButton.kr(
+            ...     lag=0.2,
+            ...     maximum=1,
+            ...     minimum=0,
+            ...     )
+            MouseButton.kr()
+
+        Returns unit generator graph.
+        '''
+        from supriya.tools import synthdeftools
+        calculation_rate = synthdeftools.CalculationRate.CONTROL
+        ugen = cls._new_expanded(
+            calculation_rate=calculation_rate,
+            lag=lag,
+            maximum=maximum,
+            minimum=minimum,
+            )
+        return ugen
---
+++ supriya/tools/ugentools/MouseX.py
@@ -0,0 +1,91 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools.synthdeftools.CalculationRate import CalculationRate
+from supriya.tools.synthdeftools.SignalRange import SignalRange
+from supriya.tools.synthdeftools.UGen import UGen
+
+
+class MouseX(UGen):
+    r'''Mouse cursor tracking unit generator.
+
+    MouseX tracks the y-axis of the mouse cursor position.
+
+    ::
+
+        >>> ugentools.MouseX.kr()
+        MouseX.kr()
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __documentation_section__ = 'User Interaction'
+
+    __slots__ = ()
+
+    _ordered_input_names = (
+        'minimum',
+        'maximum',
+        'warp',
+        'lag',
+        )
+
+    _signal_range = SignalRange.UNIPOLAR
+
+    _valid_calculation_rates = (
+        CalculationRate.CONTROL,
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        calculation_rate=None,
+        lag=0.2,
+        maximum=1,
+        minimum=0,
+        warp=0,
+        ):
+        warp = int(bool(warp))
+        UGen.__init__(
+            self,
+            calculation_rate=calculation_rate,
+            lag=lag,
+            maximum=maximum,
+            minimum=minimum,
+            warp=warp,
+            )
+
+    ### PUBLIC METHODS ###
+
+    @classmethod
+    def kr(
+        cls,
+        lag=0.2,
+        maximum=1,
+        minimum=0,
+        warp=0,
+        ):
+        r'''Creates a control-rate mouse cursor tracking unit generator.
+
+        ::
+
+            >>> ugentools.MouseX.kr(
+            ...     lag=0.2,
+            ...     maximum=1,
+            ...     minimum=0,
+            ...     warp=1,
+            ...     )
+            MouseX.kr()
+
+        Returns unit generator graph.
+        '''
+        from supriya.tools import synthdeftools
+        calculation_rate = synthdeftools.CalculationRate.CONTROL
+        ugen = cls._new_expanded(
+            calculation_rate=calculation_rate,
+            lag=lag,
+            maximum=maximum,
+            minimum=minimum,
+            warp=warp,
+            )
+        return ugen
