e2b1423fbff5ec18a8cf20e0354db61cc3fa8b6f
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-11-17 22:42:01 -0800

    Improved Range.
    Added Range.scale().

--- supriya/tools/synthdeftools/Range.py
+++ supriya/tools/synthdeftools/Range.py
@@ -1,8 +1,44 @@
 # -*- encoding: utf-8 -*-
+import collections
 from supriya.tools.systemtools.SupriyaObject import SupriyaObject
 
 
 class Range(SupriyaObject):
+    r'''A range.
+
+    ::
+
+        >>> synthdeftools.Range(-1., 1.)
+        Range(
+            minimum=-1.0,
+            maximum=1.0
+            )
+
+    ::
+
+        >>> synthdeftools.Range(minimum=0.)
+        Range(
+            minimum=0.0,
+            maximum=inf
+            )
+
+    ::
+
+        >>> synthdeftools.Range()
+        Range(
+            minimum=-inf,
+            maximum=inf
+            )
+
+    ::
+
+        >>> synthdeftools.Range((0.1, 0.9))
+        Range(
+            minimum=0.1,
+            maximum=0.9
+            )
+
+    '''
 
     ### CLASS VARIABLES ###
 
@@ -20,6 +56,10 @@ class Range(SupriyaObject):
         minimum=None,
         maximum=None,
         ):
+        if isinstance(minimum, collections.Sequence) and \
+            len(minimum) == 2 and \
+            maximum is None:
+            minimum, maximum = minimum
         if minimum is None:
             minimum = float('-inf')
         if not isinstance(minimum, (float, int)):
@@ -34,6 +74,47 @@ class Range(SupriyaObject):
         self._minimum = minimum
         self._maximum = maximum
 
+    ### PUBLIC METHODS ###
+
+    @staticmethod
+    def scale(value, input_range, output_range, exponent=1.):
+        r'''Scales `value` from `input_range` to `output_range`.
+
+        Curve value exponentially by `exponent`.
+
+        ::
+
+            >>> input_range = synthdeftools.Range(0., 10.)
+            >>> output_range = synthdeftools.Range(-2.5, 2.5)
+
+        ::
+
+            >>> synthdeftools.Range.scale(0., input_range, output_range)
+            -2.5
+
+        ::
+
+            >>> synthdeftools.Range.scale(5., input_range, output_range)
+            0.0
+
+        ::
+
+            >>> synthdeftools.Range.scale(5., input_range, output_range, 2.)
+            -1.25
+
+        ::
+
+            >>> synthdeftools.Range.scale(5., input_range, output_range, 0.5)
+            1.0355...
+
+        Returns float.
+        '''
+        value = (value - input_range.minimum) / input_range.width        
+        if exponent != 1:
+            value = pow(value, exponent)
+        value = (value * output_range.width) + output_range.minimum
+        return value
+
     ### PUBLIC PROPERTIES ###
 
     @property
@@ -43,3 +124,7 @@ class Range(SupriyaObject):
     @property
     def minimum(self):
         return self._minimum
+
+    @property
+    def width(self):
+        return self._maximum - self._minimum
