1cb4587168947e4514187a4629c74de767c00ff5
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-11-20 06:01:49 -0800

    Improved SynthDef tests.

--- supriya/tools/synthdeftools/test/test_SynthDef.py
+++ supriya/tools/synthdeftools/test/test_SynthDef.py
@@ -760,15 +760,17 @@ def test_SynthDef_10():
 
     with synthdeftools.SynthDefBuilder() as builder:
         source = ugentools.PinkNoise.ar()
-        pv_chain = ugentools.FFT(source=source)
+        local_buf = ugentools.LocalBuf(2048)
+        pv_chain = ugentools.FFT(buffer_id=local_buf, source=source)
         pv_chain_a = ugentools.PV_BinScramble(pv_chain=pv_chain)
         pv_chain_b = ugentools.PV_MagFreeze(pv_chain=pv_chain)
         pv_chain = ugentools.PV_MagMul(pv_chain_a, pv_chain_b)
         ifft = ugentools.IFFT.ar(pv_chain=pv_chain)
         ugentools.Out.ar(bus=0, source=ifft)
-    synthdef = builder.build('PVCopyTest')
+    py_synthdef = builder.build('PVCopyTest')
+    py_compiled_synthdef = py_synthdef.compile()
 
-    assert tuple(repr(_) for _ in synthdef.ugens) == (
+    assert tuple(repr(_) for _ in py_synthdef.ugens) == (
         'PinkNoise.ar()',
         'MaxLocalBufs.ir()',
         'LocalBuf.ir()',
@@ -782,3 +784,20 @@ def test_SynthDef_10():
         'IFFT.ar()',
         'Out.ar()',
         )
+
+    sc_synthdef = synthdeftools.SuperColliderSynthDef(
+        'PVCopyTest',
+        r'''
+        var source, pv_chain, pv_chain_a, pv_chain_b, ifft, out;
+        source = PinkNoise.ar();
+        pv_chain = FFT(LocalBuf(2048), source);
+        pv_chain_a = PV_BinScramble(pv_chain);
+        pv_chain_b = PV_MagFreeze(pv_chain);
+        pv_chain = PV_MagMul(pv_chain_a, pv_chain_b);
+        ifft = IFFT.ar(pv_chain);
+        out = Out.ar(0, ifft);
+        '''
+        )
+    sc_compiled_synthdef = bytes(sc_synthdef.compile())
+
+    assert sc_compiled_synthdef == py_compiled_synthdef
