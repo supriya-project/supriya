a4f8102830c4adcf7c62c0a7196234c559202f55
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-11-20 11:41:05 -0800

    Documented SpecPcile UGen.

--- supriya/tools/ugentools/SpecPcile.py
+++ supriya/tools/ugentools/SpecPcile.py
@@ -1,18 +1,22 @@
 # -*- encoding: utf-8 -*-
+from supriya.tools.synthdeftools.CalculationRate import CalculationRate
 from supriya.tools.synthdeftools.UGen import UGen
 
 
 class SpecPcile(UGen):
-    r'''
+    r'''Find a percentile of FFT magnitude spectrum.
 
     ::
 
-        >>> spec_pcile = ugentools.SpecPcile.(
-        ...     pv_chain=None,
+        >>> source = ugentools.SoundIn.ar(bus=0)
+        >>> pv_chain = ugentools.FFT(source=source)
+        >>> spec_pcile = ugentools.SpecPcile.kr(
+        ...     pv_chain=pv_chain,
         ...     fraction=0.5,
         ...     interpolate=0,
         ...     )
         >>> spec_pcile
+        SpecPcile.kr()
 
     '''
 
@@ -28,20 +32,21 @@ class SpecPcile(UGen):
         'interpolate',
         )
 
-    _valid_calculation_rates = None
+    _valid_calculation_rates = (
+        CalculationRate.CONTROL,
+        )
 
     ### INITIALIZER ###
 
     def __init__(
         self,
-        calculation_rate=None,
         pv_chain=None,
         fraction=0.5,
         interpolate=0,
         ):
         UGen.__init__(
             self,
-            calculation_rate=calculation_rate,
+            calculation_rate=CalculationRate.CONTROL,
             pv_chain=pv_chain,
             fraction=fraction,
             interpolate=interpolate,
@@ -60,19 +65,20 @@ class SpecPcile(UGen):
 
         ::
 
+            >>> source = ugentools.SoundIn.ar(bus=0)
+            >>> pv_chain = ugentools.FFT(source=source)
             >>> spec_pcile = ugentools.SpecPcile.kr(
-            ...     pv_chain=None,
+            ...     pv_chain=pv_chain,
             ...     fraction=0.5,
             ...     interpolate=0,
             ...     )
             >>> spec_pcile
+            SpecPcile.kr()
 
         Returns ugen graph.
         '''
         from supriya.tools import synthdeftools
-        calculation_rate = synthdeftools.CalculationRate.CONTROL
         ugen = cls._new_expanded(
-            calculation_rate=calculation_rate,
             pv_chain=pv_chain,
             fraction=fraction,
             interpolate=interpolate,
@@ -87,12 +93,44 @@ class SpecPcile(UGen):
 
         ::
 
-            >>> spec_pcile = ugentools.SpecPcile.ar(
-            ...     pv_chain=None,
+            >>> source = ugentools.SoundIn.ar(bus=0)
+            >>> pv_chain = ugentools.FFT(source=source)
+            >>> spec_pcile = ugentools.SpecPcile.kr(
+            ...     pv_chain=pv_chain,
             ...     fraction=0.5,
             ...     interpolate=0,
             ...     )
             >>> spec_pcile.pv_chain
+            OutputProxy(
+                source=FFT(
+                    buffer_id=OutputProxy(
+                        source=LocalBuf(
+                            frame_count=2048.0,
+                            channel_count=1.0,
+                            calculation_rate=<CalculationRate.SCALAR: 0>
+                            ),
+                        output_index=0
+                        ),
+                    source=OutputProxy(
+                        source=In(
+                            bus=OutputProxy(
+                                source=NumOutputBuses(
+                                    calculation_rate=<CalculationRate.SCALAR: 0>
+                                    ),
+                                output_index=0
+                                ),
+                            calculation_rate=<CalculationRate.AUDIO: 2>,
+                            channel_count=1
+                            ),
+                        output_index=0
+                        ),
+                    active=1.0,
+                    hop=0.5,
+                    window_size=0.0,
+                    window_type=0.0
+                    ),
+                output_index=0
+                )
 
         Returns ugen input.
         '''
@@ -105,12 +143,15 @@ class SpecPcile(UGen):
 
         ::
 
-            >>> spec_pcile = ugentools.SpecPcile.ar(
-            ...     pv_chain=None,
+            >>> source = ugentools.SoundIn.ar(bus=0)
+            >>> pv_chain = ugentools.FFT(source=source)
+            >>> spec_pcile = ugentools.SpecPcile.kr(
+            ...     pv_chain=pv_chain,
             ...     fraction=0.5,
             ...     interpolate=0,
             ...     )
             >>> spec_pcile.fraction
+            0.5
 
         Returns ugen input.
         '''
@@ -123,12 +164,15 @@ class SpecPcile(UGen):
 
         ::
 
-            >>> spec_pcile = ugentools.SpecPcile.ar(
-            ...     pv_chain=None,
+            >>> source = ugentools.SoundIn.ar(bus=0)
+            >>> pv_chain = ugentools.FFT(source=source)
+            >>> spec_pcile = ugentools.SpecPcile.kr(
+            ...     pv_chain=pv_chain,
             ...     fraction=0.5,
             ...     interpolate=0,
             ...     )
             >>> spec_pcile.interpolate
+            0.0
 
         Returns ugen input.
         '''
