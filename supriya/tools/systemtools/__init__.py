4440a592b535db6eae92f4c3eb9a3c3400af6755
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-11-30 16:50:27 -0800

    Migrated webgui/server.py script to scripts/supriya-web.

--- supriya/tools/systemtools/__init__.py
+++ supriya/tools/systemtools/__init__.py
@@ -10,3 +10,4 @@ from supriya.tools.systemtools.Library import Library
 from supriya.tools.systemtools.SupriyaObject import SupriyaObject
 from supriya.tools.systemtools.SupriyaValueObject import SupriyaValueObject
 from supriya.tools.systemtools.run_supriya import run_supriya
+from supriya.tools.systemtools.run_supriya_web import run_supriya_web
--- supriya/tools/systemtools/run_supriya_web.py
+++ supriya/tools/systemtools/run_supriya_web.py
@@ -2,8 +2,8 @@
 import sys
 
 
-def run_supriya():
-    r'''Runs Supriya.
+def run_supriya_web():
+    r'''Runs Supriya with web-server.
 
     Returns none.
     '''
@@ -18,7 +18,10 @@ def run_supriya():
         "from supriya import *;",
         "server = Server();",
         "server.debug_osc = True;",
-        "server.boot();"
+        "server.boot();",
+        "server.meters.allocate();",
+        "web_server = webguitools.WebServer(server);",
+        "web_server.start();",
         )
     commands = ' '.join(commands)
 
