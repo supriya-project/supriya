0a88a1f35e9ff576b8241fa19a82b8fd96f6fb77
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-02-21 10:14:28 -0800

    Streamlined SynthDefByteCompiler.

--- supriya/tools/synthdeftools/SynthDefByteCompiler.py
+++ supriya/tools/synthdeftools/SynthDefByteCompiler.py
@@ -19,7 +19,7 @@ class SynthDefByteCompiler(SupriyaObject):
         return result
 
     @staticmethod
-    def compile_parameters_new(synthdef):
+    def compile_parameters(synthdef):
         result = []
         result.append(SynthDefByteCompiler.encode_unsigned_int_32bit(
             len(synthdef.parameters)))
@@ -37,22 +37,22 @@ class SynthDefByteCompiler(SupriyaObject):
             result.append(SynthDefByteCompiler.encode_unsigned_int_32bit(index))
         return bytes().join(result)
 
-    @staticmethod
-    def compile_parameters_old(synthdef):
-        result = []
-        result.append(SynthDefByteCompiler.encode_unsigned_int_32bit(
-            len(synthdef.parameters)))
-        for value in synthdef.parameters:
-            result.append(SynthDefByteCompiler.encode_float(value))
-        result.append(SynthDefByteCompiler.encode_unsigned_int_32bit(
-            len(synthdef.parameter_names)))
-        for key, value in sorted(
-            synthdef.parameter_names.items(),
-            key=lambda x: x[1],
-            ):
-            result.append(SynthDefByteCompiler.encode_string(key))
-            result.append(SynthDefByteCompiler.encode_unsigned_int_32bit(value))
-        return bytes().join(result)
+#    @staticmethod
+#    def compile_parameters(synthdef):
+#        result = []
+#        result.append(SynthDefByteCompiler.encode_unsigned_int_32bit(
+#            len(synthdef.parameters)))
+#        for value in synthdef.parameters:
+#            result.append(SynthDefByteCompiler.encode_float(value))
+#        result.append(SynthDefByteCompiler.encode_unsigned_int_32bit(
+#            len(synthdef.parameter_names)))
+#        for key, value in sorted(
+#            synthdef.parameter_names.items(),
+#            key=lambda x: x[1],
+#            ):
+#            result.append(SynthDefByteCompiler.encode_string(key))
+#            result.append(SynthDefByteCompiler.encode_unsigned_int_32bit(value))
+#        return bytes().join(result)
 
     @staticmethod
     def compile_synthdefs(synthdefs):
@@ -101,7 +101,7 @@ class SynthDefByteCompiler(SupriyaObject):
         result.append(SynthDefByteCompiler.encode_unsigned_int_32bit(len(synthdef.constants)))
         for constant in synthdef.constants:
             result.append(SynthDefByteCompiler.encode_float(constant))
-        result.append(SynthDefByteCompiler.compile_parameters_new(synthdef))
+        result.append(SynthDefByteCompiler.compile_parameters(synthdef))
         result.append(SynthDefByteCompiler.encode_unsigned_int_32bit(len(synthdef.ugens)))
         for ugen_index, ugen in enumerate(synthdef.ugens):
             result.append(SynthDefByteCompiler.compile_ugen(ugen, synthdef))
