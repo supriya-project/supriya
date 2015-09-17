79916abb526fa941f7d4300d3404fb01631130a7
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-09-17 11:13:56 -0700

    Modernized class and function listing functions.

--- supriya/tools/documentationtools/list_all_supriya_classes.py
+++ supriya/tools/documentationtools/list_all_supriya_classes.py
@@ -1,21 +1,11 @@
 # -*- encoding: utf-8 -*-
-import os
 from abjad.tools import documentationtools
 
 
 def list_all_supriya_classes(bases=None):
     r'''Lists all supriya classes.
     '''
-    import supriya
-    root_path = os.path.join(
-        supriya.__path__[0],
-        'tools',
-        )
-    class_crawler = documentationtools.ClassCrawler(
-        code_root=root_path,
-        root_package_name='supriya',
-        )
-    classes = class_crawler()
+    classes = documentationtools.list_all_classes(modules='supriya.tools')
     if bases:
         classes = tuple(_ for _ in classes if issubclass(_, bases))
     classes = sorted(classes, key=lambda x: x.__module__)
--- supriya/tools/documentationtools/list_all_supriya_functions.py
+++ supriya/tools/documentationtools/list_all_supriya_functions.py
@@ -1,21 +1,11 @@
 # -*- encoding: utf-8 -*-
-import os
 from abjad.tools import documentationtools
 
 
 def list_all_supriya_functions():
     r'''Lists all supriya functions.
     '''
-    import supriya
-    root_path = os.path.join(
-        supriya.__path__[0],
-        'tools',
-        )
-    function_crawler = documentationtools.FunctionCrawler(
-        code_root=root_path,
-        root_package_name='supriya',
-        )
-    functions = function_crawler()
+    functions = documentationtools.list_all_functions(modules='supriya.tools')
     functions = sorted(functions, key=lambda x: x.__module__)
     functions = tuple(functions)
     return functions
