d0703f5fd32a38f5fdc26eaa5875b7d648c8f7fb
Josiah Oberholtzer <joberholtzer@discogsinc.com>
2015-12-20 13:39:38 -0500

    Fleshed-out NRT test.

--- supriya/tools/nrttools/test/test_Session.py
+++ supriya/tools/nrttools/test/test_Session.py
@@ -5,6 +5,12 @@ from supriya.tools import synthdeftools
 from supriya.tools import ugentools
 
 
+output_filepath = os.path.abspath(os.path.join(
+    os.path.dirname(__file__),
+    'output.aiff',
+    ))
+
+
 def test_01():
     session = nrttools.Session()
     builder = synthdeftools.SynthDefBuilder()
@@ -13,8 +19,9 @@ def test_01():
     synthdef = builder.build()
     synth_one = nrttools.Synth(synthdef, 0, 10)
     session.insert([synth_one])
-    output_filename = os.path.join(
-        os.path.dirname(__file__),
-        'output.aiff',
-        )
-    session.render(output_filename)
+    exit_code, _ = session.render(output_filepath)
+    output_file_exists = os.path.exists(output_filepath)
+    if output_file_exists:
+        os.remove(output_filepath)
+    assert output_file_exists
+    assert exit_code == 0
