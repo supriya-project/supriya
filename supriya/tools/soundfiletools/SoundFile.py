af87ba57f4d267133ebe384a88c1b55740f4b335
Josiah Oberholtzer <joberholtzer@discogsinc.com>
2015-12-22 23:32:03 -0500

    Added SoundFile frame accessors.

--- supriya/tools/soundfiletools/SoundFile.py
+++ supriya/tools/soundfiletools/SoundFile.py
@@ -26,6 +26,37 @@ class SoundFile(SupriyaObject):
             self._channel_count = reader.channels
             self._sample_rate = reader.samplerate
 
+    ### PUBLIC METHODS ###
+
+    def at_frame(self, frames):
+        import wavefile
+        assert 0 <= frames <= self.frame_count
+        with wavefile.WaveReader(self.file_path) as reader:
+            reader.seek(frames)
+            iterator = reader.read_iter(size=1)
+            frame = next(iterator)
+            return frame.transpose().tolist()[0]
+
+    def at_percent(self, percent):
+        import wavefile
+        assert 0 <= percent <= 1
+        frames = int(self.frame_count * percent)
+        with wavefile.WaveReader(self.file_path) as reader:
+            reader.seek(frames)
+            iterator = reader.read_iter(size=1)
+            frame = next(iterator)
+            return frame.transpose().tolist()[0]
+
+    def at_second(self, second):
+        import wavefile
+        assert 0 <= second <= self.seconds
+        frames = second * self.sample_rate
+        with wavefile.WaveReader(self.file_path) as reader:
+            reader.seek(frames)
+            iterator = reader.read_iter(size=1)
+            frame = next(iterator)
+            return frame.transpose().tolist()[0]
+
     ### PUBLIC PROPERTIES ###
 
     @property
