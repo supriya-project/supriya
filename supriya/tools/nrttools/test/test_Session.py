7adeaa89fdb77f049848856c0df62d4380e2f100
Josiah Oberholtzer <joberholtzer@discogsinc.com>
2015-12-22 21:15:10 -0500

    Wrote Session.render() tests.

--- supriya/tools/nrttools/test/test_Session.py
+++ supriya/tools/nrttools/test/test_Session.py
@@ -1,7 +1,9 @@
 # -*- encoding: utf-8 -*-
 import os
 import unittest
+from abjad.tools import timespantools
 from supriya.tools import nrttools
+from supriya.tools import osctools
 from supriya.tools import soundfiletools
 from supriya.tools import synthdeftools
 from supriya.tools import ugentools
@@ -46,14 +48,14 @@ class TestCase(unittest.TestCase):
 
     def test_01(self):
         session = nrttools.Session()
-        synth_one = nrttools.Synth(self.build_synthdef(), 0, 1)
+        synth_one = nrttools.Synth(0, 1, synthdef=self.build_synthdef())
         session.insert([synth_one])
         exit_code, _ = session.render(self.output_filepath)
         self.assert_ok(exit_code, 1., 44100, 8)
 
     def test_02(self):
         session = nrttools.Session()
-        synth_one = nrttools.Synth(self.build_synthdef(), 0, 300)
+        synth_one = nrttools.Synth(0, 300, synthdef=self.build_synthdef())
         session.insert([synth_one])
         exit_code, _ = session.render(
             self.output_filepath,
@@ -61,3 +63,30 @@ class TestCase(unittest.TestCase):
             output_bus_channel_count=2,
             )
         self.assert_ok(exit_code, 300., 48000, 2)
+
+    def test_03(self):
+        session = nrttools.Session()
+        synth_one = nrttools.Synth(1, 2, synthdef=self.build_synthdef())
+        session.insert([synth_one])
+        timespan = timespantools.Timespan(0.5, 2.5)
+        assert session.to_osc_bundles(timespan=timespan) == [
+            osctools.OscBundle(
+                timestamp=0.5,
+                contents=(
+                    osctools.OscMessage(5, bytearray(b'SCgf\x00\x00\x00\x02\x00\x01 d719856d7deff2696a3f807f5dc79809\x00\x00\x00\x02C\xdc\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x06SinOsc\x02\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff\xff\x00\x00\x00\x01\x02\x03Out\x02\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')),
+                    osctools.OscMessage(9, 'd719856d7deff2696a3f807f5dc79809', 1000, 1, 0),
+                    )
+                ),
+            osctools.OscBundle(
+                timestamp=1.5,
+                contents=(
+                    osctools.OscMessage(11, 1000),
+                    )
+                ),
+            osctools.OscBundle(
+                timestamp=2.5,
+                contents=(
+                    osctools.OscMessage(0),
+                    )
+                ),
+            ]
