f50313745dbc729e2dd5f5bd6cbb0a0db4406d9a
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2016-01-13 18:39:53 -0800

    Writing NRTNodeAction transform tests.

--- supriya/tools/nonrealtimetools/test/test_NRTNodeAction.py
+++ supriya/tools/nonrealtimetools/test/test_NRTNodeAction.py
@@ -233,3 +233,66 @@ class TestCase(unittest.TestCase):
             nodes[1]: nodes[0],
             nodes[2]: nodes[0],
             }
+
+    def test_add_before_01(self):
+        nodes = [nonrealtimetools.NRTGroup(None, i) for i in range(4)]
+        nodes_to_children = {
+            nodes[0]: (nodes[1], nodes[2]),
+            nodes[1]: None,
+            nodes[2]: None,
+            }
+        nodes_to_parent = {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
+            }
+        action = nonrealtimetools.NRTNodeAction(
+            source=nodes[3],
+            target=nodes[2],
+            action=servertools.AddAction.ADD_BEFORE,
+            )
+        action.apply_transform(nodes_to_children, nodes_to_parent)
+        assert nodes_to_children == {
+            nodes[0]: (nodes[1], nodes[3], nodes[2]),
+            nodes[1]: None,
+            nodes[2]: None,
+            nodes[3]: None,
+            }
+        assert nodes_to_parent == {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
+            nodes[3]: nodes[0],
+            }
+
+    def test_add_after_01(self):
+        nodes = [nonrealtimetools.NRTGroup(None, i) for i in range(4)]
+        nodes_to_children = {
+            nodes[0]: (nodes[1], nodes[2]),
+            nodes[1]: None,
+            nodes[2]: None,
+            }
+        nodes_to_parent = {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
+            }
+        action = nonrealtimetools.NRTNodeAction(
+            source=nodes[3],
+            target=nodes[1],
+            action=servertools.AddAction.ADD_AFTER,
+            )
+        action.apply_transform(nodes_to_children, nodes_to_parent)
+        assert nodes_to_children == {
+            nodes[0]: (nodes[1], nodes[3], nodes[2]),
+            nodes[1]: None,
+            nodes[2]: None,
+            nodes[3]: None,
+            }
+        assert nodes_to_parent == {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
+            nodes[3]: nodes[0],
+            }
+
