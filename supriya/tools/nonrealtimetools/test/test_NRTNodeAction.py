910a24160920bf5dffbfa54d82dcb11ef4fd742d
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2016-01-13 18:36:44 -0800

    Writing NRTNodeAction transform tests.

--- supriya/tools/nonrealtimetools/test/test_NRTNodeAction.py
+++ supriya/tools/nonrealtimetools/test/test_NRTNodeAction.py
@@ -7,25 +7,229 @@ from supriya.tools import servertools
 class TestCase(unittest.TestCase):
 
     def test_add_to_head_01(self):
-        source = nonrealtimetools.NRTGroup(None, 1)
-        target = nonrealtimetools.NRTGroup(None, 0)
-        action = nonrealtimetools.NRTNodeAction(
-            source=source,
-            target=target,
-            action=servertools.AddAction.ADD_TO_HEAD,
-            )
+        nodes = [nonrealtimetools.NRTGroup(None, i) for i in range(2)]
         nodes_to_children = {
-            target: None,
+            nodes[0]: None,
             }
         nodes_to_parent = {
-            target: None,
+            nodes[0]: None,
             }
+        action = nonrealtimetools.NRTNodeAction(
+            source=nodes[1],
+            target=nodes[0],
+            action=servertools.AddAction.ADD_TO_HEAD,
+            )
         action.apply_transform(nodes_to_children, nodes_to_parent)
         assert nodes_to_children == {
-            source: None,
-            target: (source,),
+            nodes[0]: (nodes[1],),
+            nodes[1]: None,
             }
         assert nodes_to_parent == {
-            source: target,
-            target: None,
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            }
+
+    def test_add_to_head_02(self):
+        nodes = [nonrealtimetools.NRTGroup(None, i) for i in range(4)]
+        nodes_to_children = {
+            nodes[0]: (nodes[1], nodes[2]),
+            nodes[1]: None,
+            nodes[2]: (nodes[3],),
+            nodes[3]: None,
+            }
+        nodes_to_parent = {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
+            nodes[3]: nodes[2],
+            }
+        action = nonrealtimetools.NRTNodeAction(
+            source=nodes[3],
+            target=nodes[1],
+            action=servertools.AddAction.ADD_TO_HEAD,
+            )
+        action.apply_transform(nodes_to_children, nodes_to_parent)
+        assert nodes_to_children == {
+            nodes[0]: (nodes[1], nodes[2]),
+            nodes[1]: (nodes[3],),
+            nodes[2]: None,
+            nodes[3]: None,
+            }
+        assert nodes_to_parent == {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
+            nodes[3]: nodes[1],
+            }
+
+    def test_add_to_head_03(self):
+        nodes = [nonrealtimetools.NRTGroup(None, i) for i in range(3)]
+        nodes_to_children = {
+            nodes[0]: (nodes[1], nodes[2]),
+            nodes[1]: None,
+            nodes[2]: None,
+            }
+        nodes_to_parent = {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
+            }
+        action = nonrealtimetools.NRTNodeAction(
+            source=nodes[2],
+            target=nodes[0],
+            action=servertools.AddAction.ADD_TO_HEAD,
+            )
+        action.apply_transform(nodes_to_children, nodes_to_parent)
+        assert nodes_to_children == {
+            nodes[0]: (nodes[2], nodes[1]),
+            nodes[1]: None,
+            nodes[2]: None,
+            }
+        assert nodes_to_parent == {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
+            }
+
+    def test_add_to_head_04(self):
+        nodes = [nonrealtimetools.NRTGroup(None, i) for i in range(3)]
+        nodes_to_children = {
+            nodes[0]: (nodes[1], nodes[2]),
+            nodes[1]: None,
+            nodes[2]: None,
+            }
+        nodes_to_parent = {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
+            }
+        action = nonrealtimetools.NRTNodeAction(
+            source=nodes[1],
+            target=nodes[0],
+            action=servertools.AddAction.ADD_TO_HEAD,
+            )
+        action.apply_transform(nodes_to_children, nodes_to_parent)
+        assert nodes_to_children == {
+            nodes[0]: (nodes[1], nodes[2]),
+            nodes[1]: None,
+            nodes[2]: None,
+            }
+        assert nodes_to_parent == {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
+            }
+
+    def test_add_to_tail_01(self):
+        nodes = [nonrealtimetools.NRTGroup(None, i) for i in range(2)]
+        nodes_to_children = {
+            nodes[0]: None,
+            }
+        nodes_to_parent = {
+            nodes[0]: None,
+            }
+        action = nonrealtimetools.NRTNodeAction(
+            source=nodes[1],
+            target=nodes[0],
+            action=servertools.AddAction.ADD_TO_TAIL,
+            )
+        action.apply_transform(nodes_to_children, nodes_to_parent)
+        assert nodes_to_children == {
+            nodes[0]: (nodes[1],),
+            nodes[1]: None,
+            }
+        assert nodes_to_parent == {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            }
+
+    def test_add_to_tail_02(self):
+        nodes = [nonrealtimetools.NRTGroup(None, i) for i in range(4)]
+        nodes_to_children = {
+            nodes[0]: (nodes[1], nodes[2]),
+            nodes[1]: None,
+            nodes[2]: (nodes[3],),
+            nodes[3]: None,
+            }
+        nodes_to_parent = {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
+            nodes[3]: nodes[2],
+            }
+        action = nonrealtimetools.NRTNodeAction(
+            source=nodes[3],
+            target=nodes[1],
+            action=servertools.AddAction.ADD_TO_TAIL,
+            )
+        action.apply_transform(nodes_to_children, nodes_to_parent)
+        assert nodes_to_children == {
+            nodes[0]: (nodes[1], nodes[2]),
+            nodes[1]: (nodes[3],),
+            nodes[2]: None,
+            nodes[3]: None,
+            }
+        assert nodes_to_parent == {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
+            nodes[3]: nodes[1],
+            }
+
+    def test_add_to_tail_03(self):
+        nodes = [nonrealtimetools.NRTGroup(None, i) for i in range(3)]
+        nodes_to_children = {
+            nodes[0]: (nodes[1], nodes[2]),
+            nodes[1]: None,
+            nodes[2]: None,
+            }
+        nodes_to_parent = {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
+            }
+        action = nonrealtimetools.NRTNodeAction(
+            source=nodes[1],
+            target=nodes[0],
+            action=servertools.AddAction.ADD_TO_TAIL,
+            )
+        action.apply_transform(nodes_to_children, nodes_to_parent)
+        assert nodes_to_children == {
+            nodes[0]: (nodes[2], nodes[1]),
+            nodes[1]: None,
+            nodes[2]: None,
+            }
+        assert nodes_to_parent == {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
+            }
+
+    def test_add_to_tail_04(self):
+        nodes = [nonrealtimetools.NRTGroup(None, i) for i in range(3)]
+        nodes_to_children = {
+            nodes[0]: (nodes[1], nodes[2]),
+            nodes[1]: None,
+            nodes[2]: None,
+            }
+        nodes_to_parent = {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
+            }
+        action = nonrealtimetools.NRTNodeAction(
+            source=nodes[2],
+            target=nodes[0],
+            action=servertools.AddAction.ADD_TO_TAIL,
+            )
+        action.apply_transform(nodes_to_children, nodes_to_parent)
+        assert nodes_to_children == {
+            nodes[0]: (nodes[1], nodes[2]),
+            nodes[1]: None,
+            nodes[2]: None,
+            }
+        assert nodes_to_parent == {
+            nodes[0]: None,
+            nodes[1]: nodes[0],
+            nodes[2]: nodes[0],
             }
