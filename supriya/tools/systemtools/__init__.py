49f666ccb1f71ce7e6a6eb5349bff6610b82ab67
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2016-11-06 00:30:58 -0700

    Added Trellis to systemtools.

--- supriya/tools/systemtools/__init__.py
+++ supriya/tools/systemtools/__init__.py
@@ -10,5 +10,4 @@ from supriya.tools.systemtools.Enumeration import Enumeration
 from supriya.tools.systemtools.PubSub import PubSub
 from supriya.tools.systemtools.SupriyaObject import SupriyaObject
 from supriya.tools.systemtools.SupriyaValueObject import SupriyaValueObject
-from supriya.tools.systemtools.run_supriya import run_supriya
-from supriya.tools.systemtools.run_supriya_web import run_supriya_web
+from supriya.tools.systemtools.Trellis import Trellis
