d6f04900f100ee0c334e2f17268a5dbdca4c788f
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2017-04-26 20:25:44 -0700

    Added repeatable RNG NRT tests.

---
+++ supriya/tools/nonrealtimetools/test/test_Session_repeatable_random_numbers.py
@@ -0,0 +1,132 @@
+# -*- encoding: utf-8 -*-
+from supriya.tools import nonrealtimetools
+from supriya.tools import synthdeftools
+from supriya.tools import ugentools
+from nonrealtimetools_testbase import TestCase
+
+
+class TestCase(TestCase):
+
+    with synthdeftools.SynthDefBuilder() as builder:
+        source = ugentools.WhiteNoise.ar()
+        ugentools.Out.ar(bus=0, source=source)
+    nonrepeatable_noise_synthdef = builder.build()
+
+    with synthdeftools.SynthDefBuilder(rand_id=0, rand_seed=0) as builder:
+        ugentools.RandID.ir(rand_id=builder['rand_id'])
+        ugentools.RandSeed.ir(seed=builder['rand_seed'], trigger=1)
+        source = ugentools.WhiteNoise.ar()
+        ugentools.Out.ar(bus=0, source=source)
+    repeatable_noise_synthdef = builder.build()
+
+    with synthdeftools.SynthDefBuilder(rand_id=0, rand_seed=0) as builder:
+        ugentools.RandID.ir(rand_id=builder['rand_id'])
+        ugentools.RandSeed.ir(seed=builder['rand_seed'], trigger=1)
+    seed_synthdef = builder.build()
+
+    with synthdeftools.SynthDefBuilder(rand_id=0, rand_seed=0) as builder:
+        ugentools.RandID.ir(rand_id=builder['rand_id'])
+        source = ugentools.WhiteNoise.ar()
+        ugentools.Out.ar(bus=0, source=source)
+    maybe_repeatable_noise_synthdef = builder.build()
+
+    def test_nonrepeatable(self):
+        session = nonrealtimetools.Session(0, 1)
+        with session.at(0):
+            session.add_synth(
+                duration=1,
+                synthdef=self.nonrepeatable_noise_synthdef,
+                )
+        exit_code, output_file_path = session.render()
+        self.assert_ok(exit_code, 1, 44100, 1, file_path=output_file_path)
+        sampled_session = self._sample(output_file_path)
+        for _ in range(10):
+            output_file_path.unlink()
+            exit_code, output_file_path = session.render()
+            self.assert_ok(exit_code, 1, 44100, 1, file_path=output_file_path)
+            assert self._sample(output_file_path) != sampled_session
+
+    def test_repeatable(self):
+        session = nonrealtimetools.Session(0, 1)
+        with session.at(0):
+            session.add_synth(
+                duration=1,
+                synthdef=self.repeatable_noise_synthdef,
+                )
+        exit_code, output_file_path = session.render()
+        self.assert_ok(exit_code, 1, 44100, 1, file_path=output_file_path)
+        sampled_session = self._sample(output_file_path)
+        for _ in range(10):
+            output_file_path.unlink()
+            exit_code, output_file_path = session.render()
+            self.assert_ok(exit_code, 1, 44100, 1, file_path=output_file_path)
+            assert self._sample(output_file_path) == sampled_session
+
+    def test_maybe_repeatable_but_wasnt(self):
+        """
+        Setting a RandID isn't enough. The RNG will be initialized randomly.
+        """
+        session = nonrealtimetools.Session(0, 1)
+        with session.at(0):
+            session.add_synth(
+                duration=1,
+                synthdef=self.maybe_repeatable_noise_synthdef,
+                )
+        exit_code, output_file_path = session.render()
+        self.assert_ok(exit_code, 1, 44100, 1, file_path=output_file_path)
+        sampled_session = self._sample(output_file_path)
+        for _ in range(10):
+            output_file_path.unlink()
+            exit_code, output_file_path = session.render()
+            self.assert_ok(exit_code, 1, 44100, 1, file_path=output_file_path)
+            assert self._sample(output_file_path) != sampled_session
+
+    def test_maybe_repeatable_and_was(self):
+        """
+        Setting a RandID and seeding it in another synth locks us in.
+        """
+        session = nonrealtimetools.Session(0, 1)
+        with session.at(0):
+            session.add_synth(
+                duration=1,
+                synthdef=self.maybe_repeatable_noise_synthdef,
+                )
+            session.add_synth(
+                add_action='ADD_TO_HEAD',
+                duration=0,
+                synthdef=self.seed_synthdef,
+                )
+        exit_code, output_file_path = session.render()
+        self.assert_ok(exit_code, 1, 44100, 1, file_path=output_file_path)
+        sampled_session = self._sample(output_file_path)
+        for _ in range(10):
+            output_file_path.unlink()
+            exit_code, output_file_path = session.render()
+            self.assert_ok(exit_code, 1, 44100, 1, file_path=output_file_path)
+            assert self._sample(output_file_path) == sampled_session
+
+    def test_maybe_repeatable_and_almost_was(self):
+        """
+        Seeding is sensitive to node order.
+        """
+        session = nonrealtimetools.Session(0, 1)
+        with session.at(0):
+            session.add_synth(
+                duration=1,
+                synthdef=self.maybe_repeatable_noise_synthdef,
+                )
+            session.add_synth(
+                add_action='ADD_TO_TAIL',
+                duration=0,
+                synthdef=self.seed_synthdef,
+                )
+        exit_code, output_file_path = session.render()
+        self.assert_ok(exit_code, 1, 44100, 1, file_path=output_file_path)
+        first_sampled_session = sorted(self._sample(output_file_path).items())
+        for _ in range(10):
+            output_file_path.unlink()
+            exit_code, output_file_path = session.render()
+            self.assert_ok(exit_code, 1, 44100, 1, file_path=output_file_path)
+            sampled_session = sorted(self._sample(output_file_path).items())
+            assert first_sampled_session[0] != sampled_session[0]
+            assert first_sampled_session[1:] == sampled_session[1:]
