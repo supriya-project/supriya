a35e0068bb779cb5ccdd3cf198959f5698ebee54
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2017-05-27 22:52:25 -0700

    Add ProjectManager utility class.

---
+++ supriya/tools/commandlinetools/ProjectManager.py
@@ -0,0 +1,24 @@
+import importlib
+import pathlib
+
+
+class ProjectManager(object):
+
+    @staticmethod
+    def import_section_objects(section, file_path, namespace):
+        section_path = pathlib.Path(file_path).parent
+        root_path = section_path.parent
+        for path in section_path.iterdir():
+            if not path.is_dir():
+                continue
+            elif not (path / '__init__.py').exists():
+                continue
+            elif not (path / 'definition.py').exists():
+                continue
+            name = path.name
+            path = path / 'definition'
+            path_parts = (root_path.name,) + path.relative_to(root_path).parts
+            module_path = '.'.join(path_parts)
+            module = importlib.import_module(module_path)
+            member = getattr(module, section)
+            namespace[name] = member
