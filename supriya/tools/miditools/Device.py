095240976c31bcac958df01f382c1fb4f828f18d
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2017-06-24 10:39:43 -0700

    Refactoring MIDI.

--- supriya/tools/miditools/Device.py
+++ supriya/tools/miditools/Device.py
@@ -1,11 +1,12 @@
+import collections
 import logging
 import pathlib
 import rtmidi
 import threading
 import yaml
+from supriya.tools import systemtools
 from supriya.tools.miditools.View import View
 from supriya.tools.miditools.LogicalControl import LogicalControl
-from supriya.tools.miditools.LogicalManifest import LogicalManifest
 from supriya.tools.miditools.PhysicalControl import PhysicalControl
 
 
@@ -39,7 +40,7 @@ class Device:
         self._physical_controls_by_group = {}
         self._physical_controls_by_command = {}
         self._initialize_physical_controls()
-        self._logical_manifest = LogicalManifest(self)
+        self._initialize_logical_controls()
 
     ### SPECIAL METHODS ###
 
@@ -53,12 +54,109 @@ class Device:
 
     ### PRIVATE METHODS ###
 
+    def _build_modal_view(self, node_template, parents):
+        nodes = []
+        toggle_id = 'root:{}'.format(node_template['modal'])
+        all_toggles = self._node_instances[toggle_id]
+        for parent, toggles in zip(parents, all_toggles):
+            modal_view = View(name=node_template['name'], visible=True)
+            parent.add_child(modal_view)
+            for i, toggle in enumerate(toggles.children.values()):
+                view = View(
+                    name=i,
+                    visible=i == 0,
+                    )
+                nodes.append(view)
+                modal_view.add_child(view)
+                self._dependents.setdefault(toggle, []).append(view)
+        return nodes
+
+    def _build_mutex_view(self, node_template, parents):
+        nodes = []
+        physical_controls = []
+        physical_control_ids = node_template['children']
+        for physical_control_id in physical_control_ids:
+            physical_controls.extend(
+                self._get_controls_by_name(
+                    physical_control_id))
+        for parent in parents:
+            view = View(name=node_template['name'], mode='mutex')
+            nodes.append(view)
+            parent.add_child(view)
+            for i, physical_control in enumerate(physical_controls):
+                control = LogicalControl(
+                    name=i,
+                    mode='toggle',
+                    physical_control=physical_control,
+                    )
+                if i == 0:
+                    control.value = 1.0
+                view.add_child(control)
+        return nodes
+
+    def _build_view(self, node_template, parents):
+        pass
+
+    def _build_physical_controls(self, node_template, parents):
+        nodes = []
+        physical_controls = []
+        physical_control_ids = node_template['physical_control']
+        if isinstance(physical_control_ids, str):
+            physical_control_ids = [physical_control_ids]
+        for physical_control_id in physical_control_ids:
+            physical_controls.extend(
+                self._get_controls_by_name(
+                    physical_control_id))
+        for parent in parents:
+            for i, physical_control in enumerate(physical_controls, 1):
+                if 'name' in node_template:
+                    if node_template['name']:
+                        name = node_template['name']
+                        if len(physical_controls) > 1:
+                            name = '{}_{}'.format(name, i)
+                    else:
+                        name = i - 1
+                else:
+                    name = physical_control.name
+                mode = node_template.get('mode')
+                logical_control = LogicalControl(
+                    mode=mode,
+                    name=name,
+                    physical_control=physical_control,
+                    )
+                nodes.append(logical_control)
+                parent.add_child(logical_control)
+        return nodes
+
     def _get_controls_by_name(self, name):
         if name in self.physical_controls_by_group:
             return self.physical_controls_by_group[name]
         elif name in self.physical_controls:
             return [self.physical_controls[name]]
         raise KeyError
+
+    def _initialize_logical_controls(self):
+        device_manifest = self._device_manifest['device']
+        manifest = device_manifest['logical_controls']
+        self._node_templates = self._linearize_manifest(manifest)
+        self._node_instances = {}
+        self._node_instances['root'] = [View(name='root')]
+        self._dependents = {}
+        for parentage_string, node_template in self._node_templates.items():
+            parents = self._node_instances[parentage_string.rpartition(':')[0]]
+            if 'children' in node_template:
+                if 'modal' in node_template:
+                    nodes = self._build_modal_view(node_template, parents)
+                elif node_template.get('mode') == 'mutex':
+                    nodes = self._build_mutex_view(node_template, parents)
+                else:
+                    nodes = self._build_view(node_template, parents)
+            elif 'physical_control' in node_template:
+                nodes = self._build_physical_controls(node_template, parents)
+            else:
+                raise Exception(parentage_string, node_template)
+            self._node_instances[parentage_string] = nodes
+        self.rebuild_visibility_mapping()
 
     def _initialize_physical_controls(self):
         device_manifest = self._device_manifest['device']
@@ -147,6 +245,22 @@ class Device:
         self._physical_controls_by_command[key] = physical_control
         return physical_control
 
+    def _linearize_manifest(self, manifest):
+        trellis = systemtools.Trellis()
+        entries_by_parentage = {}
+        self._recurse_manifest(
+            entries_by_parentage=entries_by_parentage,
+            manifest=manifest,
+            parentage=('root',),
+            trellis=trellis,
+            )
+        templates = collections.OrderedDict()
+        for parentage_string in trellis:
+            if parentage_string == 'root':
+                continue
+            templates[parentage_string] = entries_by_parentage[parentage_string]
+        return templates
+
     def _process_one(self, message, timestamp):
         from supriya.tools import miditools
         if timestamp is None:
@@ -171,7 +285,7 @@ class Device:
         return control, value
 
     def _process_two(self, physical_control, value):
-        logical_control = self.logical_manifest.visibility_mapping.get(
+        logical_control = self.visibility_mapping.get(
             physical_control)
         if not logical_control:
             return
@@ -191,17 +305,16 @@ class Device:
                 ][0]
             if old_logical_control is new_logical_control:
                 return
-            lm = self.logical_manifest
-            old_mapping = set(lm.rebuild_visibility_mapping().values())
-            for dependent in self.logical_manifest.dependents.get(
+            old_mapping = set(self.rebuild_visibility_mapping().values())
+            for dependent in self.dependents.get(
                 old_logical_control, []):
                 dependent.visible = False
             old_logical_control.value = 0.0
             new_logical_control.value = 1.0
-            for dependent in self.logical_manifest.dependents.get(
+            for dependent in self.dependents.get(
                 logical_control, []):
                 dependent.visible = True
-            new_mapping = set(lm.rebuild_visibility_mapping().values())
+            new_mapping = set(self.rebuild_visibility_mapping().values())
             for logical_control in old_mapping - new_mapping:
                 logical_control.unmount()
             for logical_control in new_mapping - old_mapping:
@@ -216,6 +329,42 @@ class Device:
         elif logical_control.mode == LogicalControl.Mode.CONTINUOUS:
             logical_control.physical_control.set_led(value * 127)
             logical_control.value = value
+
+    def _recurse_manifest(
+        self,
+        entries_by_parentage,
+        manifest,
+        parentage,
+        trellis,
+        ):
+        for entry in manifest:
+            entry_name = entry.get('name') or entry.get('physical_control')
+            assert entry_name
+            entry_parentage = parentage + (entry_name,)
+            entry_parentage_string = ':'.join(entry_parentage)
+            assert entry_parentage_string not in entries_by_parentage
+            entries_by_parentage[entry_parentage_string] = entry
+            if parentage:
+                parentage_string = ':'.join(parentage)
+                trellis.add(
+                    parentage_string,
+                    entry_parentage_string,
+                    )
+            else:
+                trellis.add(entry_parentage_string)
+            if 'modal' in entry:
+                trellis.add(
+                    'root:{}'.format(entry.get('modal')),
+                    entry_parentage_string,
+                    )
+            children = entry.get('children', [])
+            if entry.get('mode') != 'mutex':
+                self._recurse_manifest(
+                    entries_by_parentage=entries_by_parentage,
+                    manifest=children,
+                    parentage=entry_parentage,
+                    trellis=trellis,
+                    )
 
     ### PUBLIC METHODS ###
 
@@ -255,16 +404,29 @@ class Device:
         self._midi_in.set_callback(self.__call__)
         for message in self._device_manifest['device'].get('on_startup', []):
             self.send_message(message)
-        mapping = self.logical_manifest.rebuild_visibility_mapping()
+        mapping = self.rebuild_visibility_mapping()
         for logical_control in mapping.values():
             logical_control.mount()
         return self
+
+    def rebuild_visibility_mapping(self):
+        mapping = collections.OrderedDict()
+        for logical_control in self.root_view.yield_visible_controls():
+            physical_control = logical_control.physical_control
+            assert physical_control not in mapping
+            mapping[physical_control] = logical_control
+        self._visibility_mapping = mapping
+        return mapping
 
     def send_message(self, message):
         self._midi_out.send_message(message)
         self.logger.debug('MIDI O: 0x{}'.format(bytearray(message).hex()))
 
     ### PUBLIC PROPERTIES ###
+
+    @property
+    def dependents(self):
+        return self._dependents
 
     @property
     def physical_controls(self):
@@ -283,9 +445,9 @@ class Device:
         return self._logger
 
     @property
-    def logical_manifest(self):
-        return self._logical_manifest
+    def root_view(self):
+        return self._node_instances['root'][0]
 
     @property
-    def root_view(self):
-        return self._logical_manifest.root_view
+    def visibility_mapping(self):
+        return self._visibility_mapping
