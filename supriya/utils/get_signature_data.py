08de892afc2117b878c0a15c26cb1f88254548da
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2017-08-06 13:25:29 -0700

    Hardening new().

---
+++ supriya/utils/get_signature_data.py
@@ -0,0 +1,42 @@
+import collections
+import inspect
+
+
+def get_signature_data(expr):
+    try:
+        signature = inspect.signature(expr)
+    except TypeError:
+        signature = inspect.signature(expr.__init__)
+    args = collections.OrderedDict()
+    var_args = []
+    kwargs = {}
+    for name, parameter in signature.parameters.items():
+        if parameter.kind is inspect._POSITIONAL_ONLY:
+            try:
+                args[name] = getattr(expr, name)
+            except AttributeError:
+                args[name] = expr[name]
+        elif parameter.kind is inspect._POSITIONAL_OR_KEYWORD:
+            try:
+                args[name] = getattr(expr, name)
+            except AttributeError:
+                args[name] = expr[name]
+        elif parameter.kind is inspect._VAR_POSITIONAL:
+            try:
+                var_args.extend(expr[:])
+            except TypeError:
+                var_args.extend(getattr(expr, name))
+        elif parameter.kind is inspect._KEYWORD_ONLY:
+            try:
+                kwargs[name] = getattr(expr, name)
+            except AttributeError:
+                kwargs[name] = expr[name]
+        elif parameter.kind is inspect._VAR_KEYWORD:
+            if hasattr(expr, 'items'):
+                items = expr.items()
+            else:
+                items = getattr(expr, name).items()
+            for key, value in items:
+                if key not in args:
+                    kwargs[key] = value
+    return args, var_args, kwargs
--- supriya/utils/new.py
+++ supriya/utils/new.py
@@ -1,45 +1,15 @@
-import inspect
-
-
 def new(expr, *args, **kwargs):
     """
     Template an object.
     """
-    #print('I', expr, args, kwargs)
-    try:
-        signature = inspect.signature(expr)
-    except TypeError:
-        signature = inspect.signature(expr.__init__)
-    new_args = []
-    new_kwargs = {}
-    for name, parameter in signature.parameters.items():
-        #print('    P', name, repr(parameter))
-        if parameter.kind is inspect._POSITIONAL_ONLY:
-            try:
-                new_args.append(getattr(expr, name))
-            except AttributeError:
-                new_args.append(expr[name])
-        elif parameter.kind is inspect._POSITIONAL_OR_KEYWORD:
-            try:
-                new_kwargs[name] = getattr(expr, name)
-            except AttributeError:
-                new_kwargs[name] = expr[name]
-        elif parameter.kind is inspect._VAR_POSITIONAL:
-            try:
-                new_args.extend(expr[:])
-            except TypeError:
-                new_args.extend(getattr(expr, name))
-        elif parameter.kind is inspect._KEYWORD_ONLY:
-            try:
-                new_kwargs[name] = getattr(expr, name)
-            except AttributeError:
-                new_kwargs[name] = expr[name]
-        elif parameter.kind is inspect._VAR_KEYWORD:
-            if hasattr(expr, 'items'):
-                new_kwargs.update(expr)
-            else:
-                new_kwargs.update(getattr(expr, name))
+    from supriya import utils
+    new_args, new_var_args, new_kwargs = utils.get_signature_data(expr)
     if args:
-        new_args = args
-    new_kwargs.update(kwargs)
+        new_var_args = args
+    for key, value in kwargs.items():
+        if key in new_args:
+            new_args[key] = value
+        else:
+            new_kwargs[key] = value
+    new_args = list(new_args.values()) + list(new_var_args)
     return type(expr)(*new_args, **new_kwargs)
