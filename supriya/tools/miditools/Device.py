f0e53bdb6d0513b946e4d922a16d27c82ac3245f
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2017-09-05 17:46:22 -0700

    Improve Device port opening.

--- supriya/tools/miditools/Device.py
+++ supriya/tools/miditools/Device.py
@@ -402,7 +402,14 @@ class Device:
             self._midi_in.open_virtual_port()
             self._midi_out.open_virtual_port()
         else:
-            port = port or self._device_manifest.get('port')
+            if port is None:
+                port = self._device_manifest.get('port')
+                if isinstance(port, str):
+                    port_names = self.get_ports()
+                    if port in port_names:
+                        port = port_names.index(port)
+                    else:
+                        port = None
             self._midi_in.open_port(port)
             self._midi_out.open_port(port)
         self._midi_in.ignore_types(
