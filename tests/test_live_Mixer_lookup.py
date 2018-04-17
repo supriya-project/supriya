86c9ed2fd0cc9cc6cd20663a5afa9fa38827c70b
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2018-04-17 12:41:21 -0400

    Backing-out supriya.system.TestCase.

--- tests/test_live_Mixer_lookup.py
+++ tests/test_live_Mixer_lookup.py
@@ -1,23 +1,20 @@
 import supriya.live
-import supriya.system
 
 
-class TestCase(supriya.system.TestCase):
-
-    def test_01(self):
-        mixer = supriya.live.Mixer(1, 1)
-        mixer.add_track('foo')
-        mixer.add_track('bar')
-        mixer['foo'].add_synth_slot(
-            'synth',
-            frequency=443,
-            )
-        assert mixer.lookup('allow_multiple') is mixer.allow_multiple
-        assert mixer.lookup('foo') is mixer['foo']
-        assert mixer.lookup('bar') is mixer['bar']
-        assert mixer.lookup('0') is mixer['foo']
-        assert mixer.lookup('1') is mixer['bar']
-        assert mixer.lookup('foo:synth') is mixer['foo']['synth']
-        assert mixer.lookup('foo:synth:frequency') is \
-            mixer['foo']['synth']['frequency']
-        assert mixer.lookup('foo:synth.play') is mixer['foo']['synth'].play
+def test_01():
+    mixer = supriya.live.Mixer(1, 1)
+    mixer.add_track('foo')
+    mixer.add_track('bar')
+    mixer['foo'].add_synth_slot(
+        'synth',
+        frequency=443,
+        )
+    assert mixer.lookup('allow_multiple') is mixer.allow_multiple
+    assert mixer.lookup('foo') is mixer['foo']
+    assert mixer.lookup('bar') is mixer['bar']
+    assert mixer.lookup('0') is mixer['foo']
+    assert mixer.lookup('1') is mixer['bar']
+    assert mixer.lookup('foo:synth') is mixer['foo']['synth']
+    assert mixer.lookup('foo:synth:frequency') is \
+        mixer['foo']['synth']['frequency']
+    assert mixer.lookup('foo:synth.play') is mixer['foo']['synth'].play
