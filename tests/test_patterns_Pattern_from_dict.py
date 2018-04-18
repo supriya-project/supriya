e4c342b89efd525cb5195e635f7a607d642059a7
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2018-04-18 12:58:48 -0400

    Backing-out supriya.system.TestCase.

--- tests/test_patterns_Pattern_from_dict.py
+++ tests/test_patterns_Pattern_from_dict.py
@@ -1,131 +1,131 @@
 import supriya.patterns
-import supriya.system
 import uqbar.strings
 import yaml
 
 
-class TestCase(supriya.system.TestCase):
+def test_01():
+    string = uqbar.strings.normalize("""
+    pattern:
+        type: Pwhite
+    """)
+    dict_ = yaml.load(string)
+    pattern = supriya.patterns.Pattern.from_dict(dict_['pattern'])
+    assert repr(pattern) == uqbar.strings.normalize("""
+        Pwhite()
+        """)
 
-    def test_01(self):
-        string = uqbar.strings.normalize("""
-        pattern:
+
+def test_02():
+    string = uqbar.strings.normalize("""
+    pattern:
+        type: Pbind
+        frequency: $args.frequency
+        amplitude:
             type: Pwhite
-        """)
-        dict_ = yaml.load(string)
-        pattern = supriya.patterns.Pattern.from_dict(dict_['pattern'])
-        assert repr(pattern) == uqbar.strings.normalize("""
-            Pwhite()
-            """)
-
-    def test_02(self):
-        string = uqbar.strings.normalize("""
-        pattern:
-            type: Pbind
-            frequency: $args.frequency
-            amplitude:
-                type: Pwhite
-            pan: $args.pan
-            duration:
-                type: Pwhite
-                minimum: $args.duration_min
-                maximum: $args.duration_max
-        """)
-        dict_ = yaml.load(string)
-        namespaces = dict(
-            args=dict(
-                duration_max=11,
-                duration_min=0.25,
-                frequency=443,
-                pan=0.1,
-                )
+        pan: $args.pan
+        duration:
+            type: Pwhite
+            minimum: $args.duration_min
+            maximum: $args.duration_max
+    """)
+    dict_ = yaml.load(string)
+    namespaces = dict(
+        args=dict(
+            duration_max=11,
+            duration_min=0.25,
+            frequency=443,
+            pan=0.1,
             )
-        pattern = supriya.patterns.Pattern.from_dict(
-            dict_['pattern'], namespaces=namespaces)
-        assert repr(pattern) == uqbar.strings.normalize("""
-            Pbind(
-                amplitude=Pwhite(),
-                duration=Pwhite(
-                    maximum=11,
-                    minimum=0.25,
-                    ),
-                frequency=443,
-                pan=0.1,
-                )
-            """)
-
-    def test_03(self):
-        string = uqbar.strings.normalize("""
-        pattern:
-            type: Pbind
-            frequency: $args.frequency
-            amplitude:
-                type: Pwhite
-            pan: $args.pan
-            duration:
-                type: Pwhite
-                minimum: $args.duration_min
-                maximum: $args.duration_max
-            buffer_id: $buffers.birds
-        """)
-        dict_ = yaml.load(string)
-        namespaces = dict(
-            args=dict(
-                duration_max=11,
-                duration_min=0.25,
-                frequency=443,
-                pan=0.1,
+        )
+    pattern = supriya.patterns.Pattern.from_dict(
+        dict_['pattern'], namespaces=namespaces)
+    assert repr(pattern) == uqbar.strings.normalize("""
+        Pbind(
+            amplitude=Pwhite(),
+            duration=Pwhite(
+                maximum=11,
+                minimum=0.25,
                 ),
-            buffers=dict(
-                birds=[1, 2, 3],
-                ),
+            frequency=443,
+            pan=0.1,
             )
-        pattern = supriya.patterns.Pattern.from_dict(
-            dict_['pattern'], namespaces=namespaces)
-        assert repr(pattern) == uqbar.strings.normalize("""
-            Pbind(
-                amplitude=Pwhite(),
-                buffer_id=[1, 2, 3],
-                duration=Pwhite(
-                    maximum=11,
-                    minimum=0.25,
-                    ),
-                frequency=443,
-                pan=0.1,
-                )
-            """)
-
-    def test_04(self):
-        string = uqbar.strings.normalize("""
-        pattern:
-            type: Pbind
-            frequency: $args.frequency
-            amplitude:
-                type: Pwhite
-            pan: $args.pan
-            duration:
-                type: Pwhite
-                minimum: $args.duration_min
-                maximum: $args.duration_max
         """)
-        dict_ = yaml.load(string)
-        namespaces = dict(
-            args=supriya.system.BindableNamespace(
-                duration_max=11,
-                duration_min=0.25,
-                frequency=443,
-                pan=0.1,
-                )
+
+
+def test_03():
+    string = uqbar.strings.normalize("""
+    pattern:
+        type: Pbind
+        frequency: $args.frequency
+        amplitude:
+            type: Pwhite
+        pan: $args.pan
+        duration:
+            type: Pwhite
+            minimum: $args.duration_min
+            maximum: $args.duration_max
+        buffer_id: $buffers.birds
+    """)
+    dict_ = yaml.load(string)
+    namespaces = dict(
+        args=dict(
+            duration_max=11,
+            duration_min=0.25,
+            frequency=443,
+            pan=0.1,
+            ),
+        buffers=dict(
+            birds=[1, 2, 3],
+            ),
+        )
+    pattern = supriya.patterns.Pattern.from_dict(
+        dict_['pattern'], namespaces=namespaces)
+    assert repr(pattern) == uqbar.strings.normalize("""
+        Pbind(
+            amplitude=Pwhite(),
+            buffer_id=[1, 2, 3],
+            duration=Pwhite(
+                maximum=11,
+                minimum=0.25,
+                ),
+            frequency=443,
+            pan=0.1,
             )
-        pattern = supriya.patterns.Pattern.from_dict(
-            dict_['pattern'], namespaces=namespaces)
-        assert repr(pattern) == uqbar.strings.normalize("""
-            Pbind(
-                amplitude=Pwhite(),
-                duration=Pwhite(
-                    maximum=BindableFloat(11.0),
-                    minimum=BindableFloat(0.25),
-                    ),
-                frequency=BindableFloat(443.0),
-                pan=BindableFloat(0.1),
-                )
-            """)
+        """)
+
+
+def test_04():
+    string = uqbar.strings.normalize("""
+    pattern:
+        type: Pbind
+        frequency: $args.frequency
+        amplitude:
+            type: Pwhite
+        pan: $args.pan
+        duration:
+            type: Pwhite
+            minimum: $args.duration_min
+            maximum: $args.duration_max
+    """)
+    dict_ = yaml.load(string)
+    namespaces = dict(
+        args=supriya.system.BindableNamespace(
+            duration_max=11,
+            duration_min=0.25,
+            frequency=443,
+            pan=0.1,
+            )
+        )
+    pattern = supriya.patterns.Pattern.from_dict(
+        dict_['pattern'], namespaces=namespaces)
+    assert repr(pattern) == uqbar.strings.normalize("""
+        Pbind(
+            amplitude=Pwhite(),
+            duration=Pwhite(
+                maximum=BindableFloat(11.0),
+                minimum=BindableFloat(0.25),
+                ),
+            frequency=BindableFloat(443.0),
+            pan=BindableFloat(0.1),
+            )
+        """)
