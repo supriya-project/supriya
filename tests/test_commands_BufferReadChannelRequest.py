1be6d3fa779842ce6025b6cad13c07442e13f6ba
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2018-04-21 11:34:51 -0400

    Backing-out unittest.TestCase.

--- tests/test_commands_BufferReadChannelRequest.py
+++ tests/test_commands_BufferReadChannelRequest.py
@@ -1,39 +1,37 @@
-import unittest
 import supriya.nonrealtime
 import supriya.commands
 import supriya.soundfiles
 
 
-class TestCase(unittest.TestCase):
+def test_Session():
+    session = supriya.nonrealtime.Session()
+    request = supriya.commands.BufferReadChannelRequest(
+        buffer_id=1,
+        channel_indices=[4, 5],
+        file_path=session,
+        frame_count=512,
+        leave_open=True,
+        starting_frame_in_buffer=0,
+        starting_frame_in_file=0,
+        )
+    assert request.file_path is session
+    osc_message = request.to_osc_message(with_textual_osc_command=True)
+    assert osc_message.address == '/b_readChannel'
+    assert osc_message.contents == (1, session, 0, 512, 0, 1, 4, 5)
 
-    def test_Session(self):
-        session = supriya.nonrealtime.Session()
-        request = supriya.commands.BufferReadChannelRequest(
-            buffer_id=1,
-            channel_indices=[4, 5],
-            file_path=session,
-            frame_count=512,
-            leave_open=True,
-            starting_frame_in_buffer=0,
-            starting_frame_in_file=0,
-            )
-        assert request.file_path is session
-        osc_message = request.to_osc_message(with_textual_osc_command=True)
-        assert osc_message.address == '/b_readChannel'
-        assert osc_message.contents == (1, session, 0, 512, 0, 1, 4, 5)
 
-    def test_Say(self):
-        say = supriya.soundfiles.Say('Some text.')
-        request = supriya.commands.BufferReadChannelRequest(
-            buffer_id=1,
-            channel_indices=[4, 5],
-            file_path=say,
-            frame_count=512,
-            leave_open=True,
-            starting_frame_in_buffer=0,
-            starting_frame_in_file=0,
-            )
-        assert request.file_path is say
-        osc_message = request.to_osc_message(with_textual_osc_command=True)
-        assert osc_message.address == '/b_readChannel'
-        assert osc_message.contents == (1, say, 0, 512, 0, 1, 4, 5)
+def test_Say():
+    say = supriya.soundfiles.Say('Some text.')
+    request = supriya.commands.BufferReadChannelRequest(
+        buffer_id=1,
+        channel_indices=[4, 5],
+        file_path=say,
+        frame_count=512,
+        leave_open=True,
+        starting_frame_in_buffer=0,
+        starting_frame_in_file=0,
+        )
+    assert request.file_path is say
+    osc_message = request.to_osc_message(with_textual_osc_command=True)
+    assert osc_message.address == '/b_readChannel'
+    assert osc_message.contents == (1, say, 0, 512, 0, 1, 4, 5)
