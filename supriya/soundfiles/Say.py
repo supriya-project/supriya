11784d802f5176eb0feae643781fcc2f1aed2f86
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2018-11-24 18:42:14 -0500

    Reformat supriya/soundfiles/

--- supriya/soundfiles/Say.py
+++ supriya/soundfiles/Say.py
@@ -12,22 +12,58 @@ class Say(SupriyaValueObject):
     ### CLASS VARIABLES ###
 
     _voices = (
-        'Alex', 'Alice', 'Alva', 'Amelie', 'Anna', 'Carmit', 'Damayanti',
-        'Daniel', 'Diego', 'Ellen', 'Fiona', 'Fred', 'Ioana', 'Joana', 'Jorge',
-        'Juan', 'Kanya', 'Karen', 'Kyoko', 'Laura', 'Lekha', 'Luca', 'Luciana',
-        'Maged', 'Mariska', 'Mei-Jia', 'Melina', 'Milena', 'Moira', 'Monica',
-        'Nora', 'Paulina', 'Samantha', 'Sara', 'Satu', 'Sin-ji', 'Tessa',
-        'Thomas', 'Ting-Ting', 'Veena', 'Victoria', 'Xander', 'Yelda', 'Yuna',
-        'Yuri', 'Zosia', 'Zuzana',
-        )
+        'Alex',
+        'Alice',
+        'Alva',
+        'Amelie',
+        'Anna',
+        'Carmit',
+        'Damayanti',
+        'Daniel',
+        'Diego',
+        'Ellen',
+        'Fiona',
+        'Fred',
+        'Ioana',
+        'Joana',
+        'Jorge',
+        'Juan',
+        'Kanya',
+        'Karen',
+        'Kyoko',
+        'Laura',
+        'Lekha',
+        'Luca',
+        'Luciana',
+        'Maged',
+        'Mariska',
+        'Mei-Jia',
+        'Melina',
+        'Milena',
+        'Moira',
+        'Monica',
+        'Nora',
+        'Paulina',
+        'Samantha',
+        'Sara',
+        'Satu',
+        'Sin-ji',
+        'Tessa',
+        'Thomas',
+        'Ting-Ting',
+        'Veena',
+        'Victoria',
+        'Xander',
+        'Yelda',
+        'Yuna',
+        'Yuri',
+        'Zosia',
+        'Zuzana',
+    )
 
     ### INITIALIZER ###
 
-    def __init__(
-        self,
-        text,
-        voice=None,
-        ):
+    def __init__(self, text, voice=None):
         self._text = str(text)
         if voice is not None:
             voice = str(voice)
@@ -37,15 +73,12 @@ class Say(SupriyaValueObject):
     ### SPECIAL METHODS ###
 
     def __render__(
-        self,
-        output_file_path=None,
-        render_directory_path=None,
-        print_transcript=None,
-        ):
+        self, output_file_path=None, render_directory_path=None, print_transcript=None
+    ):
         output_file_path = self._build_output_file_path(
             output_file_path=output_file_path,
             render_directory_path=render_directory_path,
-            )
+        )
         assert output_file_path.parent.exists()
         if output_file_path.is_absolute():
             cwd = pathlib.Path.cwd()
@@ -57,8 +90,11 @@ class Say(SupriyaValueObject):
             print('    Rendering {}'.format(relative_file_path))
         if output_file_path.exists():
             if print_transcript:
-                print('        Skipping {}. File already exists.'.format(
-                    relative_file_path))
+                print(
+                    '        Skipping {}. File already exists.'.format(
+                        relative_file_path
+                    )
+                )
             return output_file_path
         if uqbar.io.find_executable('say'):
             command_parts = ['say']
@@ -73,8 +109,11 @@ class Say(SupriyaValueObject):
             print('        Command: {}'.format(command))
         exit_code = subprocess.call(command, shell=True)
         if print_transcript:
-            print('        Rendered {} with exit code {}.'.format(
-                relative_file_path, exit_code))
+            print(
+                '        Rendered {} with exit code {}.'.format(
+                    relative_file_path, exit_code
+                )
+            )
         if exit_code:
             raise RuntimeError
         return output_file_path
@@ -88,27 +127,25 @@ class Say(SupriyaValueObject):
             md5.update(self.voice.encode())
         md5 = md5.hexdigest()
         file_path = '{}-{}.aiff'.format(
-            uqbar.strings.to_dash_case(type(self).__name__), md5,
-            )
+            uqbar.strings.to_dash_case(type(self).__name__), md5
+        )
         return pathlib.Path(file_path)
 
     def _build_output_file_path(
-        self,
-        output_file_path=None,
-        render_directory_path=None,
-        ):
+        self, output_file_path=None, render_directory_path=None
+    ):
         if output_file_path:
-            output_file_path = pathlib.Path(
-                output_file_path).expanduser().absolute()
+            output_file_path = pathlib.Path(output_file_path).expanduser().absolute()
         elif render_directory_path:
-            render_directory_path = pathlib.Path(
-                render_directory_path).expanduser().absolute()
+            render_directory_path = (
+                pathlib.Path(render_directory_path).expanduser().absolute()
+            )
             output_file_path = render_directory_path / self._build_file_path()
         else:
             output_file_path = self._build_file_path()
-            render_directory_path = pathlib.Path(
-                supriya.output_path,
-                ).expanduser().absolute()
+            render_directory_path = (
+                pathlib.Path(supriya.output_path).expanduser().absolute()
+            )
             output_file_path = render_directory_path / self._build_file_path()
         return output_file_path
 
--- supriya/soundfiles/render.py
+++ supriya/soundfiles/render.py
@@ -1,13 +1,8 @@
-def render(
-    expr,
-    output_file_path=None,
-    render_directory_path=None,
-    **kwargs
-    ):
+def render(expr, output_file_path=None, render_directory_path=None, **kwargs):
     if not hasattr(expr, '__render__'):
         raise ValueError(expr)
     return expr.__render__(
         output_file_path=output_file_path,
         render_directory_path=render_directory_path,
-        **kwargs
-        )
+        **kwargs,
+    )
