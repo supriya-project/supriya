908fbc67d93f98dc9f2f338234e0c71cd578908d
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2019-02-10 11:32:21 -0500

    Check exit codes

--- supriya/ext/ipython.py
+++ supriya/ext/ipython.py
@@ -47,20 +47,19 @@ def patch_player():
     def render(self):
         output_path = self.renderable.__render__(**self.render_kwargs)
         # HTML5 Audio element can't display AIFFs properly, but can WAVE:
-        if (
-            uqbar.io.find_executable("ffmpeg") and
-            output_path.suffix in (".aif", ".aiff")
-        ):
+        if uqbar.io.find_executable("ffmpeg") and output_path.suffix.startswith(".aif"):
             new_output_path = output_path.with_suffix(".wav")
             command = "ffmpeg -i {} {}".format(output_path, new_output_path)
-            subprocess.call(command, shell=True)
-            output_path = new_output_path
+            exit_code = subprocess.call(command, shell=True)
+            if not exit_code:
+                output_path = new_output_path
         # Convert to MP3 if possible for smaller file sizes:
         if uqbar.io.find_executable("lame"):
             new_output_path = output_path.with_suffix(".mp3")
             command = "lame -V2 {} {}".format(output_path, new_output_path)
-            subprocess.call(command, shell=True)
-            output_path = new_output_path
+            exit_code = subprocess.call(command, shell=True)
+            if not exit_code:
+                output_path = new_output_path
         return output_path
 
     def open_output_path(self, output_path):
