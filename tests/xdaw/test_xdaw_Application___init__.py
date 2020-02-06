6d783d9d73e72e912c69547df11c36fd02ff8a96
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2020-02-06 11:17:43 -0500

    Migrated xdaw into new tloen library

--- tests/xdaw/test_xdaw_Application___init__.py
+++
@@ -1,7 +0,0 @@
-from supriya.xdaw import Application
-
-
-def test_1():
-    application = Application()
-    assert len(application.contexts) == 0
-    assert application.status == Application.Status.OFFLINE
--- tests/xdaw/test_xdaw_Application_quit.py
+++
@@ -1,9 +0,0 @@
-from supriya.xdaw import Application
-
-
-def test_1():
-    application = Application.new()
-    application.boot()
-    application.quit()
-    assert application.status == Application.Status.OFFLINE
-    assert application.primary_context.provider is None
