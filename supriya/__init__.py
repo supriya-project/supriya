a5e40e62c30d354f1bba6886d005358d0b77bdc3
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2020-02-06 12:50:43 -0500

    Cleanup

--- supriya/__init__.py
+++ supriya/__init__.py
@@ -1,3 +1,7 @@
+import appdirs
+import configparser
+import pathlib
+
 try:
     import pyximport
 
@@ -5,10 +9,6 @@ try:
     del pyximport
 except ImportError:
     pass
-
-import appdirs
-import configparser  # noqa
-import pathlib  # noqa
 
 output_path = pathlib.Path(appdirs.user_cache_dir("supriya", "supriya"))
 if not output_path.exists():
@@ -34,58 +34,6 @@ with config_path.open() as file_pointer:
 del appdirs
 del configparser
 del pathlib
-
-
-def import_structured_package(path, namespace, remove=True, verbose=False):
-    import importlib
-    import inspect
-    import pathlib
-    import traceback
-
-    package_path = pathlib.Path(path).resolve().absolute()
-    if not package_path.is_dir():
-        package_path = package_path.parent()
-    # Determine the package import path
-    root_path = package_path
-    while (root_path.parent / "__init__.py").exists():
-        root_path = root_path.parent
-    relative_path = package_path.relative_to(root_path)
-    package_import_path = ".".join((root_path.name,) + relative_path.parts)
-    if verbose:
-        print(package_import_path)
-    # Find importable modules and import their nominative object
-    for module_path in sorted(package_path.iterdir()):
-        if verbose:
-            print("    {}".format(module_path))
-        if module_path.is_dir():
-            if verbose:
-                print("        Skipping...")
-            continue
-        else:
-            if module_path.suffix not in (".py", ".pyx"):
-                if verbose:
-                    print("        Skipping...")
-                continue
-            module_name = module_path.with_suffix("").name
-            if module_name == "__init__":
-                if verbose:
-                    print("        Skipping...")
-                continue
-        module_import_path = package_import_path + "." + module_name
-        if verbose:
-            print("        Importing {}:{}".format(module_import_path, module_name))
-        module = importlib.import_module(module_import_path)
-        try:
-            namespace[module_name] = getattr(module, module_name)
-        except AttributeError:
-            if verbose:
-                print("Failed:", module_path)
-                traceback.print_exc()
-    # Delete this function from the namespace
-    this_name = inspect.currentframe().f_code.co_name
-    if remove and this_name in namespace:
-        del namespace[this_name]
-
 
 from supriya._version import __version__, __version_info__  # noqa
 from supriya.enums import (  # noqa
