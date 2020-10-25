105bedb9902c749bac05de1c69214bb1f6f1e5bd
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2020-10-25 12:39:04 -0400

    Adjust dependencies

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read_version():
 
 version = read_version()
 
-install_requires = ["PyYAML", "appdirs", "sly", "tqdm", "uqbar >= 0.4.5"]
+install_requires = ["PyYAML", "appdirs", "tqdm", "uqbar >= 0.4.8"]
 
 if LooseVersion(sys.version.split()[0]) < LooseVersion("3.7.0"):
     install_requires.append("dataclasses")
