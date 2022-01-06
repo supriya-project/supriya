baa27a81c93e42dbf36c0b020045b26d6ab7ded9
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2022-01-06 13:49:31 -0500

    Adjust publish workflow, change version

--- supriya/_version.py
+++ supriya/_version.py
@@ -1,2 +1,11 @@
-__version_info__ = (0, "4b0")
+"""
+Version information.
+
+- Major version is last two digits of the current year.
+- Minor version is the month as a digit.
+- Patch / beta tag is the number of releases during that month.
+
+This follows black's versioning scheme.
+"""
+__version_info__ = (22, "1b0")
 __version__ = ".".join(str(x) for x in __version_info__)
