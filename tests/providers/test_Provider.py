d6acddeb24d8ba89aecc884e119220843a465073
Marek Miller <marek.miller@protonmail.com>
2022-10-29 15:00:01 +0200

    Test Provider.realtime() and .realtime_async()

--- tests/providers/test_Provider.py
+++ tests/providers/test_Provider.py
@@ -12,3 +12,45 @@ def test_Provider_from_context(session, server):
     assert nonrealtime_provider.session is session
     with pytest.raises(ValueError):
         Provider.from_context(23)
+
+
+def test_Provider_realtime():
+    realtime_provider = Provider.realtime()
+    assert isinstance(realtime_provider, RealtimeProvider)
+    assert realtime_provider.server.is_running
+    assert realtime_provider.server.is_owner
+    realtime_provider.server.quit()
+    assert not realtime_provider.server.is_running
+    assert not realtime_provider.server.is_owner
+
+
+def test_Provider_realtime_supernova():
+    realtime_provider = Provider.realtime(supernova=True)
+    assert isinstance(realtime_provider, RealtimeProvider)
+    assert realtime_provider.server.is_running
+    assert realtime_provider.server.is_owner
+    realtime_provider.server.quit()
+    assert not realtime_provider.server.is_running
+    assert not realtime_provider.server.is_owner
+
+
+@pytest.mark.asyncio
+async def test_Provider_realtime_async():
+    realtime_provider = await Provider.realtime_async()
+    assert isinstance(realtime_provider, RealtimeProvider)
+    assert realtime_provider.server.is_running
+    assert realtime_provider.server.is_owner
+    await realtime_provider.server.quit()
+    assert not realtime_provider.server.is_running
+    assert not realtime_provider.server.is_owner
+
+
+@pytest.mark.asyncio
+async def test_Provider_realtime_async_supernova():
+    realtime_provider = await Provider.realtime_async(supernova=True)
+    assert isinstance(realtime_provider, RealtimeProvider)
+    assert realtime_provider.server.is_running
+    assert realtime_provider.server.is_owner
+    await realtime_provider.server.quit()
+    assert not realtime_provider.server.is_running
+    assert not realtime_provider.server.is_owner
