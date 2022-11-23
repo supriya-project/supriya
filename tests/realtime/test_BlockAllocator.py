7c3f4747c82cfc9d7355ff471e85f224e245230d
Marek Miller <marek.miller@protonmail.com>
2022-11-23 19:59:30 +0100

    Test allocation of overlapping blocks

--- tests/realtime/test_BlockAllocator.py
+++ tests/realtime/test_BlockAllocator.py
@@ -30,6 +30,14 @@ def test_allocate():
 
 def test_allocate_block_within_block():
     allocator = supriya.realtime.BlockAllocator()
-    assert allocator.allocate(1) == 0
+    assert allocator.allocate_at(0, 3) == 0
     assert allocator.allocate_at(0, 2) is None
-    assert allocator.allocate_at(0, 1) is None
+    assert allocator.allocate_at(1, 2) is None
+    assert allocator.allocate_at(2, 2) is None
+    assert allocator.allocate_at(6, 3) == 6
+    assert allocator.allocate_at(5, 2) is None
+    assert allocator.allocate_at(2, 5) is None
+    assert allocator.allocate_at(0, 9) is None
+    assert allocator.allocate_at(3, 3) == 3
+
+    assert allocator.free(99) is None
