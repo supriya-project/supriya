8d66c401929d5cbba7974f048961fb9f0cbe37a5
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2023-05-26 12:08:50 -0400

    Bump version to 23.5b5

--- supriya/_version.py
+++ supriya/_version.py
@@ -7,5 +7,5 @@ Version information.
 
 This follows black's versioning scheme.
 """
-__version_info__ = (23, "5b4")
+__version_info__ = (23, "5b5")
 __version__ = ".".join(str(x) for x in __version_info__)
