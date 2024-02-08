2f04d2d2160b34523d47d18ecbe79fa9d4a6484b
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2024-02-08 10:19:34 -0500

    Use functions, not strings for operators

--- tests/patterns/test_BinaryOpPattern.py
+++ tests/patterns/test_BinaryOpPattern.py
@@ -1,3 +1,5 @@
+import operator
+
 import pytest
 
 from supriya.patterns import BinaryOpPattern, SequencePattern
@@ -7,18 +9,18 @@ from supriya.patterns.testutils import run_pattern_test
 @pytest.mark.parametrize(
     "stop_at, operator, input_a, input_b, expected, is_infinite",
     [
-        (None, "+", 1, 7, [8], True),
-        (None, "+", [1], 7, [(8,)], True),
-        (None, "+", [1], [7], [(8,)], True),
-        (None, "+", [[1]], [7], [((8,),)], True),
-        (None, "+", [[[1]]], [7], [(((8,),),)], True),
-        (None, "+", [1, 2], [7, 8], [(8, 10)], True),
-        (None, "+", [1, 2], [7, 8, 10], [(8, 10, 11)], True),
-        (None, "*", SequencePattern([1, 2, 3]), 4, [4, 8, 12], False),
-        (None, "*", SequencePattern([1, 2, 3], None), 4, [4, 8, 12], True),
+        (None, operator.add, 1, 7, [8], True),
+        (None, operator.add, [1], 7, [(8,)], True),
+        (None, operator.add, [1], [7], [(8,)], True),
+        (None, operator.add, [[1]], [7], [((8,),)], True),
+        (None, operator.add, [[[1]]], [7], [(((8,),),)], True),
+        (None, operator.add, [1, 2], [7, 8], [(8, 10)], True),
+        (None, operator.add, [1, 2], [7, 8, 10], [(8, 10, 11)], True),
+        (None, operator.mul, SequencePattern([1, 2, 3]), 4, [4, 8, 12], False),
+        (None, operator.mul, SequencePattern([1, 2, 3], None), 4, [4, 8, 12], True),
         (
             None,
-            "+",
+            operator.add,
             SequencePattern(["a", "b", "c"]),
             SequencePattern(["1", "2", "3"]),
             ["a1", "b2", "c3"],
--- tests/patterns/test_UnaryOpPattern.py
+++ tests/patterns/test_UnaryOpPattern.py
@@ -1,3 +1,5 @@
+import operator
+
 import pytest
 
 from supriya.patterns import SequencePattern, UnaryOpPattern
@@ -7,13 +9,13 @@ from supriya.patterns.testutils import run_pattern_test
 @pytest.mark.parametrize(
     "stop_at, operator, input_, expected, is_infinite",
     [
-        (None, "-", 1, [-1], True),
-        (None, "-", [1], [(-1,)], True),
-        (None, "-", [[1]], [((-1,),)], True),
-        (None, "-", [[[1]]], [(((-1,),),)], True),
-        (None, "-", [1, 2], [(-1, -2)], True),
-        (None, "-", SequencePattern([1, 2, 3]), [-1, -2, -3], False),
-        (None, "-", SequencePattern([1, 2, 3], None), [-1, -2, -3], True),
+        (None, operator.neg, 1, [-1], True),
+        (None, operator.neg, [1], [(-1,)], True),
+        (None, operator.neg, [[1]], [((-1,),)], True),
+        (None, operator.neg, [[[1]]], [(((-1,),),)], True),
+        (None, operator.neg, [1, 2], [(-1, -2)], True),
+        (None, operator.neg, SequencePattern([1, 2, 3]), [-1, -2, -3], False),
+        (None, operator.neg, SequencePattern([1, 2, 3], None), [-1, -2, -3], True),
     ],
 )
 def test(stop_at, operator, input_, expected, is_infinite):
