bd6b3ade82b9081df0dd08d47b63e7aa26ac1f56
Joséphine Wolf Oberholtzer <josephine.wolf.oberholtzer@gmail.com>
2025-02-15 17:31:40 -0500

    Harden scsynth.Options

--- tests/test_scsynth.py
+++ tests/test_scsynth.py
@@ -46,10 +46,11 @@ def test_find_on_path(mock_env_scsynth_path, monkeypatch):
 @pytest.mark.parametrize(
     "kwargs, expected",
     [
+        # nothing specified
         (
             {},
             [
-                "/path/to/scsynth",
+                "/path/to/executable",
                 "-R",
                 "0",
                 "-l",
@@ -62,7 +63,7 @@ def test_find_on_path(mock_env_scsynth_path, monkeypatch):
         (
             {"input_device": "Device X"},
             [
-                "/path/to/scsynth",
+                "/path/to/executable",
                 "-H",
                 "Device X",
                 "",
@@ -78,7 +79,7 @@ def test_find_on_path(mock_env_scsynth_path, monkeypatch):
         (
             {"output_device": "Device Y"},
             [
-                "/path/to/scsynth",
+                "/path/to/executable",
                 "-H",
                 "",
                 "Device Y",
@@ -94,7 +95,7 @@ def test_find_on_path(mock_env_scsynth_path, monkeypatch):
         (
             {"input_device": "Device P", "output_device": "Device Q"},
             [
-                "/path/to/scsynth",
+                "/path/to/executable",
                 "-H",
                 "Device P",
                 "Device Q",
@@ -110,7 +111,7 @@ def test_find_on_path(mock_env_scsynth_path, monkeypatch):
         (
             {"input_device": "Device Z", "output_device": "Device Z"},
             [
-                "/path/to/scsynth",
+                "/path/to/executable",
                 "-H",
                 "Device Z",
                 "-R",
@@ -121,10 +122,103 @@ def test_find_on_path(mock_env_scsynth_path, monkeypatch):
                 "57110",
             ],
         ),
+        # many flags specified
+        (
+            {
+                "audio_bus_channel_count": 1024 * 2,
+                "block_size": 64 * 2,
+                "buffer_count": 1024 * 2,
+                "control_bus_channel_count": 16384 * 2,
+                "executable": "supernova",
+                "hardware_buffer_size": 512,
+                "initial_node_id": 100,
+                "input_bus_channel_count": 16,
+                "input_device": "Hypothetical Input Device",
+                "input_stream_mask": "01101010",
+                "ip_address": "0.0.0.0",
+                "load_synthdefs": False,
+                "maximum_logins": 23,
+                "maximum_node_count": 1024 * 2,
+                "maximum_synthdef_count": 1024 * 2,
+                "memory_locking": True,
+                "memory_size": 8192 * 2,
+                "output_bus_channel_count": 8 * 2,
+                "output_device": "Hypothetical Output Device",
+                "output_stream_mask": "10010101",
+                "password": "changeme",
+                "protocol": "tcp",
+                "random_number_generator_count": 64 * 2,
+                "restricted_path": "/restricted/path",
+                "safety_clip": "inf",
+                "sample_rate": 44100,
+                "threads": 8,
+                "ugen_plugins_path": "/path/to/ugens",
+                "verbosity": 1,
+                "wire_buffer_count": 64 * 22,
+                "zero_configuration": True,
+            },
+            [
+                "/path/to/executable",
+                "-B",
+                "0.0.0.0",
+                "-D",
+                "0",
+                "-H",
+                "Hypothetical Input Device",
+                "Hypothetical Output Device",
+                "-I",
+                "01101010",
+                "-L",
+                "-O",
+                "10010101",
+                "-P",
+                "/restricted/path",
+                "-S",
+                "44100",
+                "-T",
+                "8",
+                "-U",
+                "/path/to/ugens",
+                "-Z",
+                "512",
+                "-a",
+                "2048",
+                "-b",
+                "2048",
+                "-c",
+                "32768",
+                "-d",
+                "2048",
+                "-i",
+                "16",
+                "-l",
+                "23",
+                "-m",
+                "16384",
+                "-n",
+                "2048",
+                "-o",
+                "16",
+                "-p",
+                "changeme",
+                "-r",
+                "128",
+                "-s",
+                "inf",
+                "-t",
+                "57110",
+                "-v",
+                "1",
+                "-w",
+                "1408",
+                "-z",
+                "128",
+            ],
+        ),
     ],
 )
 def test_Options(kwargs: Dict, expected: List[str]) -> None:
     options = scsynth.Options(**kwargs)
     actual = list(options)
-    actual[0] = "/path/to/scsynth"  # replace to make it portable
+    actual[0] = "/path/to/executable"  # replace to make it portable
     assert actual == expected
