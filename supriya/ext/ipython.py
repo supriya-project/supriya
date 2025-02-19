a5c1701f7d90fd7f737350a32935476bc2714ab0
Joséphine Wolf Oberholtzer <166141926+josephine-wolf-oberholtzer@users.noreply.github.com>
2025-02-18 23:36:38 -0500

    Fix IPython extension (#442)

--- supriya/ext/ipython.py
+++ supriya/ext/ipython.py
@@ -1,22 +1,37 @@
 from IPython.display import Audio, display  # type: ignore
 
-from supriya.ext import websafe_audio
-from supriya.io import Player
+from ..io import Player, Plotter
+from ..typing import SupportsRenderMemo
+from . import websafe_audio
 
 
 def load_ipython_extension(ipython):
     ipython.extension_manager.load_extension("uqbar.ext.ipython")
     patch_player()
+    patch_plotter()
 
 
 def patch_player():
     async def patched_call(self):
-        output_path, status_code = await self.renderable.__render__(
+        if isinstance(self.renderable, SupportsRenderMemo):
+            supports_render = self.renderable.__render_memo__()
+        else:
+            supports_render = self.renderable
+        output_path, status_code = await supports_render.__render__(
             **self.render_kwargs,
         )
         if output_path:
+            # Don't open the output path natively
             output_path = websafe_audio(output_path)
             display(Audio(filename=str(output_path)))
         return output_path, status_code
 
     Player.__call__ = patched_call
+
+
+def patch_plotter():
+    def patched_call(self):
+        # Don't open the output path natively
+        return self.render()
+
+    Plotter.__call__ = patched_call
