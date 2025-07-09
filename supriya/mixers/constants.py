397d4cdf32b1b7e3d3beab456427a272b8050e68
Joséphine Wolf Oberholtzer <josephine.wolf.oberholtzer@gmail.com>
2025-07-09 08:10:26 -0400

    Implement mixers

---
+++ supriya/mixers/constants.py
@@ -0,0 +1,45 @@
+from typing import Literal, TypeAlias
+
+try:
+    from enum import StrEnum
+except ImportError:
+    from backports.strenum import StrEnum  # type: ignore
+
+Address: TypeAlias = str
+
+ChannelCount: TypeAlias = Literal[1, 2, 4, 8]
+
+
+class IO(StrEnum):
+    READ = "read"
+    WRITE = "write"
+
+
+class Names(StrEnum):
+    ACTIVE = "active"
+    AUDIO_BUSSES = "audio-busses"
+    BUFFERS = "buffers"
+    CHANNEL_STRIP = "channel-strip"
+    CONTROL_BUSSES = "control-busses"
+    DEVICES = "devices"
+    FEEDBACK = "feedback"
+    GAIN = "gain"
+    GROUP = "group"
+    INPUT = "input"
+    INPUT_LEVELS = "input-levels"
+    MAIN = "main"
+    NODES = "nodes"
+    OUTPUT = "output"
+    OUTPUT_LEVELS = "output-levels"
+    SYNTH = "synth"
+    SYNTHDEFS = "synthdefs"
+    TRACKS = "tracks"
+
+
+class Reconciliation(StrEnum):
+    CREATE = "create"
+    RECREATE = "recreate"
+    MUTATE = "mutate"
+    DESTROY = "destroy"
+    DESTROY_SHALLOW = "destroy-shallow"
+    DESTROY_ROOT = "destroy-root"
---
+++ supriya/mixers/mixers.py
@@ -0,0 +1,255 @@
+from typing import TYPE_CHECKING, Optional
+
+from ..contexts import AsyncServer
+from ..enums import AddAction, CalculationRate, DoneAction
+from ..typing import Default
+from ..ugens.system import (
+    build_channel_strip_synthdef,
+    build_meters_synthdef,
+    build_patch_cable_synthdef,
+)
+from .components import (
+    Component,
+)
+from .constants import Address, ChannelCount, Names
+from .devices import DeviceContainer
+from .specs import (
+    BusSpec,
+    GroupSpec,
+    Spec,
+    SynthDefSpec,
+    SynthSpec,
+)
+from .tracks import TrackContainer
+
+if TYPE_CHECKING:
+    from .sessions import Session
+
+
+class Mixer(
+    DeviceContainer["Session"],
+    TrackContainer,
+):
+    def __init__(
+        self,
+        *,
+        id_: int,
+        name: str | None = None,
+        parent: Optional["Session"],
+    ) -> None:
+        Component.__init__(self, id_=id_, name=name, parent=parent)
+        DeviceContainer.__init__(self)
+        TrackContainer.__init__(self)
+
+    def _delete(self) -> None:
+        self._disconnect_parentage()
+
+    def _disconnect_parentage(self) -> None:
+        if (session := self._parent) is not None and self in (
+            mixers := session._contexts.get(session._mixers.pop(self), [])
+        ):
+            mixers.remove(self)
+        super()._disconnect_parentage()
+
+    def _resolve_specs(self, context: AsyncServer | None) -> list[Spec]:
+        if not context:
+            return []
+        if not self.parent:
+            raise RuntimeError
+        channel_strip_synthdef = build_channel_strip_synthdef(
+            self.effective_channel_count
+        )
+        meters_synthdef = build_meters_synthdef(self.effective_channel_count)
+        # Output clamps to the maximum output size of the context.
+        patch_cable_synthdef = build_patch_cable_synthdef(
+            self.effective_channel_count,
+            min(
+                [
+                    self.effective_channel_count,
+                    len(context.audio_output_bus_group),
+                ]
+            ),
+        )
+        return [
+            SynthDefSpec(
+                component=self,
+                context=context,
+                name=channel_strip_synthdef.effective_name,
+                synthdef=channel_strip_synthdef,
+            ),
+            SynthDefSpec(
+                component=self,
+                context=context,
+                name=meters_synthdef.effective_name,
+                synthdef=meters_synthdef,
+            ),
+            SynthDefSpec(
+                component=self,
+                context=context,
+                name=patch_cable_synthdef.effective_name,
+                synthdef=patch_cable_synthdef,
+            ),
+            BusSpec(
+                calculation_rate=CalculationRate.AUDIO,
+                channel_count=self.effective_channel_count,
+                component=self,
+                context=context,
+                name=Names.MAIN,
+            ),
+            BusSpec(
+                calculation_rate=CalculationRate.CONTROL,
+                channel_count=1,
+                component=self,
+                context=context,
+                name=Names.GAIN,
+            ),
+            BusSpec(
+                calculation_rate=CalculationRate.CONTROL,
+                channel_count=self.effective_channel_count,
+                component=self,
+                context=context,
+                name=Names.INPUT_LEVELS,
+            ),
+            BusSpec(
+                calculation_rate=CalculationRate.CONTROL,
+                channel_count=self.effective_channel_count,
+                component=self,
+                context=context,
+                name=Names.OUTPUT_LEVELS,
+            ),
+            GroupSpec(
+                add_action=AddAction.ADD_TO_HEAD,
+                component=self,
+                context=context,
+                destroy_strategy={"gate": 0},
+                name=Names.GROUP,
+                parent_node=None,
+                target_node=None,
+            ),
+            GroupSpec(
+                add_action=AddAction.ADD_TO_HEAD,
+                component=self,
+                context=context,
+                name=Names.TRACKS,
+                parent_node=None,
+                target_node=Spec.get_address(self, Names.NODES, Names.GROUP),
+            ),
+            GroupSpec(
+                add_action=AddAction.ADD_TO_TAIL,
+                component=self,
+                context=context,
+                name=Names.DEVICES,
+                parent_node=None,
+                target_node=Spec.get_address(self, Names.NODES, Names.GROUP),
+            ),
+            SynthSpec(
+                add_action=AddAction.ADD_TO_TAIL,
+                component=self,
+                context=context,
+                destroy_strategy={
+                    "done_action": DoneAction.FREE_SYNTH_AND_ENCLOSING_GROUP
+                },
+                kwargs={
+                    "gain": Spec.get_address(self, Names.CONTROL_BUSSES, Names.GAIN),
+                    "out": Spec.get_address(self, Names.AUDIO_BUSSES, Names.MAIN),
+                },
+                name=Names.CHANNEL_STRIP,
+                parent_node=None,
+                synthdef=Spec.get_address(
+                    None, Names.SYNTHDEFS, channel_strip_synthdef.effective_name
+                ),
+                target_node=Spec.get_address(self, Names.NODES, Names.GROUP),
+            ),
+            SynthSpec(
+                add_action=AddAction.ADD_AFTER,
+                component=self,
+                context=context,
+                kwargs={
+                    "in_": Spec.get_address(self, Names.AUDIO_BUSSES, Names.MAIN),
+                    "out": Spec.get_address(
+                        self, Names.CONTROL_BUSSES, Names.INPUT_LEVELS
+                    ),
+                },
+                name=Names.INPUT_LEVELS,
+                parent_node=None,
+                synthdef=Spec.get_address(
+                    None, Names.SYNTHDEFS, meters_synthdef.effective_name
+                ),
+                target_node=Spec.get_address(self, Names.NODES, Names.TRACKS),
+            ),
+            SynthSpec(
+                add_action=AddAction.ADD_AFTER,
+                component=self,
+                context=context,
+                kwargs={
+                    "in_": Spec.get_address(self, Names.AUDIO_BUSSES, Names.MAIN),
+                    "out": Spec.get_address(
+                        self, Names.CONTROL_BUSSES, Names.OUTPUT_LEVELS
+                    ),
+                },
+                name=Names.OUTPUT_LEVELS,
+                parent_node=None,
+                synthdef=Spec.get_address(
+                    None, Names.SYNTHDEFS, meters_synthdef.effective_name
+                ),
+                target_node=Spec.get_address(self, Names.NODES, Names.CHANNEL_STRIP),
+            ),
+            SynthSpec(
+                add_action=AddAction.ADD_TO_TAIL,
+                component=self,
+                context=context,
+                kwargs={
+                    "in_": Spec.get_address(self, Names.AUDIO_BUSSES, Names.MAIN),
+                    "out": context.audio_output_bus_group,
+                },
+                name=Names.OUTPUT,
+                parent_node=None,
+                synthdef=Spec.get_address(
+                    None, Names.SYNTHDEFS, patch_cable_synthdef.effective_name
+                ),
+                target_node=Spec.get_address(self, Names.NODES, Names.GROUP),
+            ),
+        ]
+
+    async def delete(self) -> None:
+        async with self._lock:
+            await Component._reconcile(
+                context=None,
+                deleting_components=[self],
+                reconciling_components=[self],
+                session=self.session,
+            )
+
+    async def set_channel_count(self, channel_count: ChannelCount | Default) -> None:
+        async with self._lock:
+            self._channel_count = channel_count
+            await Component._reconcile(
+                context=self.context,
+                reconciling_components=[self],
+                session=self.session,
+            )
+
+    def set_name(self, name: str | None = None) -> None:
+        self._name = name
+
+    @property
+    def address(self) -> Address:
+        if self.session is None:
+            return "mixers[?]"
+        index = self.session.mixers.index(self)
+        return f"session.mixers[{index}]"
+
+    @property
+    def children(self) -> list[Component]:
+        # return [*self._tracks, *self._devices]
+        return [*self._tracks, *self._devices]
+
+    @property
+    def context(self) -> AsyncServer | None:
+        if self.parent is None:
+            return None
+        return self.parent._mixers[self]
+
+    @property
+    def numeric_address(self) -> Address:
+        return f"mixers[{self._id}]"
