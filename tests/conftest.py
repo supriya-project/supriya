4fbe7dc3e3a100c6a2da4f6425044e79ff6ce673
Joséphine Wolf Oberholtzer <josephine.wolf.oberholtzer@gmail.com>
2025-07-10 10:32:15 -0400

    Lazy evaluate SynthDef functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,6 +31,14 @@ def persistent_server():
 
 
 @pytest.fixture(autouse=True)
+def system_synthdefs_lru_cache() -> None:
+    for name in dir(system):
+        object_ = getattr(system, name)
+        if hasattr(object_, "cache_clear()"):
+            object_.cache_clear()
+
+
+@pytest.fixture(autouse=True)
 def system_synthdefs_lag_time(monkeypatch: MonkeyPatch) -> None:
     if platform.system() == "Windows" and os.environ.get("CI"):
         monkeypatch.setattr(system, "LAG_TIME", 0.25)
