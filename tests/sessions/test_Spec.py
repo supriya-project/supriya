0e5a9b77c842203db495f0abe35d74fbb1cde0ff
Joséphine Wolf Oberholtzer <josephine.wolf.oberholtzer@gmail.com>
2025-10-21 19:00:03 -0400

    Modernize Spec.feedsback tests

--- tests/sessions/test_Spec.py
+++ tests/sessions/test_Spec.py
@@ -5,53 +5,50 @@ from supriya.sessions.specs import Spec
 
 
 @pytest.mark.parametrize(
-    "writer_name, reader_name, feedsback",
+    "writer_address, reader_address, feedsback",
     [
-        ("mixer", "mixer", True),
-        ("mixer", "track_one", True),
-        ("mixer", "track_two", True),
-        ("mixer", "track_one_child", True),
-        ("mixer", "track_two_child", True),
-        ("track_one", "mixer", False),
-        ("track_one", "track_one", True),
-        ("track_one", "track_two", False),
-        ("track_one", "track_one_child", True),
-        ("track_one", "track_two_child", False),
-        ("track_two", "mixer", False),
-        ("track_two", "track_one", True),
-        ("track_two", "track_two", True),
-        ("track_two", "track_one_child", True),
-        ("track_two", "track_two_child", True),
-        ("track_one_child", "mixer", False),
-        ("track_one_child", "track_one", False),
-        ("track_one_child", "track_two", False),
-        ("track_one_child", "track_one_child", True),
-        ("track_one_child", "track_two_child", False),
-        ("track_two_child", "mixer", False),
-        ("track_two_child", "track_one", True),
-        ("track_two_child", "track_two", False),
-        ("track_two_child", "track_one_child", True),
-        ("track_two_child", "track_two_child", True),
+        ("mixers[0]", "mixers[0]", True),
+        ("mixers[0]", "mixers[0].tracks[0]", True),
+        ("mixers[0]", "mixers[0].tracks[1]", True),
+        ("mixers[0]", "mixers[0].tracks[0].tracks[0]", True),
+        ("mixers[0]", "mixers[0].tracks[1].tracks[0]", True),
+        ("mixers[0].tracks[0]", "mixers[0]", False),
+        ("mixers[0].tracks[0]", "mixers[0].tracks[0]", True),
+        ("mixers[0].tracks[0]", "mixers[0].tracks[1]", False),
+        ("mixers[0].tracks[0]", "mixers[0].tracks[0].tracks[0]", True),
+        ("mixers[0].tracks[0]", "mixers[0].tracks[1].tracks[0]", False),
+        ("mixers[0].tracks[1]", "mixers[0]", False),
+        ("mixers[0].tracks[1]", "mixers[0].tracks[0]", True),
+        ("mixers[0].tracks[1]", "mixers[0].tracks[1]", True),
+        ("mixers[0].tracks[1]", "mixers[0].tracks[0].tracks[0]", True),
+        ("mixers[0].tracks[1]", "mixers[0].tracks[1].tracks[0]", True),
+        ("mixers[0].tracks[0].tracks[0]", "mixers[0]", False),
+        ("mixers[0].tracks[0].tracks[0]", "mixers[0].tracks[0]", False),
+        ("mixers[0].tracks[0].tracks[0]", "mixers[0].tracks[1]", False),
+        ("mixers[0].tracks[0].tracks[0]", "mixers[0].tracks[0].tracks[0]", True),
+        ("mixers[0].tracks[0].tracks[0]", "mixers[0].tracks[1].tracks[0]", False),
+        ("mixers[0].tracks[1].tracks[0]", "mixers[0]", False),
+        ("mixers[0].tracks[1].tracks[0]", "mixers[0].tracks[0]", True),
+        ("mixers[0].tracks[1].tracks[0]", "mixers[0].tracks[1]", False),
+        ("mixers[0].tracks[1].tracks[0]", "mixers[0].tracks[0].tracks[0]", True),
+        ("mixers[0].tracks[1].tracks[0]", "mixers[0].tracks[1].tracks[0]", True),
     ],
 )
 @pytest.mark.asyncio
 async def test_Spec_feedsback(
-    basic_session: tuple[Session, str, str],
     feedsback: bool,
-    writer_name: str,
-    reader_name: str,
+    writer_address: str,
+    reader_address: str,
 ) -> None:
-    session, _, _ = basic_session
-    components: dict[str, Component] = {
-        "mixer": (mixer := session.mixers[0]),
-        "track_one": (track_one := mixer.tracks[0]),
-        "track_two": (track_two := await mixer.add_track()),
-        "track_one_child": await track_one.add_track(),
-        "track_two_child": await track_two.add_track(),
-    }
-    writer = components[writer_name]
-    reader = components[reader_name]
+    session = Session()
+    mixer = await session.add_mixer()
+    track_one = await mixer.add_track()
+    track_two = await mixer.add_track()
+    await track_one.add_track()
+    await track_two.add_track()
+    writer = session[writer_address]
+    reader = session[reader_address]
     assert Spec.feedsback(writer.graph_order, reader.graph_order) == feedsback, (
-        writer_name,
-        reader_name,
+        writer_address,
+        reader_address,
     )
