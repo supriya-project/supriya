2696b8e555d282a8539c1f055ba456fa2878f372
Joséphine Wolf Oberholtzer <josephine.wolf.oberholtzer@gmail.com>
2026-02-07 20:34:47 -0500

    Implementing polyphony handling

--- supriya/sessions/performers.py
+++ supriya/sessions/performers.py
@@ -8,7 +8,7 @@ from typing import (
     Type,
 )
 
-from .constants import IO
+from .constants import IO, PolyphonyMode
 
 logger = logging.getLogger(__name__)
 
@@ -27,7 +27,7 @@ class NoteOn(PerformanceEvent):
 @dataclasses.dataclass(frozen=True)
 class NoteOff(PerformanceEvent):
     note_number: float
-    velocity: float
+    velocity: float = 0.0
 
 
 class Performer:
@@ -41,6 +41,36 @@ class Performer:
             NoteOn: self._on_note_on,
             NoteOff: self._on_note_off,
         }
+        self._polyphony_limit: int | None = None
+        self._polyphony_mode: PolyphonyMode = PolyphonyMode.FREE_OLDEST
+        self._retrigger: bool = False
+
+    def _apply_polyphony(
+        self,
+        *,
+        note_number: float,
+        polyphony_mode: PolyphonyMode,
+        polyphony_limit: int,
+    ) -> list[PerformanceEvent]:
+        events: list[PerformanceEvent] = []
+        if (count := len(self._input_note_numbers) - polyphony_limit) < 1:
+            return events
+        if polyphony_mode == PolyphonyMode.FREE_OLDEST:
+            for _ in range(count):
+                events.append(NoteOff(note_number=self._input_note_numbers.pop(0)))
+            return events
+        if polyphony_mode == PolyphonyMode.FREE_LOWEST:
+            sorted_note_numbers = sorted(self._input_note_numbers)
+        elif polyphony_mode == PolyphonyMode.FREE_HIGHEST:
+            sorted_note_numbers = sorted(self._input_note_numbers, reverse=True)
+        elif polyphony_mode == PolyphonyMode.FREE_NEAREST:
+            sorted_note_numbers = sorted(
+                self._input_note_numbers, key=lambda x: (abs(x - note_number), x)
+            )
+        for i in range(count):
+            self._input_note_numbers.remove(note_number := sorted_note_numbers[i])
+            events.append(NoteOff(note_number=note_number))
+        return events
 
     def _flush(self) -> None:
         logger.info("flushing: self=%s", self)
@@ -74,29 +104,40 @@ class Performer:
 
     def _on_note_on(self, event: PerformanceEvent, io: IO) -> list[PerformanceEvent]:
         assert isinstance(event, NoteOn)
+        events: list[PerformanceEvent] = []
         if io == IO.READ:
-            if event.note_number in self._input_note_numbers:
-                return []
-            self._input_note_numbers.append(event.note_number)
-            return [event]
+            if event.note_number not in self._input_note_numbers:
+                if self._polyphony_limit:
+                    events.extend(
+                        self._apply_polyphony(
+                            note_number=event.note_number,
+                            polyphony_mode=self._polyphony_mode,
+                            polyphony_limit=self._polyphony_limit - 1,
+                        )
+                    )
+                # apply polyphony limit
+                self._input_note_numbers.append(event.note_number)
+                events.append(event)
+            elif self._retrigger:
+                events.extend([NoteOff(note_number=event.note_number), event])
         else:
-            if event.note_number in self._output_note_numbers:
-                return []
-            self._output_note_numbers.add(event.note_number)
-            return [event]
+            if event.note_number not in self._output_note_numbers:
+                self._output_note_numbers.add(event.note_number)
+                events.append(event)
+        return events
 
     def _on_note_off(self, event: PerformanceEvent, io: IO) -> list[PerformanceEvent]:
         assert isinstance(event, NoteOff)
+        events: list[PerformanceEvent] = []
         if io == IO.READ:
-            if event.note_number not in self._input_note_numbers:
-                return []
-            self._input_note_numbers.remove(event.note_number)
-            return [event]
+            if event.note_number in self._input_note_numbers:
+                self._input_note_numbers.remove(event.note_number)
+                events.append(event)
         else:
-            if event.note_number not in self._output_note_numbers:
-                return []
-            self._output_note_numbers.add(event.note_number)
-            return [event]
+            if event.note_number in self._output_note_numbers:
+                self._output_note_numbers.remove(event.note_number)
+                events.append(event)
+        return events
 
     def _perform_loop(
         self, performer: "Performer", io: IO, events: list[PerformanceEvent]
@@ -116,6 +157,12 @@ class Performer:
                 if not events:
                     continue
                 stack.append((performer, io, events))
+
+    def _set_polyphony_limit(self, limit: int | None) -> None:
+        raise NotImplementedError
+
+    def _set_polyphony_mode(self, mode: PolyphonyMode) -> None:
+        raise NotImplementedError
 
     def perform(self, events: list[PerformanceEvent]) -> None:
         self._perform_loop(self, IO.READ, events)
