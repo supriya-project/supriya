0278267ce09203aa9b6df7779426138101c24f1e
Joséphine Wolf Oberholtzer <josephine.wolf.oberholtzer@gmail.com>
2026-03-16 10:44:23 -0400

    Use int / slice with ServerSHM

--- tests/contexts/test_ServerSHM.py
+++ tests/contexts/test_ServerSHM.py
@@ -14,8 +14,6 @@ def test_shared_memory(server: Server) -> None:
         calculation_rate="CONTROL", count=random.randint(4, 16)
     )
 
-    assert server.shared_memory[bus] == 0.0
-    assert server.shared_memory[bus_group] == [0.0] * len(bus_group)
     assert server.shared_memory[int(bus)] == 0.0
     assert server.shared_memory[int(bus_group) : int(bus_group) + len(bus_group)] == [
         0.0
@@ -31,14 +29,3 @@ def test_shared_memory(server: Server) -> None:
         server.shared_memory[int(bus_group) : int(bus_group) + len(bus_group)]
         == values[: len(bus_group)]
     )
-
-    random.shuffle(values)
-    server.shared_memory[bus] = values[-1]
-    server.shared_memory[bus_group] = values[: len(bus_group)]
-    assert (
-        server.shared_memory[int(bus_group) : int(bus_group) + len(bus_group)]
-        == values[: len(bus_group)]
-    )
-    assert server.shared_memory[int(bus)] == values[-1]
-    assert server.shared_memory[bus] == values[-1]
-    assert server.shared_memory[bus_group] == values[: len(bus_group)]
